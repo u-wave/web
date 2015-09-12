4f583df07d8b2bd6246667390fb992c4fa966e2c
René Kooi <rene@kooi.me>
2015-09-12 19:28:06 +0200

    build: make generated CSS classes less insane

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 browserify                                \
-  -p [ css-modulesify -o lib/style.css ]  \
+  -p [ css-modulesify -o lib/style.css    \
+                      -d src/js        ]  \
   -t babelify                             \
   -e src/js/app.js                        \
   -o lib/out.js                           \
