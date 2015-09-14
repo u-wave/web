ac9406968a7e69a345ad555e7d45dbdcbdd30083
René Kooi <rene@kooi.me>
2015-09-14 12:23:35 +0200

    playback: fire change events every second a song plays

--- src/js/stores/CurrentMediaStore.js
+++ src/js/stores/CurrentMediaStore.js
@@ -4,17 +4,33 @@ import dispatcher from '../dispatcher';
 
 const clone = x => assign({}, x);
 
+function tick() {
+  CurrentMediaStore.emit('change');
+}
+
 let media = {};
+let startTime = null;
 
 const CurrentMediaStore = assign(new EventEmitter, {
+  init() {
+    setInterval(() => {
+      tick();
+    }, 1000);
+  },
+
   getMedia() {
     return clone(media);
+  },
+
+  getTimeElapsed() {
+    return Math.floor(Date.now() / 1000 - startTime);
   },
 
   dispatchToken: dispatcher.register(payload => {
     switch (payload.action) {
     case 'advance':
       media = payload.media;
+      startTime = Math.floor(Date.now() / 1000) - (media.seek || 0);
       CurrentMediaStore.emit('change');
       break;
     default:
