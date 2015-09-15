587c676b09a77d6f259bda88ac7437f581f5cf3a
René Kooi <rene@kooi.me>
2015-09-15 22:02:52 +0200

    chat: hide chat placeholder when focused

--- src/js/components/Chat/Input.js
+++ src/js/components/Chat/Input.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 export default class Input extends React.Component {
 
+  state = { focused: false };
+
+  onFocus(e) {
+    this.setState({ focused: true });
+  }
+  onBlur(e) {
+    this.setState({ focused: false });
+  }
+
   render() {
     return (
       <div className="ChatInput">
         <input
           className="ChatInput-input"
           type="text"
-          placeholder="Click here to chat!"
+          placeholder={this.state.focused ? '' : 'Click here to chat!'}
+          onFocus={this.onFocus.bind(this)}
+          onBlur={this.onBlur.bind(this)}
         />
       </div>
     );
