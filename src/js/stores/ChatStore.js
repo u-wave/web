12702dfd465d28a0e170d7f9aa879905287836b4
René Kooi <rene@kooi.me>
2015-09-15 09:25:43 +0200

    chat: keep up to $MAX_MESSAGES messages in memory

--- src/js/stores/ChatStore.js
+++ src/js/stores/ChatStore.js
@@ -2,6 +2,7 @@ import assign from 'object-assign';
 import EventEmitter from 'events';
 import dispatcher from '../dispatcher';
 
+const MAX_MESSAGES = 500;
 let messages = [];
 
 const ChatStore = assign(new EventEmitter, {
@@ -13,6 +14,9 @@ const ChatStore = assign(new EventEmitter, {
     switch (payload.action) {
     case 'chatReceive':
       messages = messages.concat([ payload.message ]);
+      if (messages.length > MAX_MESSAGES) {
+        messages = messages.slice(1);
+      }
       ChatStore.emit('change');
       break;
     default:
