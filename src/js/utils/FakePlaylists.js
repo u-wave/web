cca9a20e9d78e751d90d12a7f8d4f06c37ab8d72
Renée Kooi <rene@kooi.me>
2015-09-24 15:58:45 +0200

    playlists: more/better test data

--- src/js/utils/FakePlaylists.js
+++ src/js/utils/FakePlaylists.js
@@ -4,9 +4,43 @@ function id() {
   return Math.floor(Math.random() * 1e9);
 }
 
+const playlists = [
+  {
+    name: 'K-pop',
+    media: [
+      { artist: 'f(x)', title: 'Airplane' },
+      { artist: '4Minute', title: 'Crazy' }
+    ]
+  },
+  {
+    name: 'K-pop also',
+    media: [
+      { artist: 'Dead Gakkahs', title: 'PARADOX OVER PARADOX' },
+      { artist: 'DIEALRIGHT', title: 'Heaven' }
+    ]
+  },
+  {
+    name: 'K-indie',
+    media: [
+      { artist: 'Hee Young', title: 'Are You Still Waiting?' },
+      { artist: 'Morrie', title: 'Dead At 5:16' }
+    ]
+  },
+  {
+    name: 'K-hiphop',
+    media: [
+      { artist: 'Swings', title: 'Bulldozer' },
+      { artist: 'Gray', title: 'Summer Night' }
+    ]
+  }
+];
+
+playlists.forEach(pl => {
+  pl.id = id();
+  pl.count = pl.media.length;
+});
+
 export function init() {
   // Normally, a server call
-  setPlaylists([
-    { id: id(), name: 'K-pop', media: [ 1, 2, 3 ] }
-  ]);
+  setPlaylists(playlists);
 }
