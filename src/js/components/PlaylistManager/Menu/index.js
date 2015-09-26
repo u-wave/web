6b93fdd2acfdcb7bbeb33f913fc5e8fad7d6c091
Renée Kooi <rene@kooi.me>
2015-09-27 00:54:28 +0200

    playlists: sort playlist menu alphabetically

--- src/js/components/PlaylistManager/Menu/index.js
+++ src/js/components/PlaylistManager/Menu/index.js
@@ -10,9 +10,14 @@ export default class Menu extends React.Component {
 
   render() {
     const { className, playlists } = this.props;
+    const sorted = playlists.slice().sort((a, b) => {
+      if (a.name > b.name) return 1;
+      if (a.name < b.name) return -1;
+      return 0;
+    });
     return (
       <div className={cx('PlaylistMenu', className)}>
-        {playlists.map(pl => {
+        {sorted.map(pl => {
           return <PlaylistRow key={pl.id} className="PlaylistMenu-row" playlist={pl} />;
         })}
       </div>
