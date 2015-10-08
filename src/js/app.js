a80cfb7fcc880bfb59ee991efae2ad5f8a4ff9bc
Renée Kooi <rene@kooi.me>
2015-10-08 22:05:31 +0200

    app: render into wrapper div instead of body

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import CurrentMediaStore from './stores/CurrentMediaStore';
 import * as Socket from './utils/Socket';
 import * as FakePlaylists from './utils/FakePlaylists';
 
-React.render(<App />, document.body);
+React.render(<App />, document.getElementById('app'));
 
 // temporary debug and testing things
 
