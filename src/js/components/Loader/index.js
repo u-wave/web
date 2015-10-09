7f9c54b05162654121452a5441b1d322eb2da9ad
Renée Kooi <rene@kooi.me>
2015-10-10 01:00:19 +0200

    loader: force spinner to always be square

--- src/js/components/Loader/index.js
+++ src/js/components/Loader/index.js
@@ -2,7 +2,11 @@ import cx from 'classnames';
 import React from 'react';
 
 const Loader = ({ className, size }) => {
-  return <div className={cx('Loader', `Loader--${size}`, className)} />;
+  return (
+    <div className={cx('Loader', className)}>
+      <div className={cx('Spinner', `Spinner--${size}`)} />
+    </div>
+  );
 };
 
 export default Loader;
