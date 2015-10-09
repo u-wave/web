a3b1feb8f5d6d17c578591058427baf59b2f5c36
Renée Kooi <rene@kooi.me>
2015-10-09 19:24:39 +0200

    dispatcher: add debug logging to dispatcher

--- src/js/dispatcher.js
+++ src/js/dispatcher.js
@@ -1,6 +1,15 @@
 import { Dispatcher } from 'flux';
 
-const dispatcher = new Dispatcher();
+const debug = require('debug')('uwave:dispatcher');
+
+class UWaveDispatcher extends Dispatcher {
+  dispatch(payload) {
+    debug('dispatch', payload.action, payload);
+    super.dispatch(payload);
+  }
+}
+
+const dispatcher = new UWaveDispatcher();
 
 export default dispatcher;
 export const dispatch = ::dispatcher.dispatch;
