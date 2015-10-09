c0fa3f1830c4e4401743676f8d52c6f0146f3b8b
Renée Kooi <rene@kooi.me>
2015-10-09 19:32:19 +0200

    loader: fix loader size css class

--- src/js/components/Loader/index.js
+++ src/js/components/Loader/index.js
@@ -2,7 +2,7 @@ import cx from 'classnames';
 import React from 'react';
 
 const Loader = ({ className, size }) => {
-  return <div className={cx('Loader', `Loader-${size}`, className)} />;
+  return <div className={cx('Loader', `Loader--${size}`, className)} />;
 };
 
 export default Loader;
