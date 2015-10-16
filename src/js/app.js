56e52a7bbdab76dc87e37dda80092e8d422fcbc1
Renée Kooi <rene@kooi.me>
2015-10-17 00:54:32 +0200

    login: also initialise state when not logged in

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import CurrentMediaStore from './stores/CurrentMediaStore';
 import * as Socket from './utils/Socket';
-import { setJWT as restoreSession } from './actions/LoginActionCreators';
+import {
+  initState,
+  setJWT as restoreSession
+} from './actions/LoginActionCreators';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
 if ('_session' in localStorage) {
   restoreSession(localStorage._session);
+} else {
+  initState();
 }
 
 // temporary debug and testing things
