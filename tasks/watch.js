8682a321a383e9a5f10acdf6edc2798070784b9d
Renée Kooi <rene@kooi.me>
2015-10-27 09:22:43 +0100

    build: show redbox-react when component rendering fails

--- tasks/watch.js
+++ tasks/watch.js
@@ -32,7 +32,8 @@ export default function watchTask() {
     extra: {
       'react-transform': {
         transforms: [
-          { transform: 'react-transform-hmr', imports: [ 'react' ], locals: [ 'module' ] }
+          { transform: 'react-transform-hmr', imports: [ 'react' ], locals: [ 'module' ] },
+          { transform: 'react-transform-catch-errors', imports: [ 'react', 'redbox-react' ] }
         ]
       }
     }
