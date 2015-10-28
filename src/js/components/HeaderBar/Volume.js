f53af21e9ff71474777ac8958359af35c7bb29b2
Renée Kooi <rene@kooi.me>
2015-10-28 21:00:18 +0100

    header: use material-ui slider for volume control

--- src/js/components/HeaderBar/Volume.js
+++ src/js/components/HeaderBar/Volume.js
@@ -1,5 +1,6 @@
 import cx from 'classnames';
 import React from 'react';
+import Slider from 'material-ui/lib/slider';
 import VolumeDownIcon from 'material-ui/lib/svg-icons/av/volume-down';
 import VolumeMuteIcon from 'material-ui/lib/svg-icons/av/volume-mute';
 import VolumeOffIcon from 'material-ui/lib/svg-icons/av/volume-off';
@@ -7,15 +8,13 @@ import VolumeUpIcon from 'material-ui/lib/svg-icons/av/volume-up';
 import { setVolume, mute, unmute } from '../../actions/PlaybackActionCreators';
 
 export default class Volume extends React.Component {
-
   static propTypes = {
     className: React.PropTypes.string,
     volume: React.PropTypes.number,
     muted: React.PropTypes.bool
-  }
+  };
 
-  onChange(e) {
-    const volume = parseInt(e.target.value, 10);
+  onChange(e, volume) {
     setVolume(volume);
   }
 
@@ -42,14 +41,16 @@ export default class Volume extends React.Component {
           color="#fff"
           onClick={::this.toggleMute}
         />
-        <input
-          ref="value"
-          type="range"
-          min="0"
-          max="100"
-          value={this.props.volume}
-          onChange={this.onChange.bind(this)}
-        />
+        <div className="VolumeSlider-slider">
+          <Slider
+            name="volume"
+            min={0}
+            max={100}
+            defaultValue={this.props.volume}
+            style={{ marginTop: 3, marginBottom: 3 }}
+            onChange={::this.onChange}
+          />
+        </div>
       </div>
     );
   }
