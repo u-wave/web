26db2191d98738ca8eea57de2f959b1a3a08ed69
Renée Kooi <rene@kooi.me>
2015-10-30 18:46:50 +0100

    playlists: sort playlists by name in add dropdown

--- src/js/components/PlaylistManager/AddingMenu/index.js
+++ src/js/components/PlaylistManager/AddingMenu/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import naturalCmp from 'natural-compare';
 import Menu from 'material-ui/lib/menus/menu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import PlaylistStore from '../../../stores/PlaylistStore';
+
+const byName = (a, b) => naturalCmp(a.name.toLowerCase(), b.name.toLowerCase());
 
 export default class AddingMenu extends React.Component {
   static propTypes = {
@@ -18,6 +21,7 @@ export default class AddingMenu extends React.Component {
     if (!playlists) {
       playlists = PlaylistStore.getPlaylists();
     }
+    playlists = playlists.slice().sort(byName);
     return (
       <Menu
         style={{ textAlign: 'left' }}
