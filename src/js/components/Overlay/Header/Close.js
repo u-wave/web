189fd0d2f18fb9d3c2631fb7d67bc6831f464733
Renée Kooi <rene@kooi.me>
2015-11-03 23:19:16 +0100

    overlay: simplify default overlay header components

--- src/js/components/Overlay/Header/Close.js
+++ src/js/components/Overlay/Header/Close.js
@@ -3,25 +3,19 @@ import React from 'react';
 import CloseIcon from 'material-ui/lib/svg-icons/hardware/keyboard-arrow-down';
 import { closeAll } from '../../../actions/OverlayActionCreators';
 
-export default class Close extends React.Component {
-  static propTypes = {
-    className: React.PropTypes.string
-  };
+const Close = ({ className }) => {
+  return (
+    <div
+      className={cx('OverlayHeaderClose', className)}
+      onClick={closeAll}
+    >
+      <CloseIcon
+        color="#fff"
+        style={{ height: 32, width: 32, padding: 12 }}
+        className="OverlayHeaderClose-icon"
+      />
+    </div>
+  );
+};
 
-  render() {
-    const { className } = this.props;
-
-    return (
-      <div
-        className={cx('OverlayHeaderClose', className)}
-        onClick={closeAll}
-      >
-        <CloseIcon
-          color="#fff"
-          style={{ height: 32, width: 32, padding: 12 }}
-          className="OverlayHeaderClose-icon"
-        />
-      </div>
-    );
-  }
-}
+export default Close;
--- src/js/components/Overlay/Header/index.js
+++ src/js/components/Overlay/Header/index.js
@@ -1,28 +1,19 @@
 import cx from 'classnames';
 import React from 'react';
-import oneOrManyChildren from '../../../utils/propTypes/oneOrManyChildren';
 import CloseButton from './Close';
 
-export default class Header extends React.Component {
-  static propTypes = {
-    className: React.PropTypes.string,
-    title: React.PropTypes.string,
-    children: oneOrManyChildren
-  };
-
-  render() {
-    const { className, title, children } = this.props;
-
-    return (
-      <div className={cx('OverlayHeader', className)}>
-        <div className="OverlayHeader-title">
-          {title.toUpperCase()}
-        </div>
-        <div className="OverlayHeader-content">
-          {children}
-        </div>
-        <CloseButton className="OverlayHeader-close" />
+const Header = ({ className, title, children }) => {
+  return (
+    <div className={cx('OverlayHeader', className)}>
+      <div className="OverlayHeader-title">
+        {title.toUpperCase()}
       </div>
-    );
-  }
-}
+      <div className="OverlayHeader-content">
+        {children}
+      </div>
+      <CloseButton className="OverlayHeader-close" />
+    </div>
+  );
+};
+
+export default Header;
