f308ed26e6226c9fa2484a878ccd123cc97d532d
Renée Kooi <rene@kooi.me>
2015-11-05 17:37:34 +0100

    video: add fallback to SVG for blurred backdrop (fixes #32)

--- src/js/components/Video/VideoBackdrop.js
+++ src/js/components/Video/VideoBackdrop.js
@@ -1,12 +1,58 @@
 import React from 'react';
 
+const supportsBlur = window.CSS &&
+  (CSS.supports('filter', 'blur(1em)') || CSS.supports('-webkit-filter', 'blur(1em)'));
+
 export default class VideoBackdrop extends React.Component {
   static propTypes = {
     url: React.PropTypes.string
   };
 
-  render() {
+  makeSvgString(url) {
+    const width = 800;
+    const height = 480;
+    const blur = 20;
+    return `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+        viewBox="0 0 ${width} ${height}"
+      >
+        <defs>
+          <filter id="blur">
+            <feGaussianBlur stdDeviation="${blur} ${blur}" result="blur" />
+          </filter>
+        </defs>
+        <image
+          xlink:href="${url}"
+          x="${blur * -3}"
+          y="${blur * -3}"
+          filter="url(#blur)"
+          width="${width + blur * 6}"
+          height="${height + blur * 6}"
+        />
+      </svg>
+    `.replace(/\s+/g, ' ').trim();
+  }
+
+  renderBlur() {
     const { url } = this.props;
-    return <img className="VideoBackdrop" src={url} alt="" />;
+    return <img className="VideoBackdrop VideoBackdrop--blurry" src={url} alt="" />;
+  }
+
+  renderSvg() {
+    const { url } = this.props;
+    const svg = { __html: this.makeSvgString(url) };
+    return (
+      <div
+        className="VideoBackdrop VideoBackdrop--svg"
+        dangerouslySetInnerHTML={svg}
+      />
+    );
+  }
+
+  render() {
+    return supportsBlur ? this.renderBlur() : this.renderSvg();
   }
 }
