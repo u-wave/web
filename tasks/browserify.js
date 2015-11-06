913e7303c3fdd7e2c3ed59400e105d54b3e2f267
Renée Kooi <rene@kooi.me>
2015-11-06 17:05:17 +0100

    build: collapse require calls when minifying js

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -2,21 +2,29 @@ import { dest } from 'gulp';
 import babelify from 'babelify';
 import browserify from 'browserify';
 import buffer from 'gulp-buffer';
+import collapse from 'bundle-collapser/plugin';
 import envify from 'envify/custom';
 import source from 'vinyl-source-stream';
 import uglify from 'gulp-uglify';
 import when from 'gulp-if';
 
 export default function browserifyTask({ minify = false }) {
-  return browserify({
+  const b = browserify({
     debug: !minify,
     entries: './src/js/app.js'
-  })
-    .transform(babelify, { stage: 0 })
-    .transform(envify({
-      _: 'purge',
-      NODE_ENV: minify ? 'production' : 'development'
-    }), { global: true })
+  });
+
+  b.transform(babelify, { stage: 0 });
+  b.transform(envify({
+    _: 'purge',
+    NODE_ENV: minify ? 'production' : 'development'
+  }), { global: true });
+
+  if (minify) {
+    b.plugin(collapse);
+  }
+
+  return b
     .bundle()
     .pipe(source('out.js'))
     .pipe(when(minify, buffer()))
