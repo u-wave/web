3d3c833c7f76de6e6754ed8d303dcc4add1f06f4
Renée Kooi <rene@kooi.me>
2015-11-16 19:41:10 +0100

    settings: persist settings to localstorage, closes #43

--- src/stores/SettingsStore.js
+++ src/stores/SettingsStore.js
@@ -2,11 +2,29 @@ import assign from 'object-assign';
 import EventEmitter from 'eventemitter3';
 import dispatcher from '../dispatcher';
 
+const SETTINGS_KEY = 'uwaveSettings';
+
 const settings = {
   muted: false,
   videoSize: 'large',
   volume: 0
 };
+
+// cookie blocking safe localStorage settings persistence
+function load() {
+  try {
+    assign(settings, JSON.parse(localStorage.getItem(SETTINGS_KEY)));
+  } catch (e) {
+    // Ok!
+  }
+}
+function save() {
+  try {
+    localStorage.setItem(SETTINGS_KEY, JSON.stringify(settings));
+  } catch (e) {
+    // Ok!
+  }
+}
 
 const SettingsStore = assign(new EventEmitter, {
   getAll() {
@@ -28,5 +46,8 @@ const SettingsStore = assign(new EventEmitter, {
     }
   })
 });
+
+SettingsStore.on('change', save);
+load();
 
 export default SettingsStore;
