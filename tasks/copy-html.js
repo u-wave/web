ae0dc276c230278cb9f842eebe17de27ba60d184
Renée Kooi <rene@kooi.me>
2015-11-18 15:07:30 +0100

    build: prerender app by passing --prerender to `gulp` or `gulp html`

--- tasks/copy-html.js
+++ tasks/copy-html.js
@@ -1,9 +1,18 @@
 import { src, dest } from 'gulp';
+import { transform as replace } from 'gulp-insert';
 import minifyHtml from 'gulp-minify-html';
 import when from 'gulp-if';
 
-export default function copyHtmlTask({ minify = false }) {
+function renderApp() {
+  const React = require('react');
+  const { renderToString } = require('react-dom/server');
+  const App = require('../src/components/App');
+  return renderToString(<App />);
+}
+
+export default function copyHtmlTask({ minify = false, prerender = false }) {
   return src('src/index.html')
+    .pipe(when(prerender, replace(contents => contents.replace('<!-- App -->', renderApp))))
     .pipe(when(minify, minifyHtml()))
     .pipe(dest('lib/'));
 }
