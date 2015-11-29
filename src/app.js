776769dd9a4307aa70ea78afcbc966dbca9f37a5
Renée Kooi <rene@kooi.me>
2015-11-29 16:32:00 +0100

    stores: remove obsolete CurrentMediaStore.init() call from app start code

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import CurrentMediaStore from './stores/CurrentMediaStore';
 import * as Socket from './utils/Socket';
 import { init as restoreSession } from './utils/Session';
 import { initState } from './actions/LoginActionCreators';
@@ -17,7 +16,6 @@ restoreSession();
 initState();
 
 // temporary debug and testing things
-CurrentMediaStore.init();
 Socket.connect();
 
 window.debug = require('debug');
