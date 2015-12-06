25e249533208d254ad6e81395ae1df06795bb683
Renée Kooi <rene@kooi.me>
2015-12-06 16:35:45 +0100

    test: add mocha & chai

--- tasks/lint-js.js
+++ tasks/lint-js.js
@@ -2,7 +2,7 @@ import { src } from 'gulp';
 import eslint from 'gulp-eslint';
 
 export default function lintJsTask() {
-  return src([ 'src/**/*.js', 'gulpfile.babel.js', 'tasks/**/*.js' ])
+  return src([ 'src/**/*.js', 'gulpfile.babel.js', 'tasks/**/*.js', 'test/*.js' ])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
