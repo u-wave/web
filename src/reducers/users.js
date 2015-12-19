dd9dded24cbea5b66a6637640aeed71050d3cc5d
Renée Kooi <rene@kooi.me>
2015-12-19 17:30:43 +0100

    users: temp-fix race condition between user JOIN and LOAD
    Sometimes the LOAD event (from REST api) would come back after the
    JOIN event (from websocket) and override it. This resulted in the
    current user not existing in the Users state.
    
    This temp-fix just merges the LOAD event in instead of overriding.
    It might introduce even more out-of-sync issues later but it works
    for now ✌

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -9,7 +9,15 @@ export default function reduce(state = initialState, action = {}) {
   const { type, payload } = action;
   switch (type) {
   case LOAD:
-    return indexBy(payload.users, '_id');
+    // this is merged in instead of replacing the state, because sometimes the
+    // JOIN event from the current user comes in before the LOAD event, and then
+    // the current user is sometimes excluded from the state. it looks like this
+    // approach could cause problems, too, though.
+    // TODO maybe replace state instead anyway and merge in the current user?
+    return {
+      ...state,
+      ...indexBy(payload.users, '_id')
+    };
   case JOIN:
     return {
       ...state,
