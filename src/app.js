bc6cd32e741a69f30876ac06ec665f80a61461fc
Renée Kooi <rene@kooi.me>
2015-12-29 16:59:09 +0100

    doc: more comments in application setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+// Polyfills for browsers that might not yet support Promises or the Fetch API
+// (newer & better XMLHttpRequest).
 import 'es6-promise';
 import 'whatwg-fetch';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -12,26 +15,45 @@ import { startTicking } from './actions/TickerActionCreators';
 
 import configureStore from './store/configureStore';
 
+// The Store holds all of the application state. The @connect calls in the React
+// Container components use this store to access state.
 const store = configureStore();
 
+// Check if we have a previous login session going.
 const jwt = readSession();
 if (jwt) {
   store.dispatch(setJWT(jwt));
 }
 
+// Load application state like the current DJ, online users, etc.
 store.dispatch(initState());
+
+// Start the clock! üWave stores the current time in the application state
+// primarily to make sure that different timers in the UI update simultaneously.
 store.dispatch(startTicking());
 
+// WebSocket connections! It uses the `store` object to dispatch actions
+// received from the üWave server.
 Socket.connect(store);
 
-// Material-UI dependency
+// A Material-UI dependency, removes the delay from tap events on some mobile
+// devices. üWave currently isn't compatible with mobile yet, but material-ui
+// wants this!
 require('react-tap-event-plugin')();
 
+// Render üWave! 😱
 ReactDOM.render(
+  // <Provider> does some magic to make all of the @connect calls in the React
+  // Container components work. This is how React Components find the
+  // application state that they need to render.
   <Provider store={store}>
     <AppContainer />
   </Provider>,
   document.getElementById('app')
 );
 
+// This exposes a global `debug.enable()` function that you can call to get some
+// extra debug output.
+// Usually you'll want to do `debug.enable('uwave:*')` and then refresh the
+// page.
 window.debug = require('debug');
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,25 +7,51 @@ import { TICK } from '../constants/actionTypes/time';
 import persistSettings from './persistSettings';
 import * as reducers from '../reducers';
 
+// Setting up a store in Redux can be kind of messy because there are a lot of
+// things coming together in one place. Luckily, we don't have _that_ much going
+// on in üWave, so it's kind of manageable.
+
 export default function createUwaveStore() {
   const middleware = [
-    // for async actions
+    // Redux-Thunk allows dispatching a function to the store instead of an
+    // action object. These functions can then dispatch action objects as they
+    // please. It's used primarily for async actions: for example, actions that
+    // send HTTP requests. Those might dispatch an action object once the
+    // request finishes.
     thunk,
+    // Redux-Logger logs state changes to the console, including the
+    // Before-state, the Action object, and the After-state. Invaluable for
+    // debugging :)
     logger({
-      // avoid log spam
+      // üWave has a Time store that changes every second with the TICK action,
+      // but we don't need to see that happening all the time. This hides the
+      // TICK actions from the logs.
       predicate: (getState, action) => action.type !== TICK
     })
   ];
 
+  // Creating a function to create a store! What a time to be alive :)
   const createStoreWithMiddleware = compose(
+    // Adds all of the above ☝ features to the store.
     applyMiddleware(...middleware),
+    // Keeps the user's settings in localStorage, so that a refresh doesn't
+    // reset all your preferences.
+    // This is done separately from the Middleware features, because it changes
+    // the _initial_ `settings` state, something that Middleware can't do.
     persistSettings
   )(createStore);
+
+  // Yes! We made it :)
   const store = createStoreWithMiddleware(
+    // Finish up the reducer function by combining all the different reducers
+    // into one big reducer that works on one big state object.
     combineReducers(reducers)
   );
 
   if (module.hot) {
+    // Update the store's reducer function when the reducer source code has
+    // changed. See /tasks/watch.js for more on Hot Reloading!
+    // This is only used when debugging, not in a deployed app.
     module.hot.accept('../reducers', () => {
       store.replaceReducer(combineReducers(
         require('../reducers')
