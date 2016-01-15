6ee1c95f48b20220bd62cdddd6cc28073fcf934c
Renée Kooi <rene@kooi.me>
2016-01-15 22:58:37 +0100

    booth: fix progress bar when booth is empty
    Previously, an empty booth would set the progress bar size
    to "Infinity%", which caused browsers to keep it at its previous
    position.
    
    This patch moves the progress computation to a selector, which is
    where computations are supposed to happen in the first place. It
    also deals with media durations of 0 (and thus empty booths) by
    defaulting the progress bar width to "0%".

--- src/containers/HeaderBar.js
+++ src/containers/HeaderBar.js
@@ -4,17 +4,14 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { setVolume, mute, unmute } from '../actions/PlaybackActionCreators';
 
-import { djSelector, startTimeSelector, mediaSelector, mediaDurationSelector } from '../selectors/boothSelectors';
-import { currentTimeSelector } from '../selectors/timeSelectors';
+import { djSelector, mediaSelector, mediaProgressSelector } from '../selectors/boothSelectors';
 import { volumeSelector, isMutedSelector } from '../selectors/settingSelectors';
 import HeaderBar from '../components/HeaderBar';
 
 const mapStateToProps = createStructuredSelector({
-  mediaStartTime: startTimeSelector,
-  mediaDuration: mediaDurationSelector,
+  mediaProgress: mediaProgressSelector,
   media: mediaSelector,
   dj: djSelector,
-  currentTime: currentTimeSelector,
   volume: volumeSelector,
   muted: isMutedSelector
 });
