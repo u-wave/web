fd5af25ff5650e69df670bcefa216293035ced67
Renée Kooi <rene@kooi.me>
2016-01-17 23:50:15 +0100

    settings: add cog icon to settings button

--- src/components/FooterBar/UserInfo.js
+++ src/components/FooterBar/UserInfo.js
@@ -1,14 +1,28 @@
 import cx from 'classnames';
 import React from 'react';
+import SettingsIcon from 'material-ui/lib/svg-icons/action/settings';
+
 import Avatar from '../Avatar';
 
 const UserInfo = ({ className, user, ...attrs }) => {
   return (
-    <div className={cx('UserInfo', className)} {...attrs}>
+    <div
+      className={cx('UserInfo', className)}
+      {...attrs}
+    >
       <Avatar
         className="UserInfo-avatar"
         user={user}
       />
+      <div className="UserInfo-settings">
+        <SettingsIcon
+          color={'#fff'}
+          style={{
+            width: '100%',
+            height: '100%'
+          }}
+        />
+      </div>
     </div>
   );
 };
