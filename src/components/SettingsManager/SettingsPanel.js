e6dd27803031f262f0b42b1a79b1fd66def76b61
Renée Kooi <rene@kooi.me>
2016-01-19 22:23:44 +0100

    settings: add links to Github, Licence

--- src/components/SettingsManager/SettingsPanel.js
+++ src/components/SettingsManager/SettingsPanel.js
@@ -1,8 +1,28 @@
 import cx from 'classnames';
 import React, { Component, PropTypes } from 'react';
 import Toggle from 'material-ui/lib/toggle';
+import FlatButton from 'material-ui/lib/flat-button';
+import LicenseIcon from 'material-ui/lib/svg-icons/action/copyright';
 
+import GithubIcon from './GithubIcon';
 import Profile from './Profile';
+
+const linkStyle = {
+  display: 'block',
+  height: 24,
+  lineHeight: '24px',
+  marginBottom: 20
+};
+const iconStyle = { verticalAlign: 'top' };
+
+const linkProps = {
+  style: linkStyle,
+  linkButton: true,
+  target: '_blank',
+  labelPosition: 'after',
+  hoverColor: 'transparent',
+  textTransform: 'none'
+};
 
 export default class SettingsPanel extends Component {
   static propTypes = {
@@ -51,7 +71,27 @@ export default class SettingsPanel extends Component {
           )}
         </div>
         <div className="SettingsPanel-column SettingsPanel-column--right">
-          [Links to stuff]
+          <FlatButton
+            href="https://github.com/u-wave"
+            label="üWave"
+            {...linkProps}
+          >
+            <GithubIcon style={iconStyle} />
+          </FlatButton>
+          <FlatButton
+            href="https://github.com/u-wave/u-wave-web"
+            label="üWave Web Client Source Code"
+            {...linkProps}
+          >
+            <GithubIcon style={iconStyle} />
+          </FlatButton>
+          <FlatButton
+            href="https://github.com/u-wave/u-wave-web/tree/master/LICENSE"
+            label="License"
+            {...linkProps}
+          >
+            <LicenseIcon style={iconStyle} />
+          </FlatButton>
         </div>
       </div>
     );
