0b797aa59d6f7687d45a8eb8441451d39e6efdcf
Renée Kooi <rene@kooi.me>
2016-01-25 22:30:54 +0100

    mod: add actions for deleting chat messages (by id, user, all)

--- src/actions/ModerationActionCreators.js
+++ src/actions/ModerationActionCreators.js
@@ -1,10 +1,14 @@
-import { post } from '../utils/Request';
+import { del, post } from '../utils/Request';
 import { djSelector } from '../selectors/boothSelectors';
 import { tokenSelector } from '../selectors/userSelectors';
 
 import {
   SKIP_DJ_START, SKIP_DJ_COMPLETE
 } from '../constants/actionTypes/moderation';
+
+import {
+  removeMessage, removeMessagesByUser, removeAllMessages
+} from './ChatActionCreators';
 
 export function skipCurrentDJ(reason = '') {
   return (dispatch, getState) => {
@@ -29,6 +33,50 @@ export function skipCurrentDJ(reason = '') {
         error: true,
         payload: error,
         meta: payload
+      }));
+  };
+}
+
+export function deleteChatMessage(id) {
+  return (dispatch, getState) => {
+    const jwt = tokenSelector(getState());
+    del(jwt, `/v1/chat/${id}`)
+      .then(res => res.json())
+      .then(() => dispatch(removeMessage(id)))
+      .catch(error => dispatch({
+        type: undefined,
+        error: true,
+        payload: error,
+        meta: { id }
+      }));
+  };
+}
+
+export function deleteChatMessagesByUser(userID) {
+  return (dispatch, getState) => {
+    const jwt = tokenSelector(getState());
+    del(jwt, `/v1/chat/user/${userID}`)
+      .then(res => res.json())
+      .then(() => dispatch(removeMessagesByUser(userID)))
+      .catch(error => dispatch({
+        type: undefined,
+        error: true,
+        payload: error,
+        meta: { userID }
+      }));
+  };
+}
+
+export function deleteAllChatMessages() {
+  return (dispatch, getState) => {
+    const jwt = tokenSelector(getState());
+    del(jwt, `/v1/chat`)
+      .then(res => res.json())
+      .then(() => dispatch(removeAllMessages()))
+      .catch(error => dispatch({
+        type: undefined,
+        error: true,
+        payload: error
       }));
   };
 }
