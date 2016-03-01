ff2335c6099f4b0cfb4a0ba04475abddb1633adf
Renée Kooi <rene@kooi.me>
2016-03-01 20:19:14 +0100

    test: use a real store in addToPlaylistMenu reducer tests

--- test/reducers/addToPlaylistMenu.js
+++ test/reducers/addToPlaylistMenu.js
@@ -1,60 +1,47 @@
 import { expect } from 'chai';
-import { OPEN_ADD_MEDIA_MENU } from '../../src/constants/actionTypes/playlists';
-import { closeAddMediaMenu } from '../../src/actions/PlaylistActionCreators';
-import addToPlaylistMenu from '../../src/reducers/addToPlaylistMenu';
+
+import createStore from '../../src/store/configureStore';
+import {
+  addMediaMenu, closeAddMediaMenu
+} from '../../src/actions/PlaylistActionCreators';
+import * as s from '../../src/selectors/addToPlaylistMenuSelectors';
 
 describe('reducers/addToPlaylistMenu', () => {
-  const initialState = () => addToPlaylistMenu(undefined, { type: '@@redux/INIT' });
   it('should default to a closed context menu', () => {
-    const state = addToPlaylistMenu(undefined, { type: '@@redux/INIT' });
-    expect(state).to.eql({
+    const { getState } = createStore();
+    expect(s.addToPlaylistMenuSelector(getState())).to.eql({
+      type: null,
       open: false,
       position: { x: 0, y: 0 },
-      playlists: [],
-      type: null,
-      data: null
+      playlists: []
     });
   });
 
   describe('action: playlists/OPEN_ADD_MEDIA_MENU', () => {
+    const { dispatch, getState } = createStore();
     it('should open the menu at the given position with the given media', () => {
-      let state = initialState();
-      state = addToPlaylistMenu(state, {
-        type: OPEN_ADD_MEDIA_MENU,
-        payload: {
-          media: [ { _id: 'mmedia' } ]
-        },
-        meta: {
-          type: 'favorite',
-          position: { x: 800, y: 300 },
-          playlists: [ { _id: 'pplaylist' } ]
-        }
-      });
-      expect(state.data).to.eql({
-        media: [ { _id: 'mmedia' } ]
-      });
-      expect(state.open).to.be.true;
-      expect(state.type).to.equal('favorite');
-      expect(state.position).to.eql({ x: 800, y: 300 });
-      expect(state.playlists).to.eql([ { _id: 'pplaylist' } ]);
+      dispatch(addMediaMenu(
+        [ { _id: 'mmedia' } ],
+        { x: 800, y: 300 }
+      ));
+      expect(s.mediaSelector(getState())).to.eql([ { _id: 'mmedia' } ]);
+      expect(s.isOpenSelector(getState())).to.be.true;
+      expect(s.isFavoriteSelector(getState())).to.be.false;
+      expect(s.positionSelector(getState())).to.eql({ x: 800, y: 300 });
     });
   });
 
-  describe('action: overlay/CLOSE_ADD_MEDIA_MENU', () => {
+  describe('action: playlists/CLOSE_ADD_MEDIA_MENU', () => {
+    const { dispatch, getState } = createStore();
     it('should close the menu', () => {
-      let state = {
-        open: true,
-        position: { x: 800, y: 300 },
-        playlists: [],
-        type: 'add',
-        data: {
-          media: [ { _id: 'hoi' } ]
-        }
-      };
-      state = addToPlaylistMenu(state, closeAddMediaMenu());
-      expect(state.open).to.be.false;
-      expect(state.playlists).to.have.length(0);
-      expect(state.data).to.be.null;
+      dispatch(addMediaMenu(
+        [ { _id: 'mmedia' } ],
+        { x: 800, y: 300 }
+      ));
+      expect(s.isOpenSelector(getState())).to.be.true;
+
+      dispatch(closeAddMediaMenu());
+      expect(s.isOpenSelector(getState())).to.be.false;
     });
   });
 });
