d863f50ed9b648f2bc71b673ea9de1e680c21d7b
Renée Kooi <rene@kooi.me>
2016-03-26 14:18:35 +0100

    deps: include play-audio and new-chain

---
+++ .temp-unpublished-node_modules/new-chain/index.js
@@ -0,0 +1,55 @@
+module.exports = newChain;
+module.exports.from = from;
+
+function from(chain){
+
+  return function(){
+    var m, i;
+
+    m = methods.apply(undefined, arguments);
+    i   = m.length;
+
+    while ( i -- ) {
+      chain[ m[i].name ] = m[i].fn;
+    }
+
+    m.forEach(function(method){
+      chain[ method.name ] = function(){
+        method.fn.apply(this, arguments);
+        return chain;
+      };
+    });
+
+    return chain;
+  };
+
+}
+
+function methods(){
+  var all, el, i, len, result, key;
+
+  all    = Array.prototype.slice.call(arguments);
+  result = [];
+  i      = all.length;
+
+  while ( i -- ) {
+    el = all[i];
+
+    if ( typeof el == 'function' ) {
+      result.push({ name: el.name, fn: el });
+      continue;
+    }
+
+    if ( typeof el != 'object' ) continue;
+
+    for ( key in el ) {
+      result.push({ name: key, fn: el[key] });
+    }
+  }
+
+  return result;
+}
+
+function newChain(){
+  return from({}).apply(undefined, arguments);
+}
