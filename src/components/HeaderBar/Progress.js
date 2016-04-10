ac3395a5d8bbb6104c0ad0a012fdf4da511c2253
Renée Kooi <rene@kooi.me>
2016-04-10 12:45:49 +0200

    style: correctly vendor-prefix transform styles

--- src/components/HeaderBar/Progress.js
+++ src/components/HeaderBar/Progress.js
@@ -1,16 +1,15 @@
 import cx from 'classnames';
 import React from 'react';
 
+import transformStyle from '../../utils/transformStyle';
+
 const Progress = ({ className, percent }) => {
   const width = isFinite(percent) ? percent : 0;
   return (
     <div className={cx('Progress', className)}>
       <div
         className="Progress-fill"
-        style={{
-          transform: `scaleX(${width})`,
-          webkitTransform: `scaleX(${width})`
-        }}
+        style={transformStyle(`scaleX(${width})`)}
       />
     </div>
   );
