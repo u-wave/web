00213c9313c011d17b6512f04e7f4d85b6adec61
Renée Kooi <rene@kooi.me>
2016-04-28 12:22:28 +0200

    middleware: inject app config into html, fixes #217

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,30 @@
+import { load as loadHtml } from 'cheerio';
+import * as path from 'path';
+import { readFile } from 'fs';
 import router from 'router';
 import serveStatic from 'serve-static';
 
+function injectConfig(html, config) {
+  const $ = loadHtml(html);
+  $('#u-wave-config').text(JSON.stringify(config));
+  return $.html();
+}
+
 export default function uwaveWebClient(uw, options = {}) {
-  const basePath = options.basePath !== undefined ? options.basePath : __dirname;
+  const {
+    basePath = __dirname,
+    ...clientOptions
+  } = options;
 
   return router()
+    .get('/', (req, res, next) => {
+      readFile(path.join(basePath, 'index.html'), 'utf8', (err, content) => {
+        if (err) {
+          next();
+          return;
+        }
+        res.end(injectConfig(content, clientOptions));
+      });
+    })
     .use(serveStatic(basePath));
 }
