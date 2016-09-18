71410ab3c461f1df1ac6f39514a2735f48771bd3
Renée Kooi <renee@kooi.me>
2016-09-18 12:56:29 +0200

    locale: add translations for "time ago" in play history

--- src/components/TimeAgo/index.js
+++ src/components/TimeAgo/index.js
@@ -5,11 +5,29 @@ import ms from 'ms';
 
 import timed from '../../utils/timed';
 
+// Bit weird to do it like this perhaps, convert to an english string first and
+// then translate afterwards.
+function translateMs(str) {
+  // `ms` output of the form "3 hours"
+  const [ count, key ] = str.split(' ');
+  return {
+    key: `${key.replace(/s$/, '')}s`,
+    count: parseInt(count, 10)
+  };
+}
+
 const TimeAgo = ({ t, currentTime, timestamp }) => {
   const secondsAgo = Math.max(0, Math.floor((currentTime - timestamp) / 1000));
+  const msString = ms(secondsAgo * 1000, { long: true });
+  const { key, count } = translateMs(msString);
   return (
     <span>
-      {t('timeAgo', { time: ms(secondsAgo * 1000, { long: true }) })}
+      {t('timeAgo.format', {
+        time: t(`timeAgo.${key}`, {
+          count,
+          defaultValue: msString
+        })
+      })}
     </span>
   );
 };
