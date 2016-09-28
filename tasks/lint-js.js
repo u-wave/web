62672e32039f104a3c9b461f9edf93531fff16f4
Renée Kooi <renee@kooi.me>
2016-09-28 12:18:07 +0200

    build: use npm-run-path's env() function

--- tasks/lint-js.js
+++ tasks/lint-js.js
@@ -3,9 +3,7 @@ import npmRunPath from 'npm-run-path';
 
 export default function lintJsTask() {
   return new Promise((resolve, reject) => {
-    const env = {
-      PATH: npmRunPath()
-    };
+    const env = npmRunPath.env();
     exec('eslint --cache --color .', { env }, (error, stdout) => {
       if (error) {
         console.error(stdout);
