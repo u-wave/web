7c200196511c561a0c2aeffbe8912f28aa7d246c
Renée Kooi <renee@kooi.me>
2016-11-08 13:49:29 +0100

    remove bluebird-removing hacks. youtube-player uses native promises now

--- src/utils/Promise.js
+++
@@ -1,7 +0,0 @@
-// This file is used instead of Bluebird by dependencies.
-// See /tasks/browserify.js for more.
-
-// Expose Promise as a default export. This module works with dependencies that
-// expect Bluebird: Bluebird exports a Promise object as "module.exports", but
-// es6-promise exports a `.Promise` property instead.
-export { Promise as default } from 'es6-promise';
