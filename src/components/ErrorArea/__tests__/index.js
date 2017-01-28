0ac5d2e42e1ba9baa47dad404901e230b2ce6c29
Renée Kooi <renee@kooi.me>
2017-01-29 00:21:13 +0100

    test: Add tests for <ErrorArea/> component

---
+++ src/components/ErrorArea/__tests__/index.js
@@ -0,0 +1,59 @@
+/* eslint-env mocha */
+/* eslint-disable import/no-extraneous-dependencies */
+/* global testUtils */
+// TODO figure out how to make the above apply automatically in test files.
+import * as React from 'react';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import ErrorArea, { errorThemeSelector } from '../';
+
+describe('<ErrorArea />', () => {
+  it('should not show if there is no error', () => {
+    expect(
+      shallow(
+        <ErrorArea
+          error={null}
+          onDismiss={() => {}}
+        />,
+        { context: testUtils.context }
+      ).dive().childAt(0)
+    ).to.have.prop('open', false);
+  });
+
+  it('should open if there is an error', () => {
+    expect(
+      shallow(
+        <ErrorArea
+          error="Something is WRONG!"
+          onDismiss={() => {}}
+        />,
+        { context: testUtils.context }
+      ).dive().childAt(0)
+    ).to.have.prop('open', true);
+  });
+
+  it('uses the error notification palette overrides', () => {
+    const notificationTheme = testUtils.muiTheme.palette.notifications;
+
+    const contextTheme = errorThemeSelector(testUtils.context).muiTheme;
+
+    expect(contextTheme.snackbar).to.have.property('textColor', notificationTheme.errorTextColor);
+    expect(contextTheme.snackbar).to.have.property('backgroundColor', notificationTheme.errorBackgroundColor);
+  });
+
+  it('closes when user clicks anywhere on the page', () => {
+    const spy = sinon.spy();
+
+    const area = shallow((
+      <ErrorArea
+        error="Message"
+        onDismiss={spy}
+      />
+    ), { context: testUtils.context }).dive();
+
+    area.childAt(0).prop('onRequestClose').call();
+
+    expect(spy.calledOnce).to.equal(true);
+  });
+});
