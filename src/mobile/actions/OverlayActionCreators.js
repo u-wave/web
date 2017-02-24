8bc256243d395705dc3ded934f0a9d22135a479a
Renée Kooi <renee@kooi.me>
2017-02-24 20:31:08 +0100

    Fix lint.

--- src/mobile/actions/OverlayActionCreators.js
+++ src/mobile/actions/OverlayActionCreators.js
@@ -1,6 +1,7 @@
 import { togglePlaylistManager } from '../../actions/OverlayActionCreators';
 import { selectPlaylist } from '../../actions/PlaylistActionCreators';
 
+// eslint-disable-next-line import/prefer-default-export
 export function openPlaylist(playlistID) {
   return (dispatch) => {
     dispatch(togglePlaylistManager());
