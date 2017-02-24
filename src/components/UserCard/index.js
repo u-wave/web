c8b40d061de6e2d5377b38daa6e52185093b8d5d
Renée Kooi <renee@kooi.me>
2017-02-24 19:59:36 +0100

    users: Fix user cards overflowing window boundary, closes #469

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -2,24 +2,70 @@ import * as React from 'react';
 import RenderToLayer from 'material-ui/internal/RenderToLayer';
 import UserCard from './UserCard';
 
-const UserCardWrapper = ({ onClose, position, ...props }) => (
-  <RenderToLayer
-    open
-    componentClickAway={onClose}
-    render={() => (
-      <div style={{ position: 'absolute', left: position.x, top: position.y }}>
-        <UserCard {...props} />
-      </div>
-    )}
-  />
-);
+class UserCardWrapper extends React.Component {
+  static propTypes = {
+    onClose: React.PropTypes.func.isRequired,
+    position: React.PropTypes.shape({
+      x: React.PropTypes.number.isRequired,
+      y: React.PropTypes.number.isRequired
+    }).isRequired
+  };
 
-UserCardWrapper.propTypes = {
-  onClose: React.PropTypes.func.isRequired,
-  position: React.PropTypes.shape({
-    x: React.PropTypes.number.isRequired,
-    y: React.PropTypes.number.isRequired
-  }).isRequired
-};
+  state = {
+    positionDiffX: 0,
+    positionDiffY: 0
+  };
+
+  componentDidMount() {
+    this.fitInsideWindow();
+  }
+
+  componentDidUpdate() {
+    this.fitInsideWindow();
+  }
+
+  fitInsideWindow() {
+    const card = this.container.firstChild;
+    const rect = card.getBoundingClientRect();
+    const offsetBottom = window.innerHeight - rect.bottom;
+    if (offsetBottom < 0) {
+      this.setState({
+        positionDiffY: offsetBottom
+      });
+    }
+  }
+
+  refContainer = (container) => {
+    this.container = container;
+  };
+
+  render() {
+    const {
+      onClose,
+      position,
+      ...props
+    } = this.props;
+    const { positionDiffX, positionDiffY } = this.state;
+
+    return (
+      <RenderToLayer
+        open
+        componentClickAway={onClose}
+        render={() => (
+          <div
+            style={{
+              position: 'absolute',
+              left: position.x + positionDiffX,
+              top: position.y + positionDiffY
+            }}
+            ref={this.refContainer}
+          >
+            <UserCard {...props} />
+          </div>
+        )}
+      />
+    );
+  }
+}
 
 export default UserCardWrapper;
