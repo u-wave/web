880c4e0d425580f14c3d060d6b315dc91c841dd0
Renée Kooi <renee@kooi.me>
2017-03-11 21:07:05 +0100

    Add static page compilation and a Privacy Policy page. (#570)

---
+++ static/privacy.md
@@ -0,0 +1,25 @@
+# ![üWave](../assets/img/logo-white.png) Privacy Policy
+
+üWave attempts to store only the bare minimum of data necessary to make the site
+easy to use.
+
+## Information About You
+
+üWave may log the following information about you when you visit the site:
+
+ - Your IP address and your "User-Agent", i.e. the browser and browser version
+   you are using, as well as the time of your visit.
+
+üWave stores the following information about you when you create a user account:
+
+ - Your username and email address;
+ - Your encrypted password.
+
+It is not possible to delete your account yourself at this time. If you wish to
+delete this data, please ask the administrators.
+
+## Information Stored on Your Computer
+
+When you log in to üWave, the site stores a "cookie" on your computer containing
+a unique token. This is used to log you in again automatically if you visit the
+site again on the same computer.
---
+++ tasks/utils/loadStaticHtmlTemplate.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const cheerio = require('cheerio');
+const createTemplate = require('lodash/template');
+
+/**
+ * A Webpack loader that creates a template for html-webpack-plugin to render
+ * a static HTML page.
+ */
+module.exports = function loadStaticHtmlTemplate(source) {
+  const cb = this.async();
+  const parentTemplate = require.resolve('../../src/markdown.prod.html');
+
+  // Extract the likely title from the document.
+  const $ = cheerio.load(source);
+  const title = $('h1').first().text().trim();
+
+  fs.readFile(parentTemplate, 'utf8', (err, templateSource) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    // Rest of this is similar to the default html-webpack-plugin loader.
+    // https://github.com/jantimon/html-webpack-plugin/blob/b8fd1427bc7714f0b37e1ef8ed9e4477a7bbafcd/lib/loader.js
+    const template = createTemplate(templateSource);
+
+    cb(null, `
+      module.exports = (data) => {
+        const title = ${JSON.stringify(title)};
+        const contents = ${JSON.stringify(source)};
+        const compilation = data.compilation;
+        const webpack = data.webpack;
+        const webpackConfig = data.webpackConfig;
+        const htmlWebpackPlugin = data.htmlWebpackPlugin;
+
+        return (${template.source})();
+      };
+    `);
+  });
+};
