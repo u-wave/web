c702f99bddc9ed1ea789053d995cb82f11914bc1
Renée Kooi <renee@kooi.me>
2017-03-13 18:42:02 +0100

    Mock asset file imports for tests.

--- test/common.js
+++ test/common.js
@@ -2,3 +2,13 @@ require('babel-register')({
   plugins: [ 'transform-es2015-modules-commonjs' ]
 });
 require('yamlify/register');
+
+// Mock an asset like Webpack's file-loader.
+function mockAsset(modulePath) {
+  const path = require.resolve(modulePath);
+  require.cache[path] = {
+    exports: path
+  };
+}
+
+mockAsset('../assets/audio/mention.mp3');
