301e47266a67dcbb9ee4ef6e82ed9ccf44b0e57c
Renée Kooi <renee@kooi.me>
2017-04-10 18:56:32 +0200

    build: Transpile ES2015+ dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -184,6 +184,23 @@ module.exports = {
         test: /\.json$/,
         use: [ 'json-loader' ]
       },
+      // JS loader for dependencies that use ES2015+:
+      {
+        test: /\.js$/,
+        include: [
+          /truncate-url/
+        ],
+        use: {
+          loader: 'babel-loader',
+          query: {
+            babelrc: false,
+            presets: [
+              [ 'latest', { es2015: { modules: false } } ]
+            ]
+          }
+        }
+      },
+      // JS loader for our own code:
       {
         test: /\.js$/,
         exclude: /node_modules/,
