eeb58690a86c6fb93df6d2fa3f3751758b2289f1
Renée Kooi <renee@kooi.me>
2017-04-17 13:09:20 +0200

    fixup! mobile: Add white logo to manifest.

--- tasks/utils/ExtractManifestPlugin.js
+++ tasks/utils/ExtractManifestPlugin.js
@@ -1,28 +1,26 @@
-module.exports = class ExtractManifestPlugin {
-  constructor(opts) {
-    this.opts = opts;
-  }
+module.exports = function ExtractManifestPlugin(opts) {
+  return {
+    apply(compiler) {
+      const evil = eval; // eslint-disable-line no-eval
 
-  apply(compiler) {
-    const evil = eval; // eslint-disable-line no-eval
+      compiler.plugin('emit', (compilation, cb) => {
+        const filename = compilation.namedChunks[opts.chunkName].files[0];
+        const manifest = evil(compilation.assets[filename].source()).default;
 
-    compiler.plugin('emit', (compilation, cb) => {
-      const filename = compilation.namedChunks[this.opts.chunkName].files[0];
-      const manifest = evil(compilation.assets[filename].source()).default;
+        const json = opts.minimize
+          ? JSON.stringify(manifest)
+          : JSON.stringify(manifest, null, 2);
 
-      const json = this.opts.minimize
-        ? JSON.stringify(manifest)
-        : JSON.stringify(manifest, null, 2);
+        // eslint-disable-next-line no-param-reassign
+        delete compilation.assets[filename];
+        // eslint-disable-next-line no-param-reassign
+        compilation.assets['manifest.json'] = {
+          source: () => json,
+          size: () => json.length
+        };
 
-      // eslint-disable-next-line no-param-reassign
-      delete compilation.assets[filename];
-      // eslint-disable-next-line no-param-reassign
-      compilation.assets['manifest.json'] = {
-        source: () => json,
-        size: () => json.length
-      };
-
-      cb();
-    });
-  }
+        cb();
+      });
+    }
+  };
 };
