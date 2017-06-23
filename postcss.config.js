54b238af188d62ddf9a1b8e7ee89ebfa694f82e2
Renée Kooi <renee@kooi.me>
2017-06-23 13:35:04 +0200

    build: Fix postcss config for node v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,16 @@
-module.exports = ({ env }) => ({
-  plugins: {
-    'postcss-import': {},
-    'postcss-url': {
-      url: 'rebase'
-    },
-    'postcss-cssnext': {},
-    cssnano: env === 'production' && {
-      autoprefixer: false
+module.exports = (options) => {
+  const env = options.env;
+
+  return {
+    plugins: {
+      'postcss-import': {},
+      'postcss-url': {
+        url: 'rebase'
+      },
+      'postcss-cssnext': {},
+      cssnano: env === 'production' && {
+        autoprefixer: false
+      }
     }
-  }
-});
+  };
+};
