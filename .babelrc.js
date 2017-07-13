ed838637c3b05bca1b64262fd0f4ef1e54e9ec1b
Renée Kooi <renee@kooi.me>
2017-07-13 20:54:35 +0200

    Work on tests (#705)
    * No colocating tests
    
    * test: Add tests for `mergeIncludedModels`.
    
    * test: Target current Node version with Babel
    
    * test: Add tests for YouTube <Player/> component

--- .babelrc.js
+++ .babelrc.js
@@ -8,6 +8,9 @@ if (browsers) {
 if (env === 'production') {
   targets.uglify = true;
 }
+if (env === 'testing') {
+  targets.node = 'current';
+}
 
 const preset = {
   presets: [
@@ -18,7 +21,7 @@ const preset = {
       // Force enable the classes transform, react-hot-loader doesn't
       // appear to work well with native classes + arrow functions in
       // transpiled class properties.
-      include: ['transform-es2015-classes']
+      include: env === 'development' ? ['transform-es2015-classes'] : []
     }],
     'stage-2',
     'react'
