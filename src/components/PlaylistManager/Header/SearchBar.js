c36b445d736ddd7c7f5e9f1bf5dbd80ebabde9c1
Renée Kooi <renee@kooi.me>
2017-07-14 15:12:17 +0200

    Extract SearchBar component. (#708)

--- src/components/PlaylistManager/Header/SearchBar.js
+++ src/components/PlaylistManager/Header/SearchBar.js
@@ -1,68 +1,28 @@
-import cx from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { translate } from 'react-i18next';
-import SearchIcon from 'material-ui/svg-icons/action/search';
+import BaseSearchBar from '../../SearchBar';
 import SourcePicker from './SourcePicker';
 
-const enhance = translate();
+const SearchBar = ({
+  className,
+  source,
+  onSubmit,
+  onSourceChange
+}) => (
+  <BaseSearchBar className={className} onSubmit={onSubmit}>
+    <SourcePicker
+      className="SearchBar-source"
+      selected={source}
+      onChange={onSourceChange}
+    />
+  </BaseSearchBar>
+);
 
-class SearchBar extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    source: PropTypes.string,
-    onSubmit: PropTypes.func,
-    t: PropTypes.func.isRequired,
-    onSourceChange: PropTypes.func
-  };
+SearchBar.propTypes = {
+  className: PropTypes.string,
+  source: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onSourceChange: PropTypes.func.isRequired
+};
 
-  state = { focused: false };
-
-  handleFocus = () => {
-    this.setState({ focused: true });
-  };
-  handleBlur = () => {
-    this.setState({ focused: false });
-  };
-
-  handleKeyDown = (e) => {
-    const { onSubmit } = this.props;
-    if (e.key === 'Enter') {
-      onSubmit(this.input.value);
-    }
-  };
-
-  refInput = (input) => {
-    this.input = input;
-  };
-
-  render() {
-    const { t, source, onSourceChange } = this.props;
-    const { focused } = this.state;
-    return (
-      <div className={cx('SearchBar', focused ? 'is-focused' : '', this.props.className)}>
-        <div className="SearchBar-icon">
-          <SearchIcon color="#fff" />
-        </div>
-        <SourcePicker
-          className="SearchBar-source"
-          selected={source}
-          onChange={onSourceChange}
-        />
-        <div className="SearchBar-query">
-          <input
-            ref={this.refInput}
-            className="SearchBar-input"
-            type="text"
-            placeholder={focused ? '' : t('playlists.search.action')}
-            onFocus={this.handleFocus}
-            onBlur={this.handleBlur}
-            onKeyDown={this.handleKeyDown}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-export default enhance(SearchBar);
+export default SearchBar;
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,62 @@
+import cx from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { translate } from 'react-i18next';
+import SearchIcon from 'material-ui/svg-icons/action/search';
+
+const enhance = translate();
+
+class SearchBar extends React.Component {
+  static propTypes = {
+    t: PropTypes.func.isRequired,
+    className: PropTypes.string,
+    children: PropTypes.node,
+    onSubmit: PropTypes.func.isRequired
+  };
+
+  state = { focused: false };
+
+  handleFocus = () => {
+    this.setState({ focused: true });
+  };
+  handleBlur = () => {
+    this.setState({ focused: false });
+  };
+
+  handleKeyDown = (e) => {
+    const { onSubmit } = this.props;
+    if (e.key === 'Enter') {
+      onSubmit(this.input.value);
+    }
+  };
+
+  refInput = (input) => {
+    this.input = input;
+  };
+
+  render() {
+    const { t, children } = this.props;
+    const { focused } = this.state;
+    return (
+      <div className={cx('SearchBar', focused ? 'is-focused' : '', this.props.className)}>
+        <div className="SearchBar-icon">
+          <SearchIcon color="#fff" />
+        </div>
+        {children}
+        <div className="SearchBar-query">
+          <input
+            ref={this.refInput}
+            className="SearchBar-input"
+            type="text"
+            placeholder={focused ? '' : t('playlists.search.action')}
+            onFocus={this.handleFocus}
+            onBlur={this.handleBlur}
+            onKeyDown={this.handleKeyDown}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default enhance(SearchBar);
