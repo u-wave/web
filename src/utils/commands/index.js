c368a9b85e09d912d1765a2f40ff5ca168f9d11b
Renée Kooi <renee@kooi.me>
2017-08-18 11:04:18 +0200

    More chat commands (#723)
    * chat: Add volume control chat commands.
    
    * chat: Add /upvote, /downvote commands.
    
    * chat: Add /nick command to change username.

--- src/utils/commands/index.js
+++ src/utils/commands/index.js
@@ -2,5 +2,7 @@ import './bans';
 import './chat';
 import './help';
 import './mutes';
+import './playback';
 import './staff';
+import './users';
 import './waitlist';
---
+++ src/utils/commands/playback.js
@@ -0,0 +1,41 @@
+import { register } from '../ChatCommands';
+import { log } from '../../actions/ChatActionCreators';
+import {
+  setVolume,
+  mute,
+  unmute
+} from '../../actions/PlaybackActionCreators';
+import {
+  doUpvote,
+  doDownvote
+} from '../../actions/VoteActionCreators';
+
+register(
+  'volume',
+  'Set the current volume.',
+  {
+    action: (value) => {
+      const volume = parseInt(value, 10);
+      if (!isFinite(volume) || volume < 0 || volume > 100) {
+        return log('Volume must be a number between 0 and 100.');
+      }
+      return setVolume(volume);
+    }
+  }
+);
+
+register('mute', 'Mute the volume.', {
+  action: () => mute()
+});
+
+register('unmute', 'Unmute the media volume.', {
+  action: () => unmute()
+});
+
+register('upvote', 'Upvote the current track.', {
+  action: () => doUpvote()
+});
+
+register('downvote', 'Downvote the current track.', {
+  action: () => doDownvote()
+});
