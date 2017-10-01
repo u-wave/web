b11df00271c741e751f4375106d1c0d034cf7d52
soonshiny <Frederic.Lotz@studmail.w-hs.de>
2017-10-01 15:34:14 +0200

    Improve SettingsPanel design (#745)
    * [WIP] Basic CSS Fix, missing correct role color
    
    * added muitheme and role colors to usernames
    
    * CSS linting

--- src/components/SettingsManager/Profile.js
+++ src/components/SettingsManager/Profile.js
@@ -1,22 +1,59 @@
+import muiThemeable from 'material-ui/styles/muiThemeable';
 import cx from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Avatar from '../Avatar';
 import ChangeUsernameButton from './ChangeUsernameButton';
 
-const Profile = ({ className, user, onChangeUsername }) => (
+const enhance = muiThemeable();
+
+const tempRoleIDToReadableName = [
+  'User',
+  'Special',
+  'Moderator',
+  'Manager',
+  'Admin'
+];
+
+const formatJoinDate = date => new Date(date).toLocaleString([], {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric'
+});
+
+
+const tempRoleIDToRoleName = {
+  0: 'default',
+  1: 'special',
+  2: 'moderator',
+  3: 'manager',
+  4: 'admin'
+};
+
+
+const Profile = ({ className, user, onChangeUsername, muiTheme }) => (
   <div className={cx('SettingsPanelProfile', className)}>
     <Avatar
       className="SettingsPanelProfile-avatar"
       user={user}
     />
-    <h2 className="SettingsPanelProfile-username">
-      {user.username}
-      <ChangeUsernameButton
-        onChangeUsername={onChangeUsername}
-        initialUsername={user.username}
-      />
-    </h2>
+    <div className="SettingsPanelProfile-textblock">
+      <h2 className="SettingsPanelProfile-username">
+        {user.username}
+        <ChangeUsernameButton
+          onChangeUsername={onChangeUsername}
+          initialUsername={user.username}
+        />
+      </h2>
+      <p style={{ color: muiTheme.rankColors[tempRoleIDToRoleName[user.role]] }} className="SettingsPanelProfile-role">
+        {tempRoleIDToReadableName[user.role]}
+      </p>
+      <p className="SettingsPanelProfile-date">
+        {formatJoinDate(user.createdAt)}
+      </p>
+    </div>
   </div>
 );
 
@@ -24,7 +61,8 @@ Profile.propTypes = {
   className: PropTypes.string,
   user: PropTypes.object.isRequired,
 
-  onChangeUsername: PropTypes.func.isRequired
+  onChangeUsername: PropTypes.func.isRequired,
+  muiTheme: PropTypes.object.isRequired
 };
 
-export default Profile;
+export default enhance(Profile);
