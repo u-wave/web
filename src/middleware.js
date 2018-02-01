0283b3cb9c6c684b502f9ece51cb0f5753717b6d
Renée Kooi <renee@kooi.me>
2018-02-01 18:56:17 +0100

    Replace trumpet → hstream

--- src/middleware.js
+++ src/middleware.js
@@ -1,26 +1,8 @@
-import * as path from 'path';
-import * as defaultFs from 'fs';
-import trumpet from 'trumpet';
+import path from 'path';
+import defaultFs from 'fs';
+import hstream from 'hstream';
 import router from 'router';
 import serveStatic from 'connect-gzip-static';
-
-function injectConfig(transform, config) {
-  transform.select('#u-wave-config')
-    .createWriteStream()
-    .end(JSON.stringify(config));
-}
-
-function injectTitle(transform, title) {
-  transform.select('title')
-    .createWriteStream()
-    .end(title);
-}
-
-function injectResetKey(transform, key) {
-  transform.select('#reset-data')
-    .createWriteStream()
-    .end(key);
-}
 
 export default function uwaveWebClient(uw, options = {}) {
   const {
@@ -34,19 +16,21 @@ export default function uwaveWebClient(uw, options = {}) {
 
   return clientRouter
     .get('/', (req, res) => {
-      const transform = trumpet();
-      injectTitle(transform, title);
-      injectConfig(transform, clientOptions);
+      const transform = hstream({
+        title,
+        '#u-wave-config': JSON.stringify(clientOptions)
+      });
 
       fs.createReadStream(path.join(basePath, 'index.html'), 'utf8')
         .pipe(transform)
         .pipe(res);
     })
     .get('/reset/:key', (req, res) => {
-      const transform = trumpet();
-      injectTitle(transform, title);
-      injectConfig(transform, { apiUrl: clientOptions.apiUrl });
-      injectResetKey(transform, req.params.key);
+      const transform = hstream({
+        title,
+        '#u-wave-config': JSON.stringify({ apiUrl: clientOptions.apiUrl }),
+        '#reset-data': req.params.key
+      });
 
       fs.createReadStream(path.join(basePath, 'password-reset.html'), 'utf8')
         .pipe(transform)
