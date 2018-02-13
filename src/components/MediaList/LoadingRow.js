a02307ffc752399a2174c405773c200eb8c6a7cb
Renée Kooi <renee@kooi.me>
2018-02-13 14:13:46 +0100

    Fix LoadingRow lint

--- src/components/MediaList/LoadingRow.js
+++ src/components/MediaList/LoadingRow.js
@@ -3,29 +3,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MediaLoadingIndicator from './MediaLoadingIndicator';
 
-export default class LoadingRow extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    selected: PropTypes.bool
-  };
+const LoadingRow = ({
+  className,
+  selected = false,
+  ...attrs
+}) => (
+  <div
+    className={cx('MediaListRow', 'is-loading', className, selected && 'is-selected')}
+    {...attrs}
+  >
+    <MediaLoadingIndicator className="MediaListRow-loader" />
+    <div className="MediaListRow-artist"> … </div>
+    <div className="MediaListRow-title"> … </div>
+    <div className="MediaListRow-duration"> … </div>
+  </div>
+);
 
-  static defaultProps = {
-    selected: false
-  };
+LoadingRow.propTypes = {
+  className: PropTypes.string,
+  selected: PropTypes.bool
+};
 
-  render() {
-    const { className, selected, ...attrs } = this.props;
-    const selectedClass = selected ? 'is-selected' : '';
-    return (
-      <div
-        className={cx('MediaListRow', 'is-loading', className, selectedClass)}
-        {...attrs}
-      >
-        <MediaLoadingIndicator className="MediaListRow-loader" />
-        <div className="MediaListRow-artist"> … </div>
-        <div className="MediaListRow-title"> … </div>
-        <div className="MediaListRow-duration"> … </div>
-      </div>
-    );
-  }
-}
+export default LoadingRow;
