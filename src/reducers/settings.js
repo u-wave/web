e7eb2f5207e9abbc9a59795822d0efd4db89fafb
Renée Kooi <renee@kooi.me>
2018-02-24 20:10:11 +0100

    Fix defaults for new nested settings when loading old user settings.

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -24,7 +24,17 @@ export default function reduce(state = initialState, action = {}) {
   switch (type) {
   case LOAD_SETTINGS:
     // Loading settings defaults to the initial state.
-    return { ...initialState, ...payload };
+    return {
+      ...initialState,
+      ...payload,
+      // Merge notification settings if we have new ones.
+      // Needed if new notification types were added since the last time
+      // settings were saved to localStorage.
+      notifications: payload ? {
+        ...initialState.notifications,
+        ...payload.notifications
+      } : initialState.notifications
+    };
   case CHANGE_SETTING:
     return merge(state, payload, { clone: true });
   default:
