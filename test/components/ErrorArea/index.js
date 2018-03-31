e98bd54060b7e037244498b59f89ddc2f068d753
Renée Kooi <renee@kooi.me>
2018-03-31 19:56:49 +0200

    Port ErrorArea to material-ui@next.

--- test/components/ErrorArea/index.js
+++ test/components/ErrorArea/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import sinon from 'sinon';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import ErrorArea, { errorThemeSelector } from '../../../src/components/ErrorArea';
+import ErrorArea from '../../../src/components/ErrorArea';
 
 describe('<ErrorArea />', () => {
   it('should not show if there is no error', () => {
@@ -23,15 +23,6 @@ describe('<ErrorArea />', () => {
       />,
       { context: testUtils.context },
     ).dive().childAt(0)).to.have.prop('open', true);
-  });
-
-  it('uses the error notification palette overrides', () => {
-    const notificationTheme = testUtils.muiTheme.palette.notifications;
-
-    const contextTheme = errorThemeSelector(testUtils.context).muiTheme;
-
-    expect(contextTheme.snackbar).to.have.property('textColor', notificationTheme.errorTextColor);
-    expect(contextTheme.snackbar).to.have.property('backgroundColor', notificationTheme.errorBackgroundColor);
   });
 
   it('closes when user clicks anywhere on the page', () => {
