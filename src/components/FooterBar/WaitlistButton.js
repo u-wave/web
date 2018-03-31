078759841d898121dc2f5d8848b66ffd84ecbc20
Renée Kooi <renee@kooi.me>
2018-03-31 13:07:12 +0200

    Port WaitlistButton to material-ui@next

--- src/components/FooterBar/WaitlistButton.js
+++ src/components/FooterBar/WaitlistButton.js
@@ -4,73 +4,87 @@ import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import compose from 'recompose/compose';
 import pure from 'recompose/pure';
-import muiThemeable from 'material-ui/styles/muiThemeable';
-import FlatButton from 'material-ui/FlatButton';
-import LockedIcon from 'material-ui/svg-icons/action/lock';
+import { withStyles } from 'material-ui-next/styles'; // eslint-disable-line
+import Button from 'material-ui-next/Button'; // eslint-disable-line
+import LockedIcon from 'material-ui-icons/Lock';
 
-const inlineIconStyle = {
-  width: '1em',
-  height: '1em',
-};
-
-const buttonStyle = {
-  height: '100%',
-  fontSize: '11pt',
-  textTransform: 'uppercase',
-  // Align multiline button text nicely. Good for languages where "Join Waitlist"
-  // becomes a longer phrase, especially.
-  lineHeight: '24px',
-};
+const buttonStyle = theme => ({
+  button: {
+    height: '100%',
+    fontSize: '11pt',
+    textTransform: 'uppercase',
+    // Align multiline button text nicely. Good for languages where "Join Waitlist"
+    // becomes a longer phrase, especially.
+    lineHeight: '24px',
+    padding: '8px 4px',
+    color: theme.palette.primary.contrastText,
+    backgroundColor: theme.palette.primary.main,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light,
+    },
+  },
+  locked: {
+    color: theme.palette.text.disabled,
+  },
+  icon: {
+    width: '1em',
+    height: '1em',
+    color: theme.palette.primary.contrastText,
+  },
+  iconLocked: {
+    color: theme.palette.text.disabled,
+  },
+});
 
 const WaitlistButton = ({
   t,
-  muiTheme,
+  classes,
   userInWaitlist,
   isLocked,
   onClick,
 }) => {
   let icon;
   if (isLocked) {
-    const iconColor = userInWaitlist
-      // The user can still leave the waitlist, if it's locked…
-      ? muiTheme.flatButton.textColor
-      // …but cannot join the waitlist.
-      : muiTheme.flatButton.disabledTextColor;
     icon = (
       <LockedIcon
-        style={inlineIconStyle}
-        color={iconColor}
+        className={cx(
+          classes.icon,
+          // The user can still leave the waitlist, if it's locked,
+          // but cannot join the waitlist.
+          !userInWaitlist && classes.iconLocked,
+        )}
       />
     );
   }
 
   return (
-    <FlatButton
-      className={cx('FooterBar-join', isLocked && 'FooterBar-join--locked')}
+    <Button
+      classes={{
+        root: cx('FooterBar-join', classes.button),
+        disabled: cx('FooterBar-join--locked', classes.locked),
+      }}
       disabled={isLocked && !userInWaitlist}
       onClick={onClick}
-      style={buttonStyle}
-      backgroundColor={muiTheme.palette.primary1Color}
-      hoverColor={muiTheme.palette.primary2Color}
-      rippleColor={muiTheme.palette.primary3Color}
     >
       {icon}
       {isLocked && ' '}
       {userInWaitlist ? t('waitlist.leave') : t('waitlist.join')}
-    </FlatButton>
+    </Button>
   );
 };
 
 WaitlistButton.propTypes = {
   t: PropTypes.func.isRequired,
-  muiTheme: PropTypes.object.isRequired,
+  classes: PropTypes.shape({
+    button: PropTypes.string.isRequired,
+  }).isRequired,
   userInWaitlist: PropTypes.bool,
   isLocked: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
 };
 
 export default compose(
-  muiThemeable(),
   translate(),
   pure,
+  withStyles(buttonStyle),
 )(WaitlistButton);
