8dd9bc2448b4e497db095a6fbc89afadbbf970ce
Renée Kooi <renee@kooi.me>
2018-03-31 15:39:34 +0200

    Port SettingsPanel links to material-ui@next.

--- src/components/SettingsManager/GithubIcon.js
+++ src/components/SettingsManager/GithubIcon.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SvgIcon from 'material-ui/SvgIcon';
+import SvgIcon from 'material-ui-next/SvgIcon'; // eslint-disable-line
 
 /* eslint-disable max-len */
 const GithubIcon = props => (
