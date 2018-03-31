1a09feef40fc2d839d1700c21f6dd6b4e6490497
Renée Kooi <renee@kooi.me>
2018-03-31 13:28:04 +0200

    Move WaitlistButton styles to CSS file.

--- src/components/FooterBar/WaitlistButton.js
+++ src/components/FooterBar/WaitlistButton.js
@@ -4,41 +4,11 @@ import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import compose from 'recompose/compose';
 import pure from 'recompose/pure';
-import { withStyles } from 'material-ui-next/styles'; // eslint-disable-line
 import Button from 'material-ui-next/Button'; // eslint-disable-line
 import LockedIcon from 'material-ui-icons/Lock';
 
-const buttonStyle = theme => ({
-  button: {
-    height: '100%',
-    fontSize: '11pt',
-    textTransform: 'uppercase',
-    // Align multiline button text nicely. Good for languages where "Join Waitlist"
-    // becomes a longer phrase, especially.
-    lineHeight: '24px',
-    padding: '8px 4px',
-    color: theme.palette.primary.contrastText,
-    backgroundColor: theme.palette.primary.main,
-    '&:hover': {
-      backgroundColor: theme.palette.primary.light,
-    },
-  },
-  locked: {
-    color: theme.palette.text.disabled,
-  },
-  icon: {
-    width: '1em',
-    height: '1em',
-    color: theme.palette.primary.contrastText,
-  },
-  iconLocked: {
-    color: theme.palette.text.disabled,
-  },
-});
-
 const WaitlistButton = ({
   t,
-  classes,
   userInWaitlist,
   isLocked,
   onClick,
@@ -48,10 +18,10 @@ const WaitlistButton = ({
     icon = (
       <LockedIcon
         className={cx(
-          classes.icon,
+          'FooterBar-joinIcon',
           // The user can still leave the waitlist, if it's locked,
           // but cannot join the waitlist.
-          !userInWaitlist && classes.iconLocked,
+          !userInWaitlist && 'FooterBar-joinIcon--locked',
         )}
       />
     );
@@ -60,8 +30,8 @@ const WaitlistButton = ({
   return (
     <Button
       classes={{
-        root: cx('FooterBar-join', classes.button),
-        disabled: cx('FooterBar-join--locked', classes.locked),
+        root: 'FooterBar-join',
+        disabled: 'FooterBar-join--locked',
       }}
       disabled={isLocked && !userInWaitlist}
       onClick={onClick}
@@ -75,9 +45,6 @@ const WaitlistButton = ({
 
 WaitlistButton.propTypes = {
   t: PropTypes.func.isRequired,
-  classes: PropTypes.shape({
-    button: PropTypes.string.isRequired,
-  }).isRequired,
   userInWaitlist: PropTypes.bool,
   isLocked: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
@@ -86,5 +53,4 @@ WaitlistButton.propTypes = {
 export default compose(
   translate(),
   pure,
-  withStyles(buttonStyle),
 )(WaitlistButton);
