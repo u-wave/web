8a77d829cea1389692118ac68963b515ae6395c8
Renée Kooi <renee@kooi.me>
2018-04-01 19:08:52 +0200

    Fix ErrorArea tests.

--- test/components/ErrorArea/index.js
+++ test/components/ErrorArea/index.js
@@ -6,38 +6,34 @@ import ErrorArea from '../../../src/components/ErrorArea';
 
 describe('<ErrorArea />', () => {
   it('should not show if there is no error', () => {
-    expect(shallow(
+    expect(shallow((
       <ErrorArea
         error={null}
         onDismiss={() => {}}
-      />,
-      { context: testUtils.context },
-    ).dive().childAt(0)).to.have.prop('open', false);
+      />
+    )).childAt(0)).to.have.prop('open', false);
   });
 
   it('should open if there is an error', () => {
-    expect(shallow(
+    expect(shallow((
       <ErrorArea
         error="Something is WRONG!"
         onDismiss={() => {}}
-      />,
-      { context: testUtils.context },
-    ).dive().childAt(0)).to.have.prop('open', true);
+      />
+    )).childAt(0)).to.have.prop('open', true);
   });
 
   it('closes when user clicks anywhere on the page', () => {
     const spy = sinon.spy();
 
-    const area = shallow(
-      (
-        <ErrorArea
-          error="Message"
-          onDismiss={spy}
-        />
-      ), { context: testUtils.context },
-    ).dive();
+    const area = shallow((
+      <ErrorArea
+        error="Message"
+        onDismiss={spy}
+      />
+    ));
 
-    area.childAt(0).prop('onRequestClose').call();
+    area.childAt(0).prop('onClose').call();
 
     expect(spy.calledOnce).to.equal(true);
   });
--- test/testUtils.js
+++
@@ -1,5 +0,0 @@
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import baseMuiTheme from '../src/MuiTheme';
-
-export const muiTheme = getMuiTheme(baseMuiTheme);
-export const context = { muiTheme };
