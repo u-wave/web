0bf440d7214159c76d5aa57ea44dd3e431984be2
Renée Kooi <renee@kooi.me>
2018-04-03 19:38:34 +0200

    Fix Babel config env being shared.

--- .babelrc.js
+++ .babelrc.js
@@ -1,49 +1,51 @@
-const env = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
-const browsers = process.env.BROWSERSLIST;
+module.exports = () => {
+  const env = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
+  const browsers = process.env.BROWSERSLIST;
 
-const targets = {};
-if (browsers) {
-  targets.browsers = browsers;
-}
-if (env === 'production') {
-  targets.uglify = true;
-}
-if (env === 'testing') {
-  targets.node = 'current';
-}
+  const targets = {};
+  if (browsers) {
+    targets.browsers = browsers;
+  }
+  if (env === 'production') {
+    targets.uglify = true;
+  }
+  if (env === 'testing') {
+    targets.node = 'current';
+  }
 
-const preset = {
-  presets: [
-    ['env', {
-      modules: false,
-      loose: env === 'production',
-      targets
-    }],
-    'react'
-  ],
-  plugins: [
-    'syntax-dynamic-import',
-    'transform-object-rest-spread',
-    'transform-class-properties',
-    'transform-export-extensions',
-    ['transform-runtime', { polyfill: false }]
-  ]
+  const preset = {
+    presets: [
+      ['env', {
+        modules: false,
+        loose: env === 'production',
+        targets
+      }],
+      'react'
+    ],
+    plugins: [
+      'syntax-dynamic-import',
+      'transform-object-rest-spread',
+      'transform-class-properties',
+      'transform-export-extensions',
+      ['transform-runtime', { polyfill: false }]
+    ]
+  };
+
+  if (env === 'development') {
+    preset.plugins.push('react-hot-loader/babel');
+  }
+
+  if (env === 'production') {
+    preset.plugins.push(
+      'transform-react-constant-elements',
+      'transform-react-inline-elements',
+      ['transform-react-remove-prop-types', { mode: 'wrap' }]
+    );
+  }
+
+  if (env === 'testing') {
+    preset.plugins.push('istanbul');
+  }
+
+  return preset;
 };
-
-if (env === 'development') {
-  preset.plugins.push('react-hot-loader/babel');
-}
-
-if (env === 'production') {
-  preset.plugins.push(
-    'transform-react-constant-elements',
-    'transform-react-inline-elements',
-    ['transform-react-remove-prop-types', { mode: 'wrap' }]
-  );
-}
-
-if (env === 'testing') {
-  preset.plugins.push('istanbul');
-}
-
-module.exports = preset;
