1565f58f2d659f68d8f108ae815db455564fbf44
Renée Kooi <renee@kooi.me>
2018-04-12 15:01:37 +0200

    Use qs-stringify to simplify building the soundcloud embed URL. (#863)

--- src/sources/soundcloud/PreviewPlayer.js
+++ src/sources/soundcloud/PreviewPlayer.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import qsStringify from 'qs-stringify';
 
-const createEmbedUrl = sourceID => `
-  https://w.soundcloud.com/player/
-  ?url=${encodeURIComponent(`https://api.soundcloud.com/tracks/${sourceID}`)}
-  &color=55B9FF
-  &auto_play=true
-  &hide_related=true
-  &buying=false
-  &liking=false
-  &download=false
-  &sharing=false
-  &show_comments=false
-  &visual=true
-`.replace(/\s+/g, '');
+const createEmbedUrl = sourceID =>
+  `https://w.soundcloud.com/player/?${qsStringify({
+    url: `https://api.soundcloud.com/tracks/${sourceID}`,
+    color: '55B9FF',
+    auto_play: true,
+    hide_related: true,
+    buying: false,
+    liking: false,
+    download: false,
+    sharing: false,
+    show_comments: false,
+    visual: true,
+  })}`;
 
 const PreviewPlayer = ({ media }) => (
   <iframe
