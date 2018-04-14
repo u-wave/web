4a730875f467a1b14e8406b68922af24c989def2
Renée Kooi <renee@kooi.me>
2018-04-14 21:18:56 +0200

    Split lots of chunks. (#872)

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const { ProgressPlugin } = require('webpack');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const ExtractCssPlugin = require('mini-css-extract-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
+const HtmlSiblingChunksPlugin = require('html-webpack-include-sibling-chunks-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const ManifestPlugin = require('webpack-pwa-manifest');
 
@@ -55,6 +56,7 @@ const plugins = [
   new CopyPlugin([
     { from: '../assets/favicon.ico', to: 'favicon.ico' },
   ]),
+  new HtmlSiblingChunksPlugin(),
   new HtmlPlugin({
     chunks: ['app'],
     template: './index.html',
@@ -76,7 +78,7 @@ const plugins = [
   new ManifestPlugin(require('./src/manifest').default),
 ];
 
-const optimization = {};
+let optimization;
 
 if (nodeEnv === 'production') {
   const CompressionPlugin = require('compression-webpack-plugin');
@@ -86,17 +88,26 @@ if (nodeEnv === 'production') {
 
   const compressible = /\.(js|css|svg|mp3)$/;
 
-  optimization.minimizer = [new UglifyJsPlugin({
-    parallel: true,
-    sourceMap: true,
-    uglifyOptions: {
-      toplevel: true,
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-      },
+  optimization = {
+    runtimeChunk: 'single',
+    splitChunks: {
+      automaticNameDelimiter: '-',
+      chunks: 'all',
     },
-  })];
+    minimizer: [
+      new UglifyJsPlugin({
+        parallel: true,
+        sourceMap: true,
+        uglifyOptions: {
+          toplevel: true,
+          compress: {
+            pure_getters: true,
+            unsafe: true,
+          },
+        },
+      }),
+    ],
+  };
 
   plugins.push(
     new ExtractCssPlugin({
