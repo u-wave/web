5cd0c6175599046766febedaf99bcfbd6c224b41
Renée Kooi <renee@kooi.me>
2018-04-14 12:50:07 +0200

    Use material-ui Buttons in Forms.

--- src/components/Form/Button.js
+++ src/components/Form/Button.js
@@ -1,13 +1,20 @@
 import cx from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
+import MuiButton from 'material-ui/Button';
 
 const Button = ({
   children, className, text, ...props
 }) => (
-  <button className={cx('Button', className)} {...props}>
+  <MuiButton
+    variant="raised"
+    color="primary"
+    className={cx('Button', className)}
+    type="submit"
+    {...props}
+  >
     {text || children}
-  </button>
+  </MuiButton>
 );
 
 Button.propTypes = {
