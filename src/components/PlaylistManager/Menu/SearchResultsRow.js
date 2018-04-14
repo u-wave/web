b454248ac478f7dcc562167072a1ab682ec34c8d
Renée Kooi <renee@kooi.me>
2018-04-14 15:30:43 +0200

    Replace some components with material-ui versions. (#869)
    * Use a material-ui MenuList for the playlist list.
    
    * Use material-ui Buttons in Forms.

--- src/components/PlaylistManager/Menu/SearchResultsRow.js
+++ src/components/PlaylistManager/Menu/SearchResultsRow.js
@@ -2,6 +2,7 @@ import cx from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
 import IconButton from 'material-ui/IconButton';
+import { ListItem } from 'material-ui/List';
 import SearchIcon from '@material-ui/icons/Search';
 import CloseIcon from '@material-ui/icons/Close';
 
@@ -11,24 +12,22 @@ const SearchResultsRow = ({
   onClick,
   onClose,
 }) => (
-  <div role="menuitem" className={cx('PlaylistMenuRow', 'PlaylistMenuRow--search', className)}>
-    <div className="PlaylistMenuRow-content">
-      <button
-        className="PlaylistMenuRow-title"
-        onClick={onClick}
-      >
-        <div className="PlaylistMenuRow-active-icon">
-          <SearchIcon />
-        </div>
-        &quot;{query}&quot;
-      </button>
-      <div className="PlaylistMenuRow-count">
-        <IconButton className="PlaylistMenuRow-closeButton" onClick={onClose}>
-          <CloseIcon nativeColor="#777" />
-        </IconButton>
+  <ListItem role="menuitem" className={cx('PlaylistMenuRow', 'PlaylistMenuRow--search', className)}>
+    <button
+      className="PlaylistMenuRow-title"
+      onClick={onClick}
+    >
+      <div className="PlaylistMenuRow-active-icon">
+        <SearchIcon />
       </div>
+      &quot;{query}&quot;
+    </button>
+    <div className="PlaylistMenuRow-count">
+      <IconButton className="PlaylistMenuRow-closeButton" onClick={onClose}>
+        <CloseIcon nativeColor="#777" />
+      </IconButton>
     </div>
-  </div>
+  </ListItem>
 );
 
 SearchResultsRow.propTypes = {
