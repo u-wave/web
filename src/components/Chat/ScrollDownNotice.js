ff9b344b44736eba31b055e05987de173f7dd6ca
Renée Kooi <renee@kooi.me>
2018-06-10 00:56:47 +0200

    Add a label to the scroll down button.

--- src/components/Chat/ScrollDownNotice.js
+++ src/components/Chat/ScrollDownNotice.js
@@ -1,16 +1,20 @@
 import cx from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { translate } from 'react-i18next';
 import Button from '@material-ui/core/Button';
 import ScrollDownIcon from '@material-ui/icons/ArrowDownward';
 
-const ScrollNotice = ({ show, onClick }) => (
+const enhance = translate();
+
+const ScrollNotice = ({ t, show, onClick }) => (
   <div className={cx('ChatMessages-scrollDown', show && 'is-visible')}>
     <Button
       className="ChatMessages-scrollDownButton"
       variant="fab"
       mini
       color="primary"
+      aria-label={t('chat.scrollDown')}
       onClick={onClick}
     >
       <ScrollDownIcon />
@@ -19,8 +23,9 @@ const ScrollNotice = ({ show, onClick }) => (
 );
 
 ScrollNotice.propTypes = {
+  t: PropTypes.func.isRequired,
   show: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
-export default ScrollNotice;
+export default enhance(ScrollNotice);
