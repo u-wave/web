3443d7c0caf2fdbfff081e5925c01f49a73195cc
Renée Kooi <renee@kooi.me>
2018-07-08 14:26:42 +0200

    mui controls

--- src/admin/components/SchemaForm/index.js
+++ src/admin/components/SchemaForm/index.js
@@ -2,43 +2,110 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
 import Form from '../../../components/Form';
 import TextField from '../../../components/Form/TextField';
 
-const renderers = {
-  object(spec) {
-    const children = Object.keys(spec.properties).map((key) => {
-      return <Typography>{key}</Typography>;
-    });
+function renderObjectProperties(spec, renderChild) {
+  return Object.keys(spec.properties).map((key) => {
+    const subSchema = spec.properties[key];
+
     return (
-      <div>
-        {spec.title && <Typography>{spec.title}</Typography>}
-        {spec.description && <Typography>{spec.description}</Typography>}
+      <FormGroup style={{ marginBottom: 24 }}>{renderChild(subSchema)}</FormGroup>
+    );
+  });
+}
+
+const renderers = {
+  object(spec, renderChild) {
+    const children = renderObjectProperties(spec, renderChild);
+    return (
+      <FormControl component="fieldset">
+        {spec.title && <FormLabel component="legend">{spec.title}</FormLabel>}
+        {spec.description && <FormHelperText>{spec.description}</FormHelperText>}
         {children}
-      </div>
+      </FormControl>
     );
   },
-  string: TextField,
+  textarea(schema) {
+    return (
+      <React.Fragment>
+        <Typography component="p">{schema.title}</Typography>
+        <textarea />
+        {schema.description && <FormHelperText>{schema.description}</FormHelperText>}
+      </React.Fragment>
+    );
+  },
+  string(schema) {
+    return (
+      <React.Fragment>
+        <Typography>{schema.title}</Typography>
+        <TextField />
+        {schema.description && <FormHelperText>{schema.description}</FormHelperText>}
+      </React.Fragment>
+    );
+  },
+  number(schema) {
+    return (
+      <React.Fragment>
+        <Typography>{schema.title}</Typography>
+        <TextField type="number" />
+        {schema.description && <FormHelperText>{schema.description}</FormHelperText>}
+      </React.Fragment>
+    );
+  },
+  boolean(schema) {
+    return (
+      <React.Fragment>
+        <FormControlLabel
+          control={<Checkbox />}
+          label={schema.title}
+        />
+        {schema.description && <FormHelperText>{schema.description}</FormHelperText>}
+      </React.Fragment>
+    );
+  },
 };
 
 export default class SchemaForm extends React.Component {
   static propTypes = {
     schema: PropTypes.object.isRequired,
+    unwrapRoot: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    unwrapRoot: false,
   };
 
   renderField(schema) {
-    const renderer = renderers[schema.type];
+    const widgetName = schema['ui:widget'] || schema.type;
+    const renderer = renderers[widgetName];
     if (renderer) {
-      return renderer(schema);
+      return renderer(schema, this.renderField.bind(this));
     }
+    return (
+      <p style={{ background: 'red', color: 'white'}}>
+        Unknown:
+        {widgetName}
+      </p>
+    );
   }
 
   render() {
-    const { schema } = this.props;
+    const { schema, unwrapRoot } = this.props;
+
+    const form = unwrapRoot
+      ? renderObjectProperties(schema, this.renderField.bind(this))
+      : this.renderField(schema);
 
     return (
       <Form>
-        {this.renderField(schema)}
+        {form}
       </Form>
     );
   }
--- src/admin/components/ServerConfig/index.js
+++ src/admin/components/ServerConfig/index.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SchemaForm from 'react-jsonschema-form'; // eslint-disable-line
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SchemaForm from '../SchemaForm'; // eslint-disable-line
+
+const Section = ({ schema }) => (
+  <ExpansionPanel>
+    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+      <div>
+        <Typography>{schema.title}</Typography>
+        <Typography color="textSecondary">{schema.description}</Typography>
+      </div>
+    </ExpansionPanelSummary>
+    <ExpansionPanelDetails>
+      <SchemaForm
+        schema={schema}
+        unwrapRoot={schema.type === 'object'}
+      />
+    </ExpansionPanelDetails>
+    <ExpansionPanelActions>
+      <Button color="primary" variant="raised">Save</Button>
+    </ExpansionPanelActions>
+  </ExpansionPanel>
+);
+
+Section.propTypes = {
+  schema: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+function renderSections(allSchema) {
+  return Object.keys(allSchema.properties).map(key => (
+    <Section key={key} schema={allSchema.properties[key]} />
+  ));
+}
 
 const ServerConfig = ({
   configSchema,
 }) => (
   <div>
     {configSchema ? (
-      <SchemaForm
-        schema={configSchema}
-      />
+      renderSections(configSchema)
     ) : (
       <p>
         Loading
