f76898eb3faf45603e4a7ef21a779a00b82f9cc4
Renée Kooi <renee@kooi.me>
2018-07-16 21:35:35 +0200

    Fix src/sources/soundcloud

--- src/sources/soundcloud/PlayerWrapper.js
+++ src/sources/soundcloud/PlayerWrapper.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import Player from './Player';
 import PreviewPlayer from './PreviewPlayer';
 
-const PlayerWrapper = (props) => {
-  if (props.mode === 'preview') {
+const PlayerWrapper = ({ mode, ...props }) => {
+  if (mode === 'preview') {
     return <PreviewPlayer {...props} />;
   }
-  return <Player {...props} />;
+  return <Player {...props} mode={mode} />;
 };
 
 PlayerWrapper.propTypes = {
