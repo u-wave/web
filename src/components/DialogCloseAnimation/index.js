e2c7f5268900765167cc7f69e161088429355063
Renée Kooi <renee@kooi.me>
2018-07-17 09:55:03 +0200

    Fix src/components/DialogCloseAnimation

--- src/components/DialogCloseAnimation/index.js
+++ src/components/DialogCloseAnimation/index.js
@@ -5,44 +5,45 @@ import PropTypes from 'prop-types';
  * Component that handles the material-ui/Dialog close animation when a Dialog unmounts.
  */
 export default class DialogCloseAnimation extends React.Component {
+  timeout = null;
+
   static propTypes = {
     children: PropTypes.element,
     delay: PropTypes.number.isRequired,
   };
 
   state = {
+    // eslint-disable-next-line react/destructuring-assignment
     children: this.props.children,
   };
 
   // TODO translate this to componentDidUpdate()?
   componentWillReceiveProps(nextProps) {
+    const { delay } = this.props;
+    const { children } = this.state;
+
     if (nextProps.children) {
       this.clearTimeout();
       this.setState({
         children: nextProps.children,
       });
     }
-    if (this.state.children && !nextProps.children) {
-      this.setState({
-        children: React.cloneElement(this.state.children, {
-          open: false,
-        }),
-      });
+
+    if (children && !nextProps.children) {
+      this.keepShowingChildren();
 
       this.timeout = setTimeout(() => {
         this.setState({
           children: null,
         });
         this.timeout = null;
-      }, this.props.delay);
+      }, delay);
     }
   }
 
   componentWillUnmount() {
     this.clearTimeout();
   }
-
-  timeout = null;
 
   clearTimeout() {
     if (this.timeout) {
@@ -51,7 +52,17 @@ export default class DialogCloseAnimation extends React.Component {
     }
   }
 
+  keepShowingChildren() {
+    this.setState(({ children }) => ({
+      children: React.cloneElement(children, {
+        open: false,
+      }),
+    }));
+  }
+
   render() {
-    return this.state.children || null;
+    const { children } = this.state;
+
+    return children || null;
   }
 }
