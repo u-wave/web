9e6009180928e814ba588245cb53b783d5340e7a
Renée Kooi <renee@kooi.me>
2018-07-22 22:09:46 +0200

    Change icon in "No filter results" view.

--- src/components/PlaylistManager/Panel/PlaylistFilterEmpty.js
+++ src/components/PlaylistManager/Panel/PlaylistFilterEmpty.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import Typography from '@material-ui/core/Typography';
-import EmptyIcon from '@material-ui/icons/PlaylistAdd';
+import EmptyIcon from '@material-ui/icons/Search';
 
 const enhance = translate();
 
