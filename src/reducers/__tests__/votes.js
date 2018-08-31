u-wave/web

commit 480f8ca4b81a6aec8e7d8e56763a689a8f9d47a7

Initialize votes on load (#1103)
Fixes the vote counters always being 0 on load, even if people already
voted.

Previously this was handled via the ADVANCE action, but the stats were
removed from it a while ago. That was because the stats were only
available on the initial ADVANCE, not subsequent ones, which was
inconsistent.

Now it uses the INIT_STATE action which is more appropriate.

src/reducers/votes.js
@@ -1,5 +1,5 @@
 import {
-  ADVANCE,
+  INIT_STATE,
   LOAD_VOTES,
   FAVORITE,
   UPVOTE,
@@ -17,16 +17,18 @@ const initialState = {
 export default function reduce(state = initialState, action = {}) {
   const { type, payload } = action;
   switch (type) {
-    case ADVANCE:
-      if (payload && payload.stats) {
+    case INIT_STATE: {
+      const { stats } = payload.booth || {};
+      if (stats) {
         return {
           ...state,
-          upvotes: payload.stats.upvotes,
-          downvotes: payload.stats.downvotes,
-          favorites: payload.stats.favorites,
+          upvotes: stats.upvotes,
+          downvotes: stats.downvotes,
+          favorites: stats.favorites,
         };
       }
       return initialState;
+    }
     case LOAD_VOTES:
       return {
         ...state,

commit 28ec5200aef84525d01e83c65261bc70c4ffe7a6

Clear votes again on advance.
Regression introduced by #1103

Also adding some tests for this behaviour.

src/reducers/votes.js
@@ -1,6 +1,7 @@
 import {
   INIT_STATE,
   LOAD_VOTES,
+  ADVANCE,
   FAVORITE,
   UPVOTE,
   DOWNVOTE,
@@ -14,28 +15,29 @@ const initialState = {
   favorites: [],
 };
 
+function setVotes (state, stats) {
+  return {
+    ...state,
+    upvotes: stats.upvotes,
+    downvotes: stats.downvotes,
+    favorites: stats.favorites,
+  };
+}
+
 export default function reduce(state = initialState, action = {}) {
   const { type, payload } = action;
   switch (type) {
     case INIT_STATE: {
       const { stats } = payload.booth || {};
       if (stats) {
-        return {
-          ...state,
-          upvotes: stats.upvotes,
-          downvotes: stats.downvotes,
-          favorites: stats.favorites,
-        };
+        return setVotes(state, stats);
       }
       return initialState;
     }
+    case ADVANCE:
+      return initialState;
     case LOAD_VOTES:
-      return {
-        ...state,
-        upvotes: payload.upvotes,
-        downvotes: payload.downvotes,
-        favorites: payload.favorites,
-      };
+      return setVotes(state, payload);
     case UPVOTE:
       return {
         ...state,

src/reducers/__tests__/votes.js
@@ -1,0 +1,76 @@
+import { expect } from 'chai';
+import { INIT_STATE, ADVANCE } from '../../constants/ActionTypes';
+import votes from '../votes';
+
+const EMPTY_STATE = {
+  upvotes: [],
+  downvotes: [],
+  favorites: [],
+};
+
+describe('reducers/votes', () => {
+  const initialState = () => votes(undefined, { type: '@@redux/INIT' });
+  it('should not respond to unrelated actions', () => {
+    let state = { upvotes: [1] };
+    state = votes(state, { type: 'randomOtherAction', payload: {} });
+    expect(state.upvotes).to.eql([1]);
+  });
+
+  it('should default to empty vote arrays', () => {
+    const state = votes(undefined, { type: '@@redux/INIT' });
+    expect(state).to.eql(EMPTY_STATE);
+  });
+
+  describe('action: auth/INIT_STATE', () => {
+    let state = initialState();
+
+    it('should load in votes from the init endpoint', () => {
+      state = votes(state, {
+        type: INIT_STATE,
+        payload: {
+          booth: {
+            stats: {
+              upvotes: [1],
+              downvotes: [],
+              favorites: [2],
+            },
+          },
+        },
+      });
+
+      expect(state).to.eql({
+        upvotes: [1],
+        downvotes: [],
+        favorites: [2],
+      });
+    });
+
+    it('should clear votes if init endpoint has empty booth', () => {
+      state = votes(state, {
+        type: INIT_STATE,
+        payload: { booth: null },
+      });
+
+      expect(state).to.eql(EMPTY_STATE);
+    });
+  });
+
+  describe('action: booth/ADVANCE', () => {
+    it('should reset votes state', () => {
+      const state = votes({
+        upvotes: [3, 4],
+        downvotes: [5],
+        favorites: [1, 2],
+      }, {
+        type: ADVANCE,
+        payload: {
+          historyID: 'someRandomID',
+          userID: 'seventeen',
+          media: { artist: 'about tess', title: 'Imaginedit' },
+          timestamp: 1449767164107,
+        },
+      });
+      expect(state).to.eql(EMPTY_STATE);
+    });
+  });
+});