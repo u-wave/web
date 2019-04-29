1cd2c6f486a6196e085b5b85ee476e2b4516f698
Renée Kooi <renee@kooi.me>
2019-04-29 11:50:44 +0200

    deps: update react-transition-group to v4

--- src/components/App/Overlays.js
+++ src/components/App/Overlays.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CSSTransition from 'react-transition-group/CSSTransition';
-import TransitionGroup from 'react-transition-group/TransitionGroup';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 // Use the css transitionend event to mark the finish of a transition.
 // Using this instead of a timeout prop so that we don't have to define
