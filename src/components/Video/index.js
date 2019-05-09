5511fe7e7c8a4eed1be654042be4be59e2d86a61
Renée Kooi <renee@kooi.me>
2019-05-09 12:05:45 +0200

    Rewrite Video component using hooks

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,5 +1,4 @@
 import cx from 'classnames';
-import isEqual from 'is-equal-shallow';
 import React from 'react';
 import PropTypes from 'prop-types';
 import screenfull from 'screenfull';
@@ -10,152 +9,142 @@ import VideoToolbar from './VideoToolbar';
 import MouseMoveCapture from './VideoMouseMoveCapture';
 import Player from '../Player';
 
+const {
+  useEffect, useState, useCallback, useRef,
+} = React;
+
 const defaultSourceTools = () => null;
 
 const enhance = injectMediaSources();
 
-class Video extends React.Component {
-  static propTypes = {
-    getMediaSource: PropTypes.func.isRequired,
-    isFullscreen: PropTypes.bool,
-    enabled: PropTypes.bool,
-    size: PropTypes.string,
-    volume: PropTypes.number,
-    isMuted: PropTypes.bool,
-    media: PropTypes.object,
-    seek: PropTypes.number,
-    onFullscreenEnter: PropTypes.func.isRequired,
-    onFullscreenExit: PropTypes.func.isRequired,
-  };
+function Video(props) {
+  const {
+    getMediaSource,
+    isFullscreen,
+    enabled,
+    size,
+    volume,
+    isMuted,
+    media,
+    seek,
+    onFullscreenEnter,
+    onFullscreenExit,
+  } = props;
 
-  state = {
-    shouldShowToolbar: false,
-  };
+  const [shouldShowToolbar, setShowToolbar] = useState(false);
+  const container = useRef(null);
+  const timer = useRef(null);
 
-  componentDidMount() {
+  const handleRequestFullscreenEnter = useCallback(() => {
     if (screenfull.enabled) {
-      screenfull.on('change', this.handleFullscreenChange);
+      screenfull.request(container.current);
     }
-  }
+    onFullscreenEnter();
+  }, [onFullscreenEnter]);
 
-  shouldComponentUpdate(nextProps) {
-    return !isEqual(nextProps, this.props);
-  }
+  const handleFullscreenChange = useCallback(() => {
+    if (!screenfull.isFullscreen) {
+      onFullscreenExit();
+    }
+  }, [onFullscreenExit]);
 
-  componentDidUpdate(prevProps) {
-    const { isFullscreen } = this.props;
+  const handleMouseMoveEnd = useCallback(() => {
+    clearTimeout(timer.current);
+    timer.current = null;
+    setShowToolbar(false);
+  }, []);
 
-    if (prevProps.isFullscreen && !isFullscreen && screenfull.enabled) {
+  // Show toolbar if the user mouses over the video.
+  const handleMouseMove = useCallback(() => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+    } else {
+      setShowToolbar(true);
+    }
+    timer.current = setTimeout(handleMouseMoveEnd, 5000);
+  }, []);
+
+  // Attach fullscreen exit event listener.
+  useEffect(() => {
+    if (screenfull.enabled) {
+      screenfull.on('change', handleFullscreenChange);
+      return () => screenfull.off('change', handleFullscreenChange);
+    }
+    return () => null;
+  }, [handleFullscreenChange]);
+
+  // Exit fullscreen mode if the `isFullscreen` prop changed.
+  useEffect(() => {
+    if (!isFullscreen && screenfull.enabled) {
       // Checking for `enabled` here, because our props have probably changed
-      // _after_ exiting fullscreen mode (see `this.handleFullscreenChange`).
+      // _after_ exiting fullscreen mode (see `handleFullscreenChange`).
       // This way we don't double-exit.
       if (screenfull.isFullscreen) {
         screenfull.exit();
       }
     }
+  }, [isFullscreen]);
+
+  if (!media) {
+    return <div className="Video" />;
   }
 
-  handleFullscreenEnter = () => {
-    const { onFullscreenEnter } = this.props;
+  const currentSource = getMediaSource(media.sourceType);
+  const MediaSourceTools = (currentSource && currentSource.VideoTools)
+    ? currentSource.VideoTools
+    : defaultSourceTools;
 
-    if (screenfull.enabled) {
-      screenfull.request(this.element);
-    }
-    onFullscreenEnter();
-  };
+  return (
+    <div
+      ref={container}
+      className={cx('Video', `Video--${media.sourceType}`, `Video--${size}`)}
+    >
+      <VideoBackdrop url={media.thumbnail} />
+      <Player
+        enabled={enabled}
+        size={size}
+        volume={volume}
+        isMuted={isMuted}
+        media={media}
+        seek={seek}
+      />
 
-  handleFullscreenChange = () => {
-    const { onFullscreenExit } = this.props;
-
-    if (!screenfull.isFullscreen) {
-      onFullscreenExit();
-    }
-  };
-
-  handleMouseMove = () => {
-    if (this.timer) {
-      clearTimeout(this.timer);
-    } else {
-      this.setState({ shouldShowToolbar: true });
-    }
-    this.timer = setTimeout(this.handleMouseMoveEnd, 5000);
-  };
-
-  handleMouseMoveEnd = () => {
-    clearTimeout(this.timer);
-    this.timer = null;
-    this.setState({ shouldShowToolbar: false });
-  };
-
-  refElement = (element) => {
-    this.element = element;
-  };
-
-  render() {
-    const {
-      getMediaSource,
-      isFullscreen,
-      enabled,
-      size,
-      volume,
-      isMuted,
-      media,
-      seek,
-      onFullscreenExit,
-    } = this.props;
-
-    if (!media) {
-      return <div className="Video" />;
-    }
-
-    const {
-      shouldShowToolbar,
-    } = this.state;
-
-    const currentSource = getMediaSource(media.sourceType);
-    const MediaSourceTools = (currentSource && currentSource.VideoTools)
-      ? currentSource.VideoTools
-      : defaultSourceTools;
-
-    return (
-      <div
-        ref={this.refElement}
-        className={cx('Video', `Video--${media.sourceType}`, `Video--${size}`)}
-      >
-        <VideoBackdrop url={media.thumbnail} />
-        <Player
-          enabled={enabled}
-          size={size}
-          volume={volume}
-          isMuted={isMuted}
+      {isFullscreen && (
+        <MouseMoveCapture
+          active={shouldShowToolbar}
+          onMouseMove={handleMouseMove}
+        />
+      )}
+      {isFullscreen && (
+        <VideoProgressBar
           media={media}
           seek={seek}
         />
-
-        {isFullscreen && (
-          <MouseMoveCapture
-            active={shouldShowToolbar}
-            onMouseMove={this.handleMouseMove}
-          />
-        )}
-        {isFullscreen && (
-          <VideoProgressBar
-            media={media}
-            seek={seek}
-          />
-        )}
-        {(!isFullscreen || shouldShowToolbar) && (
-          <VideoToolbar
-            isFullscreen={isFullscreen}
-            onFullscreenEnter={this.handleFullscreenEnter}
-            onFullscreenExit={onFullscreenExit}
-          >
-            <MediaSourceTools media={media} />
-          </VideoToolbar>
-        )}
-      </div>
-    );
-  }
+      )}
+      {(!isFullscreen || shouldShowToolbar) && (
+        <VideoToolbar
+          isFullscreen={isFullscreen}
+          onFullscreenEnter={handleRequestFullscreenEnter}
+          onFullscreenExit={onFullscreenExit}
+        >
+          <MediaSourceTools media={media} />
+        </VideoToolbar>
+      )}
+    </div>
+  );
 }
+
+Video.propTypes = {
+  getMediaSource: PropTypes.func.isRequired,
+  isFullscreen: PropTypes.bool,
+  enabled: PropTypes.bool,
+  size: PropTypes.string,
+  volume: PropTypes.number,
+  isMuted: PropTypes.bool,
+  media: PropTypes.object,
+  seek: PropTypes.number,
+  onFullscreenEnter: PropTypes.func.isRequired,
+  onFullscreenExit: PropTypes.func.isRequired,
+};
 
 export default enhance(Video);
