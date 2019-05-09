733f5d4fdba1d5ad1fe8657ff90b1338569e8db1
Renée Kooi <renee@kooi.me>
2019-05-09 11:57:00 +0200

    Set hidden=true on invisible tabs.
    This will tell React that rendering these is low priority.

--- src/components/SidePanels/PanelContainer.js
+++ src/components/SidePanels/PanelContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PanelContainer = ({ selected, children }) => (
-  <div className={cx('SidePanel-panel', selected && 'is-selected')}>
+  <div className={cx('SidePanel-panel', selected && 'is-selected')} hidden={!selected}>
     {children}
   </div>
 );
