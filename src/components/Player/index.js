61334fa673083456be96085a44689ffc7e256e7f
Renée Kooi <renee@kooi.me>
2019-05-09 13:24:46 +0200

    Rewrite Player component using hooks

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,73 +1,62 @@
 import cx from 'classnames';
-import isEqual from 'is-equal-shallow';
 import React from 'react';
 import PropTypes from 'prop-types';
-import injectMediaSources from '../../utils/injectMediaSources';
+import { useMediaSources } from '../../context/MediaSourceContext';
 
-const enhance = injectMediaSources();
+function Player({
+  enabled,
+  size,
+  volume,
+  isMuted,
+  media,
+  seek,
+  onPlay,
+}) {
+  const { getAllMediaSources } = useMediaSources();
 
-class Player extends React.Component {
-  static propTypes = {
-    getAllMediaSources: PropTypes.func.isRequired,
-    enabled: PropTypes.bool,
-    size: PropTypes.string,
-    volume: PropTypes.number,
-    isMuted: PropTypes.bool,
-    media: PropTypes.object,
-    seek: PropTypes.number,
-    onPlay: PropTypes.func,
+  if (!media) {
+    return <div className="Player" />;
+  }
+
+  const props = {
+    enabled,
+    media,
+    seek,
+    mode: size,
+    volume: isMuted ? 0 : volume,
+    onPlay,
   };
 
-  shouldComponentUpdate(nextProps) {
-    return !isEqual(nextProps, this.props);
-  }
-
-  render() {
-    const {
-      getAllMediaSources,
-      enabled,
-      size,
-      volume,
-      isMuted,
-      media,
-      seek,
-      onPlay,
-    } = this.props;
-
-    if (!media) {
-      return <div className="Player" />;
+  const sources = getAllMediaSources();
+  const players = Object.keys(sources).map((sourceType) => {
+    const SourcePlayer = sources[sourceType].Player;
+    if (!SourcePlayer) {
+      return null;
     }
-
-    const props = {
-      enabled,
-      media,
-      seek,
-      mode: size,
-      volume: isMuted ? 0 : volume,
-      onPlay,
-    };
-
-    const sources = getAllMediaSources();
-    const players = Object.keys(sources).map((sourceType) => {
-      const SourcePlayer = sources[sourceType].Player;
-      if (!SourcePlayer) {
-        return null;
-      }
-      return (
-        <SourcePlayer
-          key={sourceType}
-          {...props}
-          active={media.sourceType === sourceType}
-        />
-      );
-    }).filter(player => player !== null);
-
     return (
-      <div className={cx('Player', `Player--${media.sourceType}`, `Player--${size}`)}>
-        {players}
-      </div>
+      <SourcePlayer
+        key={sourceType}
+        {...props}
+        active={media.sourceType === sourceType}
+      />
     );
-  }
+  }).filter(player => player !== null);
+
+  return (
+    <div className={cx('Player', `Player--${media.sourceType}`, `Player--${size}`)}>
+      {players}
+    </div>
+  );
 }
 
-export default enhance(Player);
+Player.propTypes = {
+  enabled: PropTypes.bool,
+  size: PropTypes.string,
+  volume: PropTypes.number,
+  isMuted: PropTypes.bool,
+  media: PropTypes.object,
+  seek: PropTypes.number,
+  onPlay: PropTypes.func,
+};
+
+export default Player;
