a52afaa0afdf933377303bd5cac95968f5257c54
Renée Kooi <renee@kooi.me>
2019-05-28 18:45:30 +0200

    Fix jss style insertion

--- tasks/utils/prerender.js
+++ tasks/utils/prerender.js
@@ -1,21 +1,18 @@
 /* eslint-disable global-require */
 const h = require('react').createElement;
 const { renderToStaticMarkup } = require('react-dom/server');
-const { ServerStyleSheets, ThemeProvider, jssPreset } = require('@material-ui/styles');
+const { ServerStyleSheets, ThemeProvider, StylesProvider } = require('@material-ui/styles');
 const { createMuiTheme } = require('@material-ui/core/styles');
-const { JssProvider, SheetsRegistry } = require('react-jss');
-const jss = require('jss');
 
 module.exports = function prerender(element) {
   const theme = require('../../src/theme').default;
   const createGenerateClassName = require('../../src/utils/createGenerateClassName').default;
 
-  const styles = jss.create(jssPreset());
   const generateClassName = createGenerateClassName();
   const sheets = new ServerStyleSheets();
 
   const renderElement = sheets.collect(h(
-    JssProvider, { jss: styles, generateClassName },
+    StylesProvider, { generateClassName },
     h(
       ThemeProvider, { theme: createMuiTheme(theme) },
       element,
