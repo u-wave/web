c678df8df4ac661b4e16b3b5715fb4e4e7355256
Renée Kooi <renee@kooi.me>
2019-07-25 16:12:46 +0200

    Use narrower PropType instead of React.Children

--- src/components/SettingsManager/LabeledControl.js
+++ src/components/SettingsManager/LabeledControl.js
@@ -1,18 +1,15 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 // The control is in `children`.
 /* eslint-disable jsx-a11y/label-has-for */
 const LabeledControl = ({ id, label, children }) => {
-  const control = React.Children.only(children);
-
   return (
     <div className="LabeledControl">
       <label className="LabeledControl-label" htmlFor={id}>{label}</label>
-      {React.cloneElement(control, {
+      {React.cloneElement(children, {
         id,
-        className: cx(control.className, 'LabeledControl-control'),
+        className: cx(children.className, 'LabeledControl-control'),
       })}
     </div>
   );
@@ -22,7 +19,7 @@ const LabeledControl = ({ id, label, children }) => {
 LabeledControl.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.element.isRequired,
 };
 
 export default LabeledControl;
