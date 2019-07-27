270c0d53e6af6582b3ec3d9218607e104c3fbdd8
Renée Kooi <renee@kooi.me>
2019-07-27 14:54:02 +0200

    Port admin Bans and Users components away from recompose.

--- src/admin/components/UsersList/index.js
+++ src/admin/components/UsersList/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { translate } from '@u-wave/react-translate';
+import { useTranslator } from '@u-wave/react-translate';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
@@ -11,52 +11,52 @@ import TableCell from '@material-ui/core/TableCell';
 import UserRow from './Row';
 import Header from './Header';
 
-const enhance = translate();
-
-const UsersList = ({
-  t,
+function UsersList({
   pageSize,
   currentPage,
   totalUsers,
   users,
   onChangePage,
   onFilter,
-}) => (
-  <React.Fragment>
-    <Header onFilter={onFilter} />
-    <Table>
-      <TableHead>
-        <TableRow className="AdminUserRow">
-          <TableCell className="AdminUserRow-avatar" />
-          <TableCell>{t('admin.users.user')}</TableCell>
-          <TableCell>{t('admin.users.joinedAt')}</TableCell>
-          <TableCell>{t('admin.users.email')}</TableCell>
-          <TableCell>{t('admin.users.roles')}</TableCell>
-          <TableCell />
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {users.map(user => (
-          <UserRow user={user} />
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TablePagination
-            count={totalUsers}
-            rowsPerPage={pageSize}
-            rowsPerPageOptions={[pageSize]}
-            page={currentPage}
-            onChangePage={onChangePage}
-          />
-        </TableRow>
-      </TableFooter>
-    </Table>
-  </React.Fragment>
-);
+}) {
+  const { t } = useTranslator();
+
+  return (
+    <React.Fragment>
+      <Header onFilter={onFilter} />
+      <Table>
+        <TableHead>
+          <TableRow className="AdminUserRow">
+            <TableCell className="AdminUserRow-avatar" />
+            <TableCell>{t('admin.users.user')}</TableCell>
+            <TableCell>{t('admin.users.joinedAt')}</TableCell>
+            <TableCell>{t('admin.users.email')}</TableCell>
+            <TableCell>{t('admin.users.roles')}</TableCell>
+            <TableCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map(user => (
+            <UserRow user={user} />
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              count={totalUsers}
+              rowsPerPage={pageSize}
+              rowsPerPageOptions={[pageSize]}
+              page={currentPage}
+              onChangePage={onChangePage}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </React.Fragment>
+  );
+}
 
 UsersList.propTypes = {
-  t: PropTypes.func.isRequired,
   pageSize: PropTypes.number.isRequired,
   currentPage: PropTypes.number.isRequired,
   totalUsers: PropTypes.number.isRequired,
@@ -65,4 +65,4 @@ UsersList.propTypes = {
   onFilter: PropTypes.func.isRequired,
 };
 
-export default enhance(UsersList);
+export default UsersList;
