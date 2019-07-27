6e4dd20388ea321467d8d6424726b19b8e360021
Renée Kooi <renee@kooi.me>
2019-07-27 16:48:49 +0200

    Port DeletePlaylistButton to hooks.

--- src/components/PlaylistManager/Panel/DeletePlaylistButton.js
+++ src/components/PlaylistManager/Panel/DeletePlaylistButton.js
@@ -1,79 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { translate } from '@u-wave/react-translate';
+import { useTranslator } from '@u-wave/react-translate';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ConfirmDialog from '../../Dialogs/ConfirmDialog';
 import FormGroup from '../../Form/Group';
 
-const enhance = translate();
+const {
+  useCallback,
+  useState,
+} = React;
 
-class DeletePlaylistButton extends React.Component {
-  static propTypes = {
-    t: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired,
-    onNotDeletable: PropTypes.func.isRequired,
-    active: PropTypes.bool,
-  };
+function DeletePlaylistButton({
+  onDelete,
+  onNotDeletable,
+  active,
+}) {
+  const { t } = useTranslator();
+  const [deleting, setDeleting] = useState(false);
 
-  state = {
-    deleting: false,
-  };
-
-  handleOpen = () => {
-    const { active, onNotDeletable } = this.props;
-
+  const handleOpen = useCallback(() => {
     if (active) {
       onNotDeletable();
     } else {
-      this.setState({ deleting: true });
+      setDeleting(true);
     }
-  };
+  }, [active, onNotDeletable]);
 
-  handleClose = () => {
-    this.closeDialog();
-  };
+  const handleClose = useCallback(() => {
+    setDeleting(false);
+  }, []);
 
-  handleConfirm = (name) => {
-    const { onDelete } = this.props;
+  const handleConfirm = useCallback(name => (
+    onDelete(name).then(() => {
+      setDeleting(false);
+    })
+  ), [onDelete]);
 
-    return onDelete(name)
-      .then(this.closeDialog.bind(this));
-  };
-
-  closeDialog() {
-    this.setState({ deleting: false });
-  }
-
-  render() {
-    const { t, active } = this.props;
-    const { deleting } = this.state;
-
-    return (
-      <React.Fragment>
-        <Tooltip title={active ? t('playlists.deleteActive') : t('playlists.delete')} placement="top">
-          <IconButton
-            disabled={active}
-            className="PlaylistMeta-iconButton"
-            onClick={this.handleOpen}
-          >
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-        {deleting && (
-          <ConfirmDialog
-            title={t('dialogs.deletePlaylist.title')}
-            confirmLabel={t('dialogs.deletePlaylist.action')}
-            onConfirm={this.handleConfirm}
-            onCancel={this.handleClose}
-          >
-            <FormGroup>{t('dialogs.deletePlaylist.confirm')}</FormGroup>
-          </ConfirmDialog>
-        )}
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Tooltip title={active ? t('playlists.deleteActive') : t('playlists.delete')} placement="top">
+        <IconButton
+          disabled={active}
+          className="PlaylistMeta-iconButton"
+          onClick={handleOpen}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+      {deleting && (
+        <ConfirmDialog
+          title={t('dialogs.deletePlaylist.title')}
+          confirmLabel={t('dialogs.deletePlaylist.action')}
+          onConfirm={handleConfirm}
+          onCancel={handleClose}
+        >
+          <FormGroup>{t('dialogs.deletePlaylist.confirm')}</FormGroup>
+        </ConfirmDialog>
+      )}
+    </React.Fragment>
+  );
 }
 
-export default enhance(DeletePlaylistButton);
+DeletePlaylistButton.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  onNotDeletable: PropTypes.func.isRequired,
+  active: PropTypes.bool.isRequired,
+};
+
+export default DeletePlaylistButton;
