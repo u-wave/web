fdc5fab5394a88da8c0822f6f6f61adf99a89f21
Renée Kooi <renee@kooi.me>
2019-07-27 14:08:55 +0200

    Port room history components to hooks.

--- src/components/RoomHistory/index.js
+++ src/components/RoomHistory/index.js
@@ -1,44 +1,44 @@
 import cx from 'clsx';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { translate } from '@u-wave/react-translate';
+import { useTranslator } from '@u-wave/react-translate';
 import OverlayHeader from '../Overlay/Header';
 import OverlayContent from '../Overlay/Content';
 import HistoryList from './HistoryList';
 
-const enhance = translate();
-
-const RoomHistory = ({
-  t,
+function RoomHistory({
   className,
   onCloseOverlay,
   onOpenAddMediaMenu,
   onOpenPreviewMediaDialog,
   ...props
-}) => (
-  <div className={cx('RoomHistory', className)}>
-    <OverlayHeader
-      direction="top"
-      className="AppRow AppRow--top"
-      title={t('history.title')}
-      onCloseOverlay={onCloseOverlay}
-    />
-    <OverlayContent className="RoomHistory-body">
-      <HistoryList
-        onOpenAddMediaMenu={onOpenAddMediaMenu}
-        onOpenPreviewMediaDialog={onOpenPreviewMediaDialog}
-        {...props}
+}) {
+  const { t } = useTranslator();
+
+  return (
+    <div className={cx('RoomHistory', className)}>
+      <OverlayHeader
+        direction="top"
+        className="AppRow AppRow--top"
+        title={t('history.title')}
+        onCloseOverlay={onCloseOverlay}
       />
-    </OverlayContent>
-  </div>
-);
+      <OverlayContent className="RoomHistory-body">
+        <HistoryList
+          onOpenAddMediaMenu={onOpenAddMediaMenu}
+          onOpenPreviewMediaDialog={onOpenPreviewMediaDialog}
+          {...props}
+        />
+      </OverlayContent>
+    </div>
+  );
+}
 
 RoomHistory.propTypes = {
-  t: PropTypes.func.isRequired,
   className: PropTypes.string,
   onCloseOverlay: PropTypes.func.isRequired,
   onOpenAddMediaMenu: PropTypes.func.isRequired,
   onOpenPreviewMediaDialog: PropTypes.func.isRequired,
 };
 
-export default enhance(RoomHistory);
+export default RoomHistory;
