af11b6e81a6af4f046dbb00b3687bc36427cacc1
Renée Kooi <renee@kooi.me>
2019-07-27 23:31:58 +0200

    Turn MediaRow into a function component.

--- src/components/MediaList/Row.js
+++ src/components/MediaList/Row.js
@@ -9,6 +9,11 @@ import MediaLoadingIndicator from './MediaLoadingIndicator';
 import MediaThumbnail from './MediaThumbnail';
 import Actions from './Actions';
 
+const {
+  useCallback,
+  useEffect,
+} = React;
+
 const inSelection = (selection, media) => selection.some(item => item._id === media._id);
 
 const mediaSource = {
@@ -26,95 +31,84 @@ const collect = connect => ({
 
 const enhance = DragSource(MEDIA, mediaSource, collect);
 
-class Row extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    connectDragSource: PropTypes.func.isRequired,
-    connectDragPreview: PropTypes.func.isRequired,
-    media: PropTypes.object,
-    selected: PropTypes.bool,
-    selection: PropTypes.array,
-
-    onOpenPreviewMediaDialog: PropTypes.func,
-    onClick: PropTypes.func,
-    makeActions: PropTypes.func,
-  };
-
-  static defaultProps = {
-    selected: false,
-  };
-
-  componentDidMount() {
-    const { connectDragPreview } = this.props;
-    connectDragPreview(getEmptyImage());
-  }
-
-  handleKeyPress = (event) => {
-    const { onClick } = this.props;
+function Row({
+  className,
+  media,
+  selected = false,
+  selection,
+  connectDragSource,
+  connectDragPreview,
+  onClick,
+  onOpenPreviewMediaDialog,
+  makeActions,
+}) {
+  const handleKeyPress = useCallback((event) => {
     if (event.code === 'Space') {
       onClick();
     }
-  };
+  }, [onClick]);
 
-  handleDoubleClick = () => {
-    const { onOpenPreviewMediaDialog, media } = this.props;
+  const handleDoubleClick = useCallback(() => {
     onOpenPreviewMediaDialog(media);
-  };
+  }, [onOpenPreviewMediaDialog, media]);
 
-  render() {
-    const {
-      className,
-      media,
-      selection,
-      selected,
-      connectDragSource,
-      // actions
-      makeActions,
-      // etc
-      onClick,
-    } = this.props;
-    const selectedClass = selected ? 'is-selected' : '';
-    const loadingClass = media.loading ? 'is-loading' : '';
-    const duration = 'start' in media
-      // playlist item
-      ? media.end - media.start
-      // search result
-      : media.duration;
+  useEffect(() => {
+    connectDragPreview(getEmptyImage());
+  }, []);
 
-    return connectDragSource((
-      // Bit uneasy about this, but turning the entire row into a button seems
-      // wrong as well! Since we nest media action <button>s inside it, too.
-      //
-      // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-      <div
-        className={cx('MediaListRow', className, selectedClass, loadingClass)}
-        onDoubleClick={this.handleDoubleClick}
-        onKeyPress={this.handleKeyPress}
-        onClick={onClick}
-      >
-        {media.loading ? (
-          <MediaLoadingIndicator className="MediaListRow-loader" />
-        ) : (
-          <MediaThumbnail url={media.thumbnail} />
-        )}
-        <div className="MediaListRow-artist" title={media.artist}>
-          {media.artist}
-        </div>
-        <div className="MediaListRow-title" title={media.title}>
-          {media.title}
-        </div>
-        <div className="MediaListRow-duration">
-          {formatDuration(duration * 1000)}
-        </div>
-        <Actions
-          className={cx('MediaListRow-actions', selectedClass)}
-          selection={selection}
-          media={media}
-          makeActions={makeActions}
-        />
+  const selectedClass = selected ? 'is-selected' : '';
+  const loadingClass = media.loading ? 'is-loading' : '';
+  const duration = 'start' in media
+    // playlist item
+    ? media.end - media.start
+    // search result
+    : media.duration;
+
+  return connectDragSource((
+    // Bit uneasy about this, but turning the entire row into a button seems
+    // wrong as well! Since we nest media action <button>s inside it, too.
+    //
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      className={cx('MediaListRow', className, selectedClass, loadingClass)}
+      onDoubleClick={handleDoubleClick}
+      onKeyPress={handleKeyPress}
+      onClick={onClick}
+    >
+      {media.loading ? (
+        <MediaLoadingIndicator className="MediaListRow-loader" />
+      ) : (
+        <MediaThumbnail url={media.thumbnail} />
+      )}
+      <div className="MediaListRow-artist" title={media.artist}>
+        {media.artist}
       </div>
-    ));
-  }
+      <div className="MediaListRow-title" title={media.title}>
+        {media.title}
+      </div>
+      <div className="MediaListRow-duration">
+        {formatDuration(duration * 1000)}
+      </div>
+      <Actions
+        className={cx('MediaListRow-actions', selectedClass)}
+        selection={selection}
+        media={media}
+        makeActions={makeActions}
+      />
+    </div>
+  ));
 }
+
+Row.propTypes = {
+  className: PropTypes.string,
+  media: PropTypes.object,
+  selected: PropTypes.bool,
+  selection: PropTypes.array,
+  connectDragSource: PropTypes.func.isRequired,
+  connectDragPreview: PropTypes.func.isRequired,
+  onOpenPreviewMediaDialog: PropTypes.func,
+  onClick: PropTypes.func,
+  makeActions: PropTypes.func,
+};
 
 export default enhance(Row);
