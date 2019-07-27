91c61c6fc2e02f8c377f1429af2a0d534371c078
Renée Kooi <renee@kooi.me>
2019-07-27 14:57:45 +0200

    Port PlaylistManager container to hooks.

--- src/containers/PlaylistManager.js
+++ src/containers/PlaylistManager.js
@@ -1,31 +1,39 @@
-import compose from 'recompose/compose';
-import { connect } from 'react-redux';
-import { translate } from '@u-wave/react-translate';
-import { createStructuredSelector } from 'reselect';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslator } from '@u-wave/react-translate';
 import { selectedPlaylistSelector } from '../selectors/playlistSelectors';
 import { showSearchResultsSelector } from '../selectors/searchSelectors';
 import { showImportPanelSelector } from '../selectors/importSelectors';
 import { createPlaylist } from '../actions/PlaylistActionCreators';
 import createLazyOverlay from '../components/LazyOverlay';
 
-const mapStateToProps = createStructuredSelector({
-  selectedPlaylist: selectedPlaylistSelector,
-  showImportPanel: showImportPanelSelector,
-  showSearchResults: showSearchResultsSelector,
-});
-
-const mapDispatchToProps = (dispatch, { t }) => ({
-  onCreatePlaylist: () => dispatch(createPlaylist(t('playlists.defaultName'))),
-});
-
-const enhance = compose(
-  translate(),
-  connect(mapStateToProps, mapDispatchToProps),
-);
+const { useCallback } = React;
 
 const PlaylistManager = createLazyOverlay({
   loader: () => import('../components/PlaylistManager' /* webpackChunkName: "playlists" */),
   title: t => t('playlists.title'),
 });
 
-export default enhance(PlaylistManager);
+function PlaylistManagerContainer() {
+  const { t } = useTranslator();
+
+  const selectedPlaylist = useSelector(selectedPlaylistSelector);
+  const showImportPanel = useSelector(showImportPanelSelector);
+  const showSearchResults = useSelector(showSearchResultsSelector);
+  const dispatch = useDispatch();
+
+  const onCreatePlaylist = useCallback(() => (
+    dispatch(createPlaylist(t('playlists.defaultName')))
+  ), [dispatch, t]);
+
+  return (
+    <PlaylistManager
+      selectedPlaylist={selectedPlaylist}
+      showImportPanel={showImportPanel}
+      showSearchResults={showSearchResults}
+      onCreatePlaylist={onCreatePlaylist}
+    />
+  );
+}
+
+export default PlaylistManagerContainer;
