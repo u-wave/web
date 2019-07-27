a796eabcff5774f159f0f147e710e2cf8bbacefd
Renée Kooi <renee@kooi.me>
2019-07-27 23:09:02 +0200

    Port PlaylistRow to hooks.

--- src/components/PlaylistManager/Menu/Row.js
+++ src/components/PlaylistManager/Menu/Row.js
@@ -1,89 +1,74 @@
 import cx from 'clsx';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { DropTarget } from 'react-dnd';
+import { useDrop } from 'react-dnd';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import MenuItem from '@material-ui/core/MenuItem';
 import ActiveIcon from '@material-ui/icons/Check';
 import { MEDIA } from '../../../constants/DDItemTypes';
-
-const playlistTarget = {
-  drop({ playlist, onAddToPlaylist }, monitor) {
-    const { media } = monitor.getItem();
-    onAddToPlaylist(playlist, media);
-  },
-};
-
-const collect = (connect, monitor) => ({
-  connectDropTarget: connect.dropTarget(),
-  isOver: monitor.isOver(),
-});
-
-const enhance = DropTarget(MEDIA, playlistTarget, collect);
 
 const itemClasses = {
   root: 'PlaylistMenuRow',
   selected: 'is-selected',
 };
 
-class PlaylistRow extends React.Component {
-  static propTypes = {
-    playlist: PropTypes.object,
-    selected: PropTypes.bool,
-    isOver: PropTypes.bool.isRequired,
+function PlaylistRow({
+  playlist,
+  selected,
+  onClick,
+  onAddToPlaylist,
+}) {
+  const [{ isOver }, drop] = useDrop({
+    accept: MEDIA,
+    drop(item, monitor) {
+      const { media } = monitor.getItem();
+      onAddToPlaylist(playlist, media);
+    },
+    collect(monitor) {
+      return { isOver: monitor.isOver() };
+    },
+  });
 
-    connectDropTarget: PropTypes.func.isRequired,
-    onClick: PropTypes.func,
-    // Used in the drop handler above 👆
-    // eslint-disable-next-line react/no-unused-prop-types
-    onAddToPlaylist: PropTypes.func,
-  };
+  const activeClass = playlist.active && 'is-active';
+  const droppableClass = isOver && 'is-droppable';
 
-  render() {
-    const {
-      playlist,
-      selected,
-
-      onClick,
-
-      connectDropTarget,
-      isOver,
-    } = this.props;
-    const activeClass = playlist.active && 'is-active';
-    const droppableClass = isOver && 'is-droppable';
-
-    let icon;
-    if (playlist.creating) {
-      icon = (
-        <div className="PlaylistMenuRow-loading">
-          <CircularProgress size="100%" />
-        </div>
-      );
-    } else if (playlist.active) {
-      icon = (
-        <div className="PlaylistMenuRow-active-icon">
-          <ActiveIcon />
-        </div>
-      );
-    }
-
-    return connectDropTarget((
-      <div>
-        <MenuItem
-          selected={selected}
-          className={cx(activeClass, droppableClass)}
-          classes={itemClasses}
-          onClick={onClick}
-        >
-          <div className="PlaylistMenuRow-title">
-            {icon}
-            {playlist.name}
-          </div>
-          <div className="PlaylistMenuRow-count">{playlist.size}</div>
-        </MenuItem>
+  let icon;
+  if (playlist.creating) {
+    icon = (
+      <div className="PlaylistMenuRow-loading">
+        <CircularProgress size="100%" />
       </div>
-    ));
+    );
+  } else if (playlist.active) {
+    icon = (
+      <div className="PlaylistMenuRow-active-icon">
+        <ActiveIcon />
+      </div>
+    );
   }
+
+  return (
+    <MenuItem
+      selected={selected}
+      className={cx(activeClass, droppableClass)}
+      classes={itemClasses}
+      onClick={onClick}
+      ref={drop}
+    >
+      <div className="PlaylistMenuRow-title">
+        {icon}
+        {playlist.name}
+      </div>
+      <div className="PlaylistMenuRow-count">{playlist.size}</div>
+    </MenuItem>
+  );
 }
 
-export default enhance(PlaylistRow);
+PlaylistRow.propTypes = {
+  playlist: PropTypes.object,
+  selected: PropTypes.bool,
+  onClick: PropTypes.func,
+  onAddToPlaylist: PropTypes.func,
+};
+
+export default PlaylistRow;
