171a566d48c8fad8f3798b7decddbd96f948c65c
Renée Kooi <renee@kooi.me>
2019-07-28 12:37:38 +0200

    Port RoomHistory Row to hooks.

--- src/components/RoomHistory/Row.js
+++ src/components/RoomHistory/Row.js
@@ -1,147 +1,123 @@
 import cx from 'clsx';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { DragSource } from 'react-dnd';
+import { useDrag } from 'react-dnd';
 import { getEmptyImage } from 'react-dnd-html5-backend';
 import { MEDIA } from '../../constants/DDItemTypes';
-
 import Actions from '../MediaList/Actions';
 import SongTitle from '../SongTitle';
 import TimeAgo from '../TimeAgo';
-
 import HistoryVotes from './Votes';
+
+const {
+  useCallback,
+  useEffect,
+  useState,
+} = React;
 
 const inSelection = (selection, media) => selection.some(item => item._id === media._id);
 
-const mediaSource = {
-  beginDrag({ selection, media }) {
-    return {
-      media: inSelection(selection, media.media) ? selection : [media.media],
-    };
-  },
-};
+function HistoryRow({
+  className,
+  media: historyEntry,
+  selected = false,
+  selection,
+  onOpenPreviewMediaDialog,
+  onClick,
+  makeActions,
+}) {
+  const {
+    media, timestamp, user, stats,
+  } = historyEntry;
 
-const collect = connect => ({
-  connectDragSource: connect.dragSource(),
-  connectDragPreview: connect.dragPreview(),
-});
+  const [showActions, setShowActions] = useState(false);
+  const [, drag, connectDragPreview] = useDrag({
+    item: {
+      type: MEDIA,
+      media: inSelection(selection, media) ? selection : [media],
+    },
+  });
 
-const enhance = DragSource(MEDIA, mediaSource, collect);
-
-class HistoryRow extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    connectDragSource: PropTypes.func.isRequired,
-    connectDragPreview: PropTypes.func.isRequired,
-    media: PropTypes.object,
-    selected: PropTypes.bool,
-    selection: PropTypes.array,
-
-    onOpenPreviewMediaDialog: PropTypes.func,
-    onClick: PropTypes.func,
-    makeActions: PropTypes.func,
-  };
-
-  static defaultProps = {
-    selected: false,
-  };
-
-  state = { showActions: false };
-
-  componentDidMount() {
-    const { connectDragPreview } = this.props;
-
+  useEffect(() => {
     connectDragPreview(getEmptyImage());
-  }
+  }, []);
 
-  handleMouseEnter = () => {
-    this.setState({ showActions: true });
-  };
+  const handleMouseEnter = useCallback(() => {
+    setShowActions(true);
+  }, []);
 
-  handleMouseLeave = () => {
-    this.setState({ showActions: false });
-  };
+  const handleMouseLeave = useCallback(() => {
+    setShowActions(false);
+  }, []);
 
-  handleDoubleClick = () => {
-    const { onOpenPreviewMediaDialog, media } = this.props;
+  const handleDoubleClick = useCallback(() => {
+    onOpenPreviewMediaDialog(media);
+  }, [onOpenPreviewMediaDialog, media]);
 
-    onOpenPreviewMediaDialog(media.media);
-  };
-
-  handleKeyPress = (event) => {
-    const { onClick } = this.props;
-
-    if (event.code === 'Space') {
+  const handleKeyPress = useCallback((event) => {
+    if (event.key === 'Space') {
       onClick();
     }
-  };
+  }, [onClick]);
 
-  render() {
-    const {
-      media: historyEntry,
-      className,
-      selection,
-      selected,
-      connectDragSource,
-      // actions
-      makeActions,
-      // etc
-      onClick,
-    } = this.props;
-    const { showActions } = this.state;
-
-    const {
-      media, timestamp, user, stats,
-    } = historyEntry;
-
-    const selectedClass = selected ? 'is-selected' : '';
-    const thumbnail = (
-      <div className="MediaListRow-thumb">
-        <img
-          className="MediaListRow-image"
-          src={media.thumbnail}
-          alt=""
-        />
+  const selectedClass = selected ? 'is-selected' : '';
+  const thumbnail = (
+    <div className="MediaListRow-thumb">
+      <img
+        className="MediaListRow-image"
+        src={media.thumbnail}
+        alt=""
+      />
+    </div>
+  );
+  return (
+    // See PlaylistManager/Panel/Row.js
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      className={cx('MediaListRow', 'HistoryRow', className, selectedClass)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onDoubleClick={handleDoubleClick}
+      onKeyPress={handleKeyPress}
+      onClick={onClick}
+      ref={drag}
+    >
+      {thumbnail}
+      <SongTitle
+        className="HistoryRow-song"
+        size="mediaRow"
+        artist={media.artist}
+        title={media.title}
+      />
+      <div className="HistoryRow-votes">
+        <HistoryVotes {...stats} />
       </div>
-    );
-    return connectDragSource((
-      // See PlaylistManager/Panel/Row.js
-      // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-      <div
-        className={cx('MediaListRow', 'HistoryRow', className, selectedClass)}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-        onDoubleClick={this.handleDoubleClick}
-        onKeyPress={this.handleKeyPress}
-        onClick={onClick}
-      >
-        {thumbnail}
-        <SongTitle
-          className="HistoryRow-song"
-          size="mediaRow"
-          artist={media.artist}
-          title={media.title}
-        />
-        <div className="HistoryRow-votes">
-          <HistoryVotes {...stats} />
-        </div>
-        <div className="HistoryRow-user">
-          {user.username}
-        </div>
-        <div className="HistoryRow-time" dateTime={timestamp}>
-          <TimeAgo timestamp={timestamp} />
-        </div>
-        {showActions && (
-          <Actions
-            className={cx('MediaListRow-actions', selectedClass)}
-            selection={selection}
-            media={media}
-            makeActions={makeActions}
-          />
-        )}
+      <div className="HistoryRow-user">
+        {user.username}
       </div>
-    ));
-  }
+      <div className="HistoryRow-time" dateTime={timestamp}>
+        <TimeAgo timestamp={timestamp} />
+      </div>
+      {showActions && (
+        <Actions
+          className={cx('MediaListRow-actions', selectedClass)}
+          selection={selection}
+          media={media}
+          makeActions={makeActions}
+        />
+      )}
+    </div>
+  );
 }
 
-export default enhance(HistoryRow);
+HistoryRow.propTypes = {
+  className: PropTypes.string,
+  media: PropTypes.object.isRequired,
+  selected: PropTypes.bool,
+  selection: PropTypes.arrayOf(PropTypes.object),
+  onOpenPreviewMediaDialog: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+  makeActions: PropTypes.func.isRequired,
+};
+
+export default HistoryRow;
