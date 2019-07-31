a37528c554b8d8580934d1cfbe4fffb78925a948
Renée Kooi <renee@kooi.me>
2019-07-31 13:56:29 +0200

    Remove uses of ReactDOM.findDOMNode.

--- src/components/RoomUserList/index.js
+++ src/components/RoomUserList/index.js
@@ -1,6 +1,5 @@
 import cx from 'clsx';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import MuiList from '@material-ui/core/List';
 import List from 'react-list';
@@ -14,42 +13,44 @@ const RoomUserList = ({ className, users, guests }) => {
   // an element of the list--so we tell react-list that we have an extra row
   // when th guests row is shown.
   const length = users.length + (showGuests ? 1 : 0);
+
+  function itemsRenderer(children, ref) {
+    return (
+      <MuiList disablePadding ref={ref}>
+        {children}
+      </MuiList>
+    );
+  }
+
+  function itemRenderer(index, key) {
+    const rowClass = cx(
+      'UserList-row',
+      (index % 2 === 0) && 'UserList-row--alternate',
+    );
+    // The very last row is the guests row
+    if (index === users.length) {
+      return (
+        <GuestsRow
+          key={key}
+          className={rowClass}
+          guests={guests}
+        />
+      );
+    }
+    return (
+      <RoomUserRow
+        key={key}
+        className={rowClass}
+        user={users[index]}
+      />
+    );
+  }
+
   return (
     <div className={cx('UserList', 'UserList--online', className)}>
       <List
-        itemsRenderer={(children, ref) => (
-          <MuiList
-            disablePadding
-            ref={(list) => {
-              ref(list && ReactDOM.findDOMNode(list)); // eslint-disable-line react/no-find-dom-node
-            }}
-          >
-            {children}
-          </MuiList>
-        )}
-        itemRenderer={(index, key) => {
-          const rowClass = cx(
-            'UserList-row',
-            (index % 2 === 0) && 'UserList-row--alternate',
-          );
-          // The very last row is the guests row
-          if (index === users.length) {
-            return (
-              <GuestsRow
-                key={key}
-                className={rowClass}
-                guests={guests}
-              />
-            );
-          }
-          return (
-            <RoomUserRow
-              key={key}
-              className={rowClass}
-              user={users[index]}
-            />
-          );
-        }}
+        itemsRenderer={itemsRenderer}
+        itemRenderer={itemRenderer}
         length={length}
         type="uniform"
       />
--- src/utils/isDraggingNearTopOfRow.js
+++ src/utils/isDraggingNearTopOfRow.js
@@ -1,10 +1,5 @@
-import { findDOMNode } from 'react-dom';
-
 export default function isDraggingNearTopOfRow(monitor, component) {
-  // FIXME do this without findDOMNode, perhaps by using refs in the calling
-  // components.
-  // eslint-disable-next-line react/no-find-dom-node
-  const componentRect = findDOMNode(component).getBoundingClientRect();
+  const componentRect = component.getBoundingClientRect();
   const clientOffset = monitor.getClientOffset();
 
   const middle = Math.ceil(componentRect.height / 2);
