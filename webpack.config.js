85a18b91933c44f82fdd48f67cc7b7635f4023f7
Renée Kooi <renee@kooi.me>
2019-10-31 14:45:03 +0100

    Add `path` Node.js module shim.

--- webpack.config.js
+++ webpack.config.js
@@ -174,6 +174,8 @@ const base = {
   },
   resolve: {
     alias: {
+      // Node.js shims
+      path: 'path-browserify',
       // Use the ES modules versions of some packages.
       '@material-ui/core': path.join(__dirname, 'node_modules/@material-ui/core/esm/'),
       '@material-ui/icons': path.join(__dirname, 'node_modules/@material-ui/icons/esm/'),
