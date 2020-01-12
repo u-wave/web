040db7a93e42ea83c6dd08fbf06301c37e5a0476
Renée Kooi <renee@kooi.me>
2020-01-12 12:43:13 +0100

    build: output cjs from file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -124,13 +124,13 @@ const base = {
       {
         test: /\.mp3$/,
         use: [
-          { loader: 'file-loader', options: { name: 'static/[name]_[hash:7].[ext]' } },
+          { loader: 'file-loader', options: { esModule: false, name: 'static/[name]_[hash:7].[ext]' } },
         ],
       },
       {
         test: /\.(gif|jpe?g|png|svg)$/,
         use: [
-          { loader: 'file-loader', options: { name: 'static/[name]_[hash:7].[ext]' } },
+          { loader: 'file-loader', options: { esModule: false, name: 'static/[name]_[hash:7].[ext]' } },
           nodeEnv !== 'development' && { loader: 'image-webpack-loader' },
         ].filter(Boolean),
       },
