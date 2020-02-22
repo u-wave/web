94c5078a0ec934faddc9788e563b5e2ed85ef6ef
Renée Kooi <renee@kooi.me>
2020-02-22 15:27:57 +0100

    build: remove unused babel plugins

--- .babelrc.js
+++ .babelrc.js
@@ -23,12 +23,10 @@ module.exports = (api, envOverride) => {
         modules: false,
         loose,
         targets,
-        shippedProposals: true,
       }],
       '@babel/preset-react',
     ],
     plugins: [
-      '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-proposal-export-default-from',
       '@babel/plugin-proposal-export-namespace-from',
       ['@babel/plugin-proposal-class-properties', { loose }],
