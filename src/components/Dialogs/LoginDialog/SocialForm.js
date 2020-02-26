38e41eac4bb555b5b9b642a356c20e5db47063a1
Renée Kooi <renee@kooi.me>
2020-02-26 10:34:45 +0100

    Refactor social signup form to use Hooks.

--- src/components/Dialogs/LoginDialog/SocialForm.js
+++ src/components/Dialogs/LoginDialog/SocialForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { translate, Interpolate } from '@u-wave/react-translate';
+import { useTranslator, Interpolate } from '@u-wave/react-translate';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -15,7 +15,11 @@ import FormGroup from '../../Form/Group';
 import TextField from '../../Form/TextField';
 import Button from '../../Form/Button';
 
-const enhance = translate();
+const {
+  useCallback,
+  useRef,
+  useState,
+} = React;
 
 function AvatarList({ avatars, selected, onChange }) {
   const avatarNames = Object.keys(avatars);
@@ -54,131 +58,106 @@ AvatarList.propTypes = {
   onChange: PropTypes.func.isRequired,
 };
 
-class SocialForm extends React.Component {
-  static propTypes = {
-    t: PropTypes.func.isRequired,
-    error: PropTypes.object,
-    service: PropTypes.string.isRequired,
-    avatars: PropTypes.objectOf(PropTypes.string).isRequired,
-    suggestedName: PropTypes.string,
+function SocialForm({
+  error,
+  service,
+  avatars,
+  suggestedName = '',
+  onSocialFinish,
+}) {
+  const { t } = useTranslator();
+  const [isBusy, setBusy] = useState(false);
+  const [agreed, setAgreed] = useState(false);
+  const [avatar, setAvatar] = useState('sigil');
+  const refUsername = useRef(null);
 
-    onSocialFinish: PropTypes.func.isRequired,
-  };
-
-  static defaultProps = {
-    suggestedName: '',
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      busy: false,
-      agreed: false,
-      avatar: 'sigil',
-    };
-  }
-
-  handleSubmit = (event) => {
-    const { service, onSocialFinish } = this.props;
-    const { avatar } = this.state;
-
+  const handleSubmit = useCallback((event) => {
     event.preventDefault();
-    this.setState({ busy: true });
+    setBusy(true);
 
     onSocialFinish(service, {
       avatar,
-      username: this.username.value,
+      username: refUsername.current.value,
     }).finally(() => {
-      this.setState({ busy: false });
+      setBusy(false);
     });
-  };
+  }, [avatar, service, onSocialFinish]);
 
-  handleTosCheckbox = (event, checked) => {
-    this.setState({
-      agreed: checked,
-    });
-  };
+  const handleTosCheckbox = useCallback((event, checked) => {
+    setAgreed(checked);
+  }, []);
 
-  handleChangeAvatar = (name) => {
-    this.setState({
-      avatar: name,
-    });
-  };
+  const handleChangeAvatar = useCallback((name) => {
+    setAvatar(name);
+  }, []);
 
-  refUsername = (username) => {
-    this.username = username;
-  };
+  return (
+    <Form className="RegisterForm" onSubmit={handleSubmit}>
+      {error && <FormGroup>{error.message}</FormGroup>}
+      <FormGroup>
+        <TextField
+          ref={refUsername}
+          className="RegisterForm-field"
+          autocomplete="nickname"
+          defaultValue={suggestedName}
+          placeholder={t('login.username')}
+          icon={<UserIcon nativeColor="#9f9d9e" />}
+          autoFocus
+        />
+      </FormGroup>
 
-  render() {
-    const {
-      t,
-      error,
-      avatars,
-      suggestedName,
-    } = this.props;
-    const { agreed, avatar, busy } = this.state;
-
-    return (
-      <Form className="RegisterForm" onSubmit={this.handleSubmit}>
-        {error && <FormGroup>{error.message}</FormGroup>}
-        <FormGroup>
-          <TextField
-            ref={this.refUsername}
-            className="RegisterForm-field"
-            autocomplete="nickname"
-            defaultValue={suggestedName}
-            placeholder={t('login.username')}
-            icon={<UserIcon nativeColor="#9f9d9e" />}
-            autoFocus
+      <FormGroup>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Avatar</FormLabel>
+          <AvatarList
+            avatars={avatars}
+            selected={avatar}
+            onChange={handleChangeAvatar}
           />
-        </FormGroup>
+        </FormControl>
+      </FormGroup>
 
-        <FormGroup>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Avatar</FormLabel>
-            <AvatarList
-              avatars={avatars}
-              selected={avatar}
-              onChange={this.handleChangeAvatar}
+      <FormGroup>
+        <FormControlLabel
+          control={(
+            <Checkbox
+              checked={agreed}
+              onChange={handleTosCheckbox}
             />
-          </FormControl>
-        </FormGroup>
+          )}
+          label={(
+            <Interpolate
+              i18nKey="login.agree"
+              privacyPolicy={(
+                <a target="_blank" rel="noreferrer noopener" href="/privacy.html">
+                  {t('login.privacyPolicy')}
+                </a>
+              )}
+            />
+          )}
+        />
+      </FormGroup>
 
-        <FormGroup>
-          <FormControlLabel
-            control={(
-              <Checkbox
-                checked={agreed}
-                onChange={this.handleTosCheckbox}
-              />
-            )}
-            label={(
-              <Interpolate
-                i18nKey="login.agree"
-                privacyPolicy={(
-                  <a target="_blank" rel="noreferrer noopener" href="/privacy.html">
-                    {t('login.privacyPolicy')}
-                  </a>
-                )}
-              />
-            )}
-          />
-        </FormGroup>
-
-        <FormGroup>
-          <Button
-            className="RegisterForm-submit"
-            disabled={busy || !agreed}
-          >
-            {busy
-              ? <div className="Button-loading"><CircularProgress size="100%" /></div>
-              : t('login.register')}
-          </Button>
-        </FormGroup>
-      </Form>
-    );
-  }
+      <FormGroup>
+        <Button
+          className="RegisterForm-submit"
+          disabled={isBusy || !agreed}
+        >
+          {isBusy
+            ? <div className="Button-loading"><CircularProgress size="100%" /></div>
+            : t('login.register')}
+        </Button>
+      </FormGroup>
+    </Form>
+  );
 }
 
-export default enhance(SocialForm);
+SocialForm.propTypes = {
+  error: PropTypes.object,
+  service: PropTypes.string.isRequired,
+  avatars: PropTypes.objectOf(PropTypes.string).isRequired,
+  suggestedName: PropTypes.string,
+  onSocialFinish: PropTypes.func.isRequired,
+};
+
+export default SocialForm;
