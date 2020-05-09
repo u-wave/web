a5dbc0d64478b5e97ed28b7ca034630fb2f36e64
Renée Kooi <renee@kooi.me>
2020-05-09 17:22:09 +0200

    Use `useTranslator()` hook instead of HOC in CurrentDJ, CurrentMedia

--- src/components/HeaderBar/CurrentDJ.js
+++ src/components/HeaderBar/CurrentDJ.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { translate } from '@u-wave/react-translate';
+import { useTranslator } from '@u-wave/react-translate';
 
-const CurrentDJ = ({ t, className, dj }) => (
-  <div className={className}>
-    {t('booth.currentDJ', { user: dj.username })}
-  </div>
-);
+function CurrentDJ({ className, dj }) {
+  const { t } = useTranslator();
+
+  return (
+    <div className={className}>
+      {t('booth.currentDJ', { user: dj.username })}
+    </div>
+  );
+}
 
 CurrentDJ.propTypes = {
-  t: PropTypes.func.isRequired,
   className: PropTypes.string,
   dj: PropTypes.shape({
     username: PropTypes.string.isRequired,
   }),
 };
 
-export default translate()(CurrentDJ);
+export default CurrentDJ;
