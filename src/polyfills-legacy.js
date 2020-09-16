0601fd71f483334375b90389fc48cf8d9a8efbfa
Renée Kooi <renee@kooi.me>
2020-09-16 16:30:33 +0200

    use String.prototype.includes polyfill, use /auto entry point for es-shims

--- src/polyfills-legacy.js
+++ src/polyfills-legacy.js
@@ -1,31 +1,17 @@
 // Polyfills for browsers all the way down to IE11.
+import 'array.prototype.find/auto';
+import 'array.prototype.findindex/auto';
+import 'array-includes/auto';
+import 'object.assign/auto';
+import 'object-is/auto';
+import 'object.values/auto';
+import 'is-nan/auto';
+import 'string.prototype.includes/auto';
 import 'es6-promise/auto';
 import 'url-polyfill';
 import 'whatwg-fetch';
 import 'abortcontroller-polyfill/src/polyfill';
-import arrayFind from 'array.prototype.find';
-import arrayFindIndex from 'array.prototype.findindex';
-import arrayIncludes from 'array-includes';
-import objectAssign from 'object.assign';
-import objectIs from 'object-is';
-import objectValues from 'object.values';
-import numberIsNaN from 'is-nan';
 import numberIsFinite from 'is-finite';
-
-arrayFind.shim();
-arrayFindIndex.shim();
-arrayIncludes.shim();
-objectAssign.shim();
-objectIs.shim();
-objectValues.shim();
-numberIsNaN.shim();
-
-if (!String.prototype.includes) {
-  // eslint-disable-next-line no-extend-native
-  String.prototype.includes = function includes(substring) {
-    return this.indexOf(substring) !== -1;
-  };
-}
 
 if (!Number.isFinite) {
   // eslint-disable-next-line no-extend-native
