b77655c6294345f3ee37792edc0f52e55120c580
Renée Kooi <renee@kooi.me>
2020-09-17 10:52:22 +0200

    [temporary] use a very bad symbol polyfill

--- src/polyfills-legacy.js
+++ src/polyfills-legacy.js
@@ -8,11 +8,26 @@ import 'object.values/auto';
 import 'is-nan/auto';
 import 'string.prototype.includes/auto';
 import 'es6-promise/auto';
-import 'es6-symbol/implement';
+// import 'es6-symbol/implement';
 import 'url-polyfill';
 import 'whatwg-fetch';
 import 'abortcontroller-polyfill/src/polyfill';
 import numberIsFinite from 'is-finite';
+
+// Very bad symbol polyfill until webpack is fixed and es6-symbol can be used.
+if (!window.Symbol) {
+  let si = 0;
+  const symbols = new Map();
+  window.Symbol = function Symbol(description) {
+    return Object.assign(new String(`$$symbol-${si++}`), { description });
+  };
+  Symbol.for = function SymbolFor(key) {
+    if (!symbols.has(key)) {
+      symbols.set(key, Symbol(key));
+    }
+    return symbols.get(key);
+  };
+}
 
 if (!Number.isFinite) {
   // eslint-disable-next-line no-extend-native
