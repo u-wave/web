02a9ec406e507351b8d88e79856933af47dfabcf
soonshiny <soonshiny@github.com>
2021-02-12 15:33:18 +0100

    add icon property to sources

--- src/sources/youtube/index.js
+++ src/sources/youtube/index.js
@@ -4,14 +4,15 @@ import ImportForm from './ImportForm';
 import reducer from './reducer';
 
 const ImportPanel = React.lazy(() => import('./ImportPanel'));
-
 const logo = new URL('../../../assets/img/youtube.png', import.meta.url);
+const icon = new URL('../../../assets/img/youtube-icon.png', import.meta.url);
 
 export default function youtube() {
   return {
     name: 'youtube',
     Player,
     logo,
+    icon,
     ImportForm,
     ImportPanel,
     reducer,
