f5c55ecdffe0f56104f955f03b0945280f22d92f
Renée Kooi <renee@kooi.me>
2021-02-19 10:37:33 +0100

    Support emojis specified as full URLs (#1861)

--- src/components/Chat/Markup/Emoji.js
+++ src/components/Chat/Markup/Emoji.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Tooltip from '@material-ui/core/Tooltip';
+import emojiUrl from '../../../utils/emojiUrl';
 
 const shortcode = (name) => `:${name}:`;
-const url = (filename) => `/assets/emoji/${filename}`;
 
 const Emoji = ({ name, image }) => (
   <Tooltip title={shortcode(name)} placement="top">
     <span className="Emoji" data-emoji={name}>
       <img
         className="Emoji-img"
-        src={url(image)}
+        src={emojiUrl(image)}
         alt={shortcode(name)}
       />
     </span>
