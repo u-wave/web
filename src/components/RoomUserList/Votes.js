575452863d37a67a1b5aa6e6c51a409a14db733a
ariedro <ariedro@gmail.com>
2021-04-13 10:50:55 -0300

    fix: Add sadvotes in user list

--- src/components/RoomUserList/Votes.js
+++ src/components/RoomUserList/Votes.js
@@ -3,13 +3,17 @@ import PropTypes from 'prop-types';
 import DownvoteIcon from '@material-ui/icons/ThumbDown';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import UpvoteIcon from '@material-ui/icons/ThumbUp';
+import SadvoteIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 
 const Votes = ({
-  upvote, downvote, favorite, ...props
+  upvote, downvote, sadvote, favorite, ...props
 }) => (
   <div {...props}>
     {favorite && (
       <FavoriteIcon className="UserRow-voteIcon UserRow-voteIcon--favorite" />
+    )}
+    {sadvote && (
+      <SadvoteIcon className="UserRow-voteIcon UserRow-voteIcon--sadvote" />
     )}
     {upvote && (
       <UpvoteIcon className="UserRow-voteIcon UserRow-voteIcon--upvote" />
@@ -23,6 +27,7 @@ const Votes = ({
 Votes.propTypes = {
   upvote: PropTypes.bool,
   downvote: PropTypes.bool,
+  sadvote: PropTypes.bool,
   favorite: PropTypes.bool,
 };
 
