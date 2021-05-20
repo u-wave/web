4dc28916e9a25bc86782eb88214574b0274bc7bb
ariedro <ariedro@gmail.com>
2021-05-20 00:35:34 -0300

    chore: Add configs for new server

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 printf '\033[0;93m🤔 [WARN] Tenés que tener las keys del server para hacer esto\n'
 npm run prod
-ssh root@165.22.2.248 'rm -r /root/pluggo/web/packages/u-wave-web-middleware'
-scp -r packages/u-wave-web-middleware root@165.22.2.248:/root/pluggo/web/packages
-ssh root@165.22.2.248 "bash -c 'PATH=/root/.nvm/versions/node/v14.16.1/bin /root/.nvm/versions/node/v14.16.1/bin/pm2 restart pluggo-fe'"
+ssh root@143.198.129.115 'rm -r /root/pluggo/web/packages/u-wave-web-middleware'
+scp -r packages/u-wave-web-middleware root@143.198.129.115:/root/pluggo/web/packages
+ssh root@143.198.129.115 "bash -c 'PATH=/root/.nvm/versions/node/v14.17.0/bin /root/.nvm/versions/node/v14.17.0/bin/pm2 restart pluggo-fe'"
