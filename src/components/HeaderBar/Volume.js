59fd1b267bfc77e953d616c3803018d3503ad7d6
Renée Kooi <renee@kooi.me>
2021-07-16 16:51:08 +0200

    rewrite Volume component to hooks

--- src/components/HeaderBar/Volume.js
+++ src/components/HeaderBar/Volume.js
@@ -1,6 +1,6 @@
 import cx from 'clsx';
 import React from 'react';
-import { translate } from '@u-wave/react-translate';
+import { useTranslator } from '@u-wave/react-translate';
 import PropTypes from 'prop-types';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
@@ -10,85 +10,63 @@ import VolumeMuteIcon from '@material-ui/icons/VolumeMute';
 import VolumeOffIcon from '@material-ui/icons/VolumeOff';
 import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 
-const enhance = translate();
+function Volume({
+  className,
+  volume,
+  muted,
+  onVolumeChange,
+  onMute,
+  onUnmute,
+}) {
+  const { t } = useTranslator();
 
-class Volume extends React.Component {
-  static propTypes = {
-    t: PropTypes.func.isRequired,
-    className: PropTypes.string,
-    volume: PropTypes.number,
-    muted: PropTypes.bool,
-
-    onVolumeChange: PropTypes.func,
-    onMute: PropTypes.func,
-    onUnmute: PropTypes.func,
-  };
-
-  shouldComponentUpdate(nextProps) {
-    const { volume, muted } = this.props;
-
-    return volume !== nextProps.volume
-      || muted !== nextProps.muted;
+  let volumeIcon = <VolumeUpIcon />;
+  if (muted) {
+    volumeIcon = <VolumeOffIcon />;
+  } else if (volume === 0) {
+    volumeIcon = <VolumeMuteIcon />;
+  } else if (volume < 50) {
+    volumeIcon = <VolumeDownIcon />;
   }
 
-  handleVolumeChange = (e, volume) => {
-    const { onVolumeChange } = this.props;
-
-    onVolumeChange(volume);
+  const label = muted ? t('booth.unmute') : t('booth.mute');
+  const handleMuteClick = muted ? onUnmute : onMute;
+  const handleVolumeChange = (e, newVolume) => {
+    onVolumeChange(newVolume);
   };
 
-  handleMuteClick = () => {
-    const { muted, onMute, onUnmute } = this.props;
-
-    if (muted) {
-      onUnmute();
-    } else {
-      onMute();
-    }
-  };
-
-  render() {
-    const {
-      t,
-      className,
-      muted,
-      volume,
-    } = this.props;
-
-    let VolumeIcon = VolumeUpIcon;
-    if (muted) {
-      VolumeIcon = VolumeOffIcon;
-    } else if (volume === 0) {
-      VolumeIcon = VolumeMuteIcon;
-    } else if (volume < 50) {
-      VolumeIcon = VolumeDownIcon;
-    }
-
-    const label = muted ? t('booth.unmute') : t('booth.mute');
-
-    return (
-      <div className={cx('VolumeSlider', className)}>
-        <Tooltip title={label} position="bottom">
-          <IconButton
-            aria-label={label}
-            onClick={this.handleMuteClick}
-          >
-            <VolumeIcon />
-          </IconButton>
-        </Tooltip>
-        <div className="VolumeSlider-slider">
-          <Slider
-            size="small"
-            min={0}
-            max={100}
-            step={1}
-            value={volume}
-            onChange={this.handleVolumeChange}
-          />
-        </div>
+  return (
+    <div className={cx('VolumeSlider', className)}>
+      <Tooltip title={label} position="bottom">
+        <IconButton
+          aria-label={label}
+          onClick={handleMuteClick}
+        >
+          {volumeIcon}
+        </IconButton>
+      </Tooltip>
+      <div className="VolumeSlider-slider">
+        <Slider
+          size="small"
+          min={0}
+          max={100}
+          step={1}
+          value={volume}
+          onChange={handleVolumeChange}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default enhance(Volume);
+Volume.propTypes = {
+  className: PropTypes.string,
+  volume: PropTypes.number,
+  muted: PropTypes.bool,
+
+  onVolumeChange: PropTypes.func,
+  onMute: PropTypes.func,
+  onUnmute: PropTypes.func,
+};
+
+export default Volume;
