41a82261a7dac9d51f0e28312a4b1f5e47f5b223
Renée Kooi <renee@kooi.me>
2021-07-22 12:36:53 +0200

    remove `text=` prop from Button

--- src/components/Form/Button.js
+++ src/components/Form/Button.js
@@ -3,23 +3,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MuiButton from '@material-ui/core/Button';
 
-const Button = ({
-  children, className, text, ...props
-}) => (
-  <MuiButton
-    variant="contained"
-    color="primary"
-    className={cx('Button', className)}
-    type="submit"
-    {...props}
-  >
-    {text || children}
-  </MuiButton>
-);
+function Button({ children, className, ...props }) {
+  return (
+    <MuiButton
+      variant="contained"
+      color="primary"
+      className={cx('Button', className)}
+      type="submit"
+      {...props}
+    >
+      {children}
+    </MuiButton>
+  );
+}
 
 Button.propTypes = {
   className: PropTypes.string,
-  text: PropTypes.string,
   children: PropTypes.node,
 };
 
