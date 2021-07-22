dd80a2013143e6c06eaac1cf602abb25959792e4
Renée Kooi <renee@kooi.me>
2021-07-22 15:59:22 +0200

    soundcloud: reduce some inline styles

--- src/sources/soundcloud/SongInfo.js
+++ src/sources/soundcloud/SongInfo.js
@@ -3,42 +3,43 @@ import PropTypes from 'prop-types';
 import ArtistIcon from '@material-ui/icons/Headset';
 import TitleIcon from '@material-ui/icons/MusicNote';
 
-const iconStyles = {
-  float: 'left',
-  marginRight: 3,
-};
-
-const SongInfo = ({
+function SongInfo({
   artist, title, artistUrl, trackUrl,
-}) => (
-  <div className="src-soundcloud-SongInfo">
-    <a
-      className="src-soundcloud-SongInfo-link src-soundcloud-SongInfo-link--artist"
-      target="_blank"
-      rel="noopener noreferrer"
-      href={artistUrl}
-      title={artist}
-    >
-      <ArtistIcon style={iconStyles} />
-      {artist}
-    </a>
-    <a
-      className="src-soundcloud-SongInfo-link src-soundcloud-SongInfo-link--track"
-      target="_blank"
-      rel="noopener noreferrer"
-      href={trackUrl}
-      title={title}
-    >
-      <TitleIcon style={iconStyles} />
-      {title}
-    </a>
-  </div>
-);
+}) {
+  return (
+    <div className="src-soundcloud-SongInfo">
+      <a
+        className="src-soundcloud-SongInfo-link src-soundcloud-SongInfo-link--artist"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={artistUrl}
+        title={artist}
+      >
+        <ArtistIcon className="src-soundcloud-SongInfo-icon" />
+        {artist}
+      </a>
+      <a
+        className="src-soundcloud-SongInfo-link src-soundcloud-SongInfo-link--track"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={trackUrl}
+        title={title}
+      >
+        <TitleIcon className="src-soundcloud-SongInfo-icon" />
+        {title}
+      </a>
+    </div>
+  );
+}
 
 SongInfo.propTypes = {
+  /** The uploader name on SoundCloud. */
   artist: PropTypes.string.isRequired,
+  /** The title of the media on SoundCloud. */
   title: PropTypes.string.isRequired,
+  /** Full URL to the artist's SoundCloud page. */
   artistUrl: PropTypes.string.isRequired,
+  /** Full URL to the track page. */
   trackUrl: PropTypes.string.isRequired,
 };
 
