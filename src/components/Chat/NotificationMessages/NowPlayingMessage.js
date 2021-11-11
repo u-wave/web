83cde4233e22eb9cd9699d149375b148a2333aae
Renée Kooi <renee@kooi.me>
2021-11-11 20:10:30 +0100

    use mui 5 divider

--- src/components/Chat/NotificationMessages/NowPlayingMessage.js
+++ src/components/Chat/NotificationMessages/NowPlayingMessage.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Divider from '@mui/material/Divider';
 import MessageTimestamp from '../MessageTimestamp';
 
-const NowPlayingMessage = ({ entry, timestamp }) => (
-  <div
-    className="NowPlayingMessage"
-  >
-    <div className="NowPlayingMessage-mediaWrap">
-      <span className="NowPlayingMessage-media">
+function NowPlayingMessage({ entry, timestamp }) {
+  return (
+    <div className="NowPlayingMessage">
+      <Divider>
         {entry.artist} – {entry.title}
-      </span>
+      </Divider>
+      <div className="NowPlayingMessage-time">
+        <MessageTimestamp date={new Date(timestamp)} />
+      </div>
     </div>
-    <div className="NowPlayingMessage-time">
-      <MessageTimestamp date={new Date(timestamp)} />
-    </div>
-  </div>
-);
+  );
+}
 
 NowPlayingMessage.propTypes = {
   entry: PropTypes.object.isRequired,
