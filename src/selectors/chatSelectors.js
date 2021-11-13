bf3fc2ccc3d89843e94d3d5283f4db7d64b5bca0
Renée Kooi <renee@kooi.me>
2021-11-13 12:21:58 +0100

    Display advance messages in the chat (#1666)

--- src/selectors/chatSelectors.js
+++ src/selectors/chatSelectors.js
@@ -25,16 +25,26 @@ export const motdSelector = createSelector(
 
 const MAX_MESSAGES = 500;
 const allMessagesSelector = createSelector(baseSelector, (chat) => chat.messages);
+// Hide notifications that are disabled.
+const applyNotificationSettings = (messages, notificationSettings) => messages.filter((message) => {
+  if (message.type === 'userJoin') return notificationSettings.userJoin;
+  if (message.type === 'userLeave') return notificationSettings.userLeave;
+  if (message.type === 'userNameChanged') return notificationSettings.userNameChanged;
+  if (message.type === 'skip') return notificationSettings.skip;
+  return true;
+});
+// Only show the most recent now playing notification.
+const collapseNowPlayingNotifications = (messages) => messages.filter((message, i) => {
+  if (message.type !== 'nowPlaying') return true;
+  const nextMessage = messages[i + 1];
+  return nextMessage && nextMessage.type !== 'nowPlaying';
+});
 const filteredMessagesSelector = createSelector(
   allMessagesSelector,
   notificationSettingsSelector,
-  (messages, notificationSettings) => messages.filter((message) => {
-    if (message.type === 'userJoin') return notificationSettings.userJoin;
-    if (message.type === 'userLeave') return notificationSettings.userLeave;
-    if (message.type === 'userNameChanged') return notificationSettings.userNameChanged;
-    if (message.type === 'skip') return notificationSettings.skip;
-    return true;
-  }),
+  (messages, notificationSettings) => collapseNowPlayingNotifications(
+    applyNotificationSettings(messages, notificationSettings),
+  ),
 );
 export const messagesSelector = createSelector(
   filteredMessagesSelector,
