2dc8c35845278d1415c726ec65b772b113c067fd
Renée Kooi <renee@kooi.me>
2021-11-16 15:57:53 +0100

    build: use postcss-preset-env

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,10 +3,14 @@
 module.exports = {
   plugins: [
     // TODO replace by postcss-preset-env when its next release is out
-    'autoprefixer',
-    'postcss-nesting',
-    'postcss-logical',
-    'postcss-dir-pseudo-class',
+    ['postcss-preset-env', {
+      importFrom: './src/vars.css',
+      features: {
+        'nesting-rules': true,
+        // For `float: inline-start` support in chrome
+        'logical-properties-and-values': true,
+      },
+    }],
 
     // TODO remove uses of color-mod(), as it is not going to be standardised
     'postcss-color-mod-function',
