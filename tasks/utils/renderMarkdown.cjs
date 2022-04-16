4ef82b58df9fa66a154b5f074f5157f2dffa758f
Renée Kooi <renee@kooi.me>
2022-04-16 10:16:06 +0200

    deps: update react-markdown

--- tasks/utils/renderMarkdown.cjs
+++ tasks/utils/renderMarkdown.cjs
@@ -1,15 +1,19 @@
 'use strict';
 
-const h = require('react').createElement;
+const { createElement: h } = require('react');
 const prerender = require('./prerender.cjs');
 
-module.exports = function renderMarkdown(source) {
-  // eslint-disable-next-line global-require
-  const MarkdownWithHtml = require('../../src/components/Markdown/WithHtml').default;
+async function renderMarkdown(source) {
+  const { default: Markdown } = await import('react-markdown');
+  const { default: rehypeRaw } = await import('rehype-raw');
 
-  const { html, css } = prerender(h(MarkdownWithHtml, { source }));
+  const { html, css } = prerender(h(Markdown, {
+    rehypePlugins: [rehypeRaw],
+  }, source));
   if (css === '') {
     return html;
   }
   return `<style>${css}</style>${html}`;
-};
+}
+
+module.exports = renderMarkdown;
