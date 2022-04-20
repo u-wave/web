d41c1f71bd9aec281715f24649312e5b8678f1ac
Renée Kooi <renee@kooi.me>
2022-04-20 09:52:58 +0200

    Remove uses of nonstandard CSS `color-mod()` (#2307)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,7 +2,6 @@
 
 module.exports = {
   plugins: [
-    // TODO replace by postcss-preset-env when its next release is out
     ['postcss-preset-env', {
       importFrom: './src/vars.css',
       features: {
@@ -11,8 +10,5 @@ module.exports = {
         'logical-properties-and-values': true,
       },
     }],
-
-    // TODO remove uses of color-mod(), as it is not going to be standardised
-    'postcss-color-mod-function',
   ],
 };
