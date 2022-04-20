1969fead21eb4c9c8801d9a7795f61a70be71c23
Renée Kooi <renee@kooi.me>
2022-04-20 11:05:45 +0200

    add tooltips

--- src/components/UserCard/AddToWaitlistButton.js
+++ src/components/UserCard/AddToWaitlistButton.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { useTranslator } from '@u-wave/react-translate';
+import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import { joinWaitlist } from '../../actions/WaitlistActionCreators';
@@ -8,13 +10,16 @@ import { joinWaitlist } from '../../actions/WaitlistActionCreators';
 const { useCallback } = React;
 
 function AddToWaitlistButton({ user }) {
+  const { t } = useTranslator();
   const dispatch = useDispatch();
   const onClick = useCallback(() => dispatch(joinWaitlist(user)), [dispatch, user]);
 
   return (
-    <IconButton onClick={onClick}>
-      <GroupAddIcon />
-    </IconButton>
+    <Tooltip title={t('waitlist.add')}>
+      <IconButton onClick={onClick}>
+        <GroupAddIcon />
+      </IconButton>
+    </Tooltip>
   );
 }
 AddToWaitlistButton.propTypes = {
--- src/components/UserCard/RemoveFromWaitlistButton.js
+++ src/components/UserCard/RemoveFromWaitlistButton.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { useTranslator } from '@u-wave/react-translate';
+import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import GroupRemoveIcon from '@mui/icons-material/GroupRemove';
 import { removeWaitlistUser } from '../../actions/ModerationActionCreators';
@@ -8,13 +10,16 @@ import { removeWaitlistUser } from '../../actions/ModerationActionCreators';
 const { useCallback } = React;
 
 function RemoveFromWaitlistButton({ user }) {
+  const { t } = useTranslator();
   const dispatch = useDispatch();
   const onClick = useCallback(() => dispatch(removeWaitlistUser(user)), [dispatch, user]);
 
   return (
-    <IconButton onClick={onClick}>
-      <GroupRemoveIcon />
-    </IconButton>
+    <Tooltip title={t('waitlist.remove')}>
+      <IconButton onClick={onClick}>
+        <GroupRemoveIcon />
+      </IconButton>
+    </Tooltip>
   );
 }
 RemoveFromWaitlistButton.propTypes = {
