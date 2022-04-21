bfa8af43d8ed386880eaef8245d4becddc149617
Renée Kooi <renee@kooi.me>
2022-04-21 18:22:50 +0200

    ban tooltip

--- src/components/UserCard/BanButton.js
+++ src/components/UserCard/BanButton.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { useTranslator } from '@u-wave/react-translate';
+import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import BlockIcon from '@mui/icons-material/Block';
 import { banUser } from '../../actions/ModerationActionCreators';
@@ -8,13 +10,16 @@ import { banUser } from '../../actions/ModerationActionCreators';
 const { useCallback } = React;
 
 function BanButton({ user }) {
+  const { t } = useTranslator();
   const dispatch = useDispatch();
   const onClick = useCallback(() => dispatch(banUser(user)), [dispatch, user]);
 
   return (
-    <IconButton onClick={onClick}>
-      <BlockIcon />
-    </IconButton>
+    <Tooltip title={t('users.ban')}>
+      <IconButton onClick={onClick}>
+        <BlockIcon />
+      </IconButton>
+    </Tooltip>
   );
 }
 BanButton.propTypes = {
