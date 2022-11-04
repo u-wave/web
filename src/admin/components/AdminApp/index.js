417ec99bf82033090d22da9d72d0b77863fbaf25
Renée Kooi <renee@kooi.me>
2022-11-04 13:59:43 +0100

    admin: use `startTransition`

--- src/admin/components/AdminApp/index.js
+++ src/admin/components/AdminApp/index.js
@@ -7,50 +7,61 @@ import '../../index.css';
 
 const Fallback = () => <CircularProgress size="100%" />;
 
-const AdminApp = ({
-  currentView,
-  onTransition,
-}) => (
-  <div className="AdminApp">
-    <div className="AdminApp-menu">
-      <List>
-        <ListItem
-          selected={currentView === 'main'}
-          className="AdminApp-menuItem"
-          onClick={() => onTransition('main')}
-        >
-          <ListItemText primary="Main" />
-        </ListItem>
-        <ListItem
-          selected={currentView === 'users'}
-          className="AdminApp-menuItem"
-          onClick={() => onTransition('users')}
-        >
-          <ListItemText primary="Users" />
-        </ListItem>
-        <ListItem
-          selected={currentView === 'bans'}
-          className="AdminApp-menuItem"
-          onClick={() => onTransition('bans')}
-        >
-          <ListItemText primary="Bans" />
-        </ListItem>
-        <ListItem
-          selected={currentView === 'config'}
-          className="AdminApp-menuItem"
-          onClick={() => onTransition('config')}
-        >
-          <ListItemText primary="Server Config" />
-        </ListItem>
-      </List>
+const {
+  useTransition,
+} = React;
+
+function AdminApp({ currentView, onTransition }) {
+  const [_isPending, startTransition] = useTransition();
+
+  const onClick = (nextView) => {
+    startTransition(() => {
+      onTransition(nextView);
+    });
+  };
+
+  return (
+    <div className="AdminApp">
+      <div className="AdminApp-menu">
+        <List>
+          <ListItem
+            selected={currentView === 'main'}
+            className="AdminApp-menuItem"
+            onClick={() => onClick('main')}
+          >
+            <ListItemText primary="Main" />
+          </ListItem>
+          <ListItem
+            selected={currentView === 'users'}
+            className="AdminApp-menuItem"
+            onClick={() => onClick('users')}
+          >
+            <ListItemText primary="Users" />
+          </ListItem>
+          <ListItem
+            selected={currentView === 'bans'}
+            className="AdminApp-menuItem"
+            onClick={() => onClick('bans')}
+          >
+            <ListItemText primary="Bans" />
+          </ListItem>
+          <ListItem
+            selected={currentView === 'config'}
+            className="AdminApp-menuItem"
+            onClick={() => onClick('config')}
+          >
+            <ListItemText primary="Server Config" />
+          </ListItem>
+        </List>
+      </div>
+      <div className="AdminApp-page">
+        <React.Suspense fallback={<Fallback />}>
+          <CurrentPage page={currentView} />
+        </React.Suspense>
+      </div>
     </div>
-    <div className="AdminApp-page">
-      <React.Suspense fallback={<Fallback />}>
-        <CurrentPage page={currentView} />
-      </React.Suspense>
-    </div>
-  </div>
-);
+  );
+}
 
 AdminApp.propTypes = {
   currentView: PropTypes.string.isRequired,
