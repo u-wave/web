646e752b23b7d4fe39fd93d72e588942e92e97a8
Renée Kooi <renee@kooi.me>
2022-12-09 11:31:50 +0100

    do not transpile emotion output again

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,14 +2,24 @@
 
 const presetEnv = require('postcss-preset-env');
 
+const preset = presetEnv({
+  features: {
+    'nesting-rules': true,
+    // For `float: inline-start` support in chrome
+    'logical-properties-and-values': true,
+  },
+});
+
 module.exports = {
-  plugins: [
-    presetEnv({
-      features: {
-        'nesting-rules': true,
-        // For `float: inline-start` support in chrome
-        'logical-properties-and-values': true,
-      },
-    }),
-  ],
+  plugins: [{
+    postcssPlugin: 'u-wave-preset-env',
+    prepare(result) {
+      // CSS inside HTML files is prerendered by emotion,
+      // so it is already transpiled.
+      if (result.opts.from.includes('html-proxy')) {
+        return null;
+      }
+      return preset;
+    },
+  }],
 };
