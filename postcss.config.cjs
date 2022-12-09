7f7090f34683d7678a863f3e5fd285bd33cd703f
Renée Kooi <renee@kooi.me>
2022-12-09 12:17:04 +0100

    make the hack work

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -10,16 +10,22 @@ const preset = presetEnv({
   },
 });
 
-module.exports = {
-  plugins: [{
-    postcssPlugin: 'u-wave-preset-env',
+function skipIfHtmlProxy(plugin) {
+  return {
     prepare(result) {
       // CSS inside HTML files is prerendered by emotion,
       // so it is already transpiled.
       if (result.opts.from.includes('html-proxy')) {
         return null;
       }
-      return preset;
+      return plugin.prepare ? { ...plugin, ...plugin.prepare(result) } : plugin;
     },
+  };
+}
+
+module.exports = {
+  plugins: [{
+    postcssPlugin: 'u-wave-preset-env',
+    plugins: preset.plugins.map(skipIfHtmlProxy),
   }],
 };
