65dc747a7bfe0de46af754440ce57ad8827eb248
Renée Kooi <renee@kooi.me>
2022-12-27 13:46:58 +0100

    Fix locale loading
    Would prefer to load these as JSON files, but letting Vite turn them
    into ES modules is easier…

--- src/locales.js
+++ src/locales.js
@@ -1,27 +1,11 @@
 import en from '../locale/en.yaml';
 
-function fetchLocale(url) {
-  let cache;
-  return () => {
-    if (!cache) {
-      cache = fetch(url).then((response) => response.json());
-    }
-    return cache;
-  };
-}
+const modules = import.meta.glob(['../locale/*.yaml', '!../locale/en.yaml']);
 
-export const resources = {
-  en: () => Promise.resolve(en),
-  cs: fetchLocale(new URL('../locale/cs.yaml', import.meta.url)),
-  cy: fetchLocale(new URL('../locale/cy.yaml', import.meta.url)),
-  de: fetchLocale(new URL('../locale/de.yaml', import.meta.url)),
-  es: fetchLocale(new URL('../locale/es.yaml', import.meta.url)),
-  fr: fetchLocale(new URL('../locale/fr.yaml', import.meta.url)),
-  ko: fetchLocale(new URL('../locale/ko.yaml', import.meta.url)),
-  nl: fetchLocale(new URL('../locale/nl.yaml', import.meta.url)),
-  pt: fetchLocale(new URL('../locale/pt.yaml', import.meta.url)),
-  zh: fetchLocale(new URL('../locale/zh.yaml', import.meta.url)),
-  ru: fetchLocale(new URL('../locale/ru.yaml', import.meta.url)),
-};
+export const resources = Object.fromEntries(
+  Object.entries(modules)
+    .map(([path, loader]) => [/\/(\w+)\.yaml$/.exec(path)[1], loader])
+    .concat([['en', () => en]]),
+);
 
 export const availableLanguages = Object.keys(resources);
