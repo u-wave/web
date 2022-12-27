11dc4da350f0d29893d2a46aae0236b715a3a374
Renée Kooi <renee@kooi.me>
2022-12-27 14:05:51 +0100

    build: update npm package version after build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,11 @@
+import { readFile, writeFile } from 'node:fs/promises';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import yaml from '@rollup/plugin-yaml';
 import prerender from './tasks/prerender.mjs';
+
+const inputPkg = new URL('./package.json', import.meta.url);
+const outputPkg = new URL('./npm/package.json', import.meta.url);
 
 export default defineConfig({
   clearScreen: false,
@@ -41,5 +45,15 @@ export default defineConfig({
     prerender({ file: 'index.html', source: 'src/index.jsx' }),
     prerender({ file: 'password-reset.html', source: 'src/password-reset/index.jsx' }),
     prerender({ file: 'privacy.html', source: 'src/markdown.jsx', props: { path: 'static/privacy.md' } }),
+    {
+      name: 'u-wave-write-package-version',
+      apply: 'build',
+      async closeBundle() {
+        const { version } = JSON.parse(await readFile(inputPkg, 'utf8'));
+        const pkg = JSON.parse(await readFile(outputPkg, 'utf8'));
+        pkg.version = version;
+        await writeFile(outputPkg, `${JSON.stringify(pkg, null, 2)}\n`);
+      },
+    },
   ],
 });
