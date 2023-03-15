67702684020cf48762522ff9b81a12ceb64ee5d1
Renée Kooi <renee@kooi.me>
2023-03-15 11:42:57 +0100

    Add types for config reducer

--- src/reducers/config.ts
+++ src/reducers/config.ts
@@ -1,12 +1,21 @@
+import { AnyAction } from 'redux';
 import { INIT_STATE, LOAD_EMOTES } from '../constants/ActionTypes';
 
-export const initialState = {
+interface State {
+  roles: undefined | Record<string, string[]>;
+  apiUrl?: string,
+  socketUrl?: string,
+  emoji: Record<string, string>,
+  serverEmotes: { name: string, url: string }[],
+}
+
+export const initialState: State = {
   roles: undefined,
   emoji: {},
   serverEmotes: [],
 };
 
-export default function reduce(state = initialState, action) {
+export default function reduce(state = initialState, action: AnyAction): State {
   const { type, payload } = action;
   switch (type) {
     case INIT_STATE:
