3d8ae4a189265150d4fbdf5ebcac582bab022984
Renée Kooi <renee@kooi.me>
2023-03-15 10:55:50 +0100

    Typed useStore hook

--- src/hooks/useRedux.ts
+++ src/hooks/useRedux.ts
@@ -1,9 +1,11 @@
 import {
   useDispatch as useDispatchBase,
   useSelector as useSelectorBase,
+  useStore as useStoreBase,
   type TypedUseSelectorHook,
 } from 'react-redux';
 import configureStore, { StoreState } from '../redux/configureStore';
 
 export const useDispatch = useDispatchBase<ReturnType<typeof configureStore>['dispatch']>;
 export const useSelector: TypedUseSelectorHook<StoreState> = useSelectorBase;
+export const useStore = useStoreBase<StoreState>;
