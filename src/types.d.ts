383fff916a51bae395c78e3033f0007a89698fb4
Renée Kooi <renee@kooi.me>
2023-03-19 12:50:11 +0100

    Declare useTranslator()

--- src/types.d.ts
+++ src/types.d.ts
@@ -12,3 +12,9 @@ declare module '*.yaml' {
   const data: object;
   export default data;
 }
+
+declare module '@u-wave/react-translate' {
+  import { Translator } from '@u-wave/translate';
+
+  export function useTranslator(): Translator;
+}
