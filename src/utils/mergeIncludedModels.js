13aab8b840c4be90a479b065d6ea825269817990
Renée Kooi <renee@kooi.me>
2023-03-23 11:35:58 +0100

    use deepmerge instead of structuredClone

--- src/utils/mergeIncludedModels.js
+++ src/utils/mergeIncludedModels.js
@@ -1,11 +1,12 @@
 import dlv from 'dlv';
 import { dset } from 'dset';
+import merge from 'deepmerge';
 
 /**
  * @param {{ data: any[], meta: object, included: object }} response
  */
 function mergeIncludedModels({ data: input, meta, included }) {
-  const data = structuredClone(input);
+  const data = merge({}, input, { clone: true });
   Object.keys(meta.included ?? {}).forEach((type) => {
     meta.included[type].forEach((path) => {
       data.forEach((item) => {
