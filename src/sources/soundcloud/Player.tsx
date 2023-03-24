91544ccb7e8bac9f71dfc1655f66a8f6a9a647b1
Renée Kooi <renee@kooi.me>
2023-03-24 16:25:43 +0100

    Write soundcloud player with hooks

--- src/sources/soundcloud/Player.tsx
+++ src/sources/soundcloud/Player.tsx
@@ -1,6 +1,11 @@
 import cx from 'clsx';
-import React from 'react';
-import { translate } from '@u-wave/react-translate';
+import {
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { useTranslator } from '@u-wave/react-translate';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -29,10 +34,7 @@ function getErrorMessage(err: Error) {
   return 'soundcloud.error.other';
 }
 
-const enhance = translate();
-
 type SoundCloudPlayerProps = {
-  t: (key: string, params?: object) => string,
   className?: string,
   active: boolean,
   enabled: boolean,
@@ -41,185 +43,160 @@ type SoundCloudPlayerProps = {
   volume: number,
   onPlay?: () => void,
 };
+function SoundCloudPlayer({
+  className,
+  active,
+  enabled,
+  media,
+  seek,
+  volume,
+  onPlay,
+}: SoundCloudPlayerProps) {
+  const { t } = useTranslator();
+  const audioRef = useRef<HTMLAudioElement|null>(null);
+  const [error, setError] = useState<Error|null>(null);
+  const needsTap = error?.name === 'NotAllowedError';
 
-type SoundCloudPlayerState = { error: Error | null, needsTap: boolean };
-class SoundCloudPlayer extends React.Component<SoundCloudPlayerProps, SoundCloudPlayerState> {
-  audio = new Audio();
+  const audioUrl = useMemo(() => {
+    if ('streamUrl' in media.sourceData && typeof media.sourceData.streamUrl === 'string'
+        && enabled && active) {
+      const { streamUrl } = media.sourceData;
+      return `${streamUrl}?client_id=${CLIENT_ID}`;
+    }
+    return null;
+  }, [media, enabled, active]);
 
-  constructor(props: SoundCloudPlayerProps) {
-    super(props);
-
-    this.state = {
-      error: null,
-      needsTap: false,
-    };
-  }
-
-  componentDidMount() {
-    this.audio.addEventListener('error', () => {
-      if (this.audio.error) {
-        this.handleError(Object.assign(this.audio.error, { name: 'MediaError' }));
+  useEffect(() => {
+    const audio = new Audio();
+    audioRef.current = audio;
+    audio.addEventListener('error', () => {
+      if (audio.error) {
+        setError(Object.assign(audio.error, { name: 'MediaError' }));
       } else {
-        this.handleError(new Error('Unknown error'));
+        setError(new Error('Unknown error'));
       }
     });
-    this.audio.autoplay = true;
-    this.play();
-  }
+  }, []);
 
-  componentDidUpdate(prevProps: SoundCloudPlayerProps) {
-    const {
-      volume, active, enabled, media,
-    } = this.props;
-
-    if (prevProps.volume !== volume) {
-      this.audio.volume = volume / 100;
-    }
-    if (prevProps.media.sourceID !== media.sourceID
-        || prevProps.enabled !== enabled
-        || prevProps.active !== active) {
-      if (enabled && active) {
-        this.play();
-      } else {
-        this.stop();
-      }
-    }
-  }
-
-  componentWillUnmount() {
-    this.stop();
-  }
-
-  handleError = (error: Error) => {
-    this.setState({
-      error,
-      needsTap: error.name === 'NotAllowedError',
-    });
-  };
-
-  handlePlay = () => {
-    this.play();
-  };
-
-  play() {
-    const {
-      active, enabled, media, volume, seek, onPlay,
-    } = this.props;
-
-    this.setState({ needsTap: false, error: null });
-    if (enabled && active) {
-      // In Firefox we have to wait for the "canplaythrough" event before
-      // seeking.
-      // http://stackoverflow.com/a/34970444
-      const doSeek = () => {
-        this.audio.currentTime = seek + (media.start ?? 0);
-        this.audio.volume = volume / 100;
-        this.audio.removeEventListener('canplaythrough', doSeek, false);
+  useEffect(() => {
+    setError(null);
+    if (!audioRef.current || !audioUrl) {
+      return () => {
+        // dummy
       };
-
-      if ('streamUrl' in media.sourceData && typeof media.sourceData.streamUrl === 'string') {
-        const { streamUrl } = media.sourceData;
-        this.audio.src = `${streamUrl}?client_id=${CLIENT_ID}`;
-        this.audio.play().catch(this.handleError);
-        this.audio.addEventListener('canplaythrough', doSeek, false);
-        if (onPlay) {
-          this.audio.addEventListener('play', onPlay, false);
-        }
-      } else {
-        this.handleError(new Error('Server did not send a stream URL'));
-      }
-    } else {
-      this.stop();
     }
-  }
-
-  stop() {
-    this.setState({ error: null });
-    this.audio.pause();
-  }
-
-  render() {
-    const { active } = this.props;
-    if (!active) {
-      return null;
-    }
-
-    const { t, media, className } = this.props;
-    const { error, needsTap } = this.state;
-    const sourceData = media.sourceData as undefined | {
-      username: string,
-      fullTitle: string,
-      artistUrl: string,
-      permalinkUrl: string,
+    const audio = audioRef.current;
+    audio.src = audioUrl;
+    audio.play().catch(setError);
+    const doSeek = () => {
+      audio.currentTime = seek + (media.start ?? 0);
+      audio.removeEventListener('canplaythrough', doSeek, false);
     };
-    if (!sourceData) {
-      return <div className={cx('src-soundcloud-Player', className)} />;
-    }
+    audio.addEventListener('canplaythrough', doSeek, false);
 
-    if (needsTap) {
-      return (
-        <div className={cx('src-soundcloud-Player', className)}>
-          <Paper className="src-soundcloud-Player-message">
-            <Typography component="p" paragraph>
-              {t('booth.autoplayBlocked')}
-            </Typography>
-            <Button variant="contained" onClick={this.handlePlay}>
-              {t('booth.play')}
-            </Button>
-          </Paper>
-        </div>
-      );
-    }
+    return () => {
+      audio.pause();
+      audio.removeEventListener('canplaythrough', doSeek, false);
+    };
 
-    if (error) {
-      return (
-        <div className={cx('src-soundcloud-Player', className)}>
-          <Paper className="src-soundcloud-Player-error">
-            <SvgIcon path={mdiAlertCircle} className="src-soundcloud-Player-errorIcon" />
-            <Typography component="p">
-              {t('soundcloud.error.template', {
-                error: t(getErrorMessage(error), { message: error.message }),
-              })}
-            </Typography>
-          </Paper>
-        </div>
-      );
-    }
+    // `seek` / `media.start` should only be taken into account when
+    // the media changes.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [audioUrl]);
 
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume / 100;
+    }
+  }, [volume]);
+
+  useEffect(() => {
+    const handler = () => {
+      setError(null);
+      onPlay?.();
+    };
+
+    const audio = audioRef.current;
+    if (audio) {
+      audio.addEventListener('play', handler, false);
+    }
+    return () => {
+      if (audio) {
+        audio.removeEventListener('play', handler);
+      }
+    };
+  }, [onPlay]);
+
+  const sourceData = media.sourceData as undefined | {
+    username: string,
+    fullTitle: string,
+    artistUrl: string,
+    permalinkUrl: string,
+  };
+  if (!sourceData) {
+    return <div className={cx('src-soundcloud-Player', className)} />;
+  }
+
+  if (needsTap) {
     return (
       <div className={cx('src-soundcloud-Player', className)}>
-        <div className="src-soundcloud-Player-meta">
-          <div className="src-soundcloud-Player-info">
-            <img
-              className="src-soundcloud-Player-art"
-              src={media.thumbnail}
-              alt=""
-            />
-            <div className="src-soundcloud-Player-links">
-              <SongInfo
-                artist={sourceData.username}
-                title={sourceData.fullTitle}
-                artistUrl={sourceData.artistUrl}
-                trackUrl={sourceData.permalinkUrl}
-              />
-            </div>
-          </div>
-          <a
-            href={sourceData.permalinkUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="src-soundcloud-Player-permalink"
-          >
-            View on{' '}
-            <img
-              src={soundcloudLogo.href}
-              alt="SoundCloud"
-            />
-          </a>
-        </div>
+        <Paper className="src-soundcloud-Player-message">
+          <Typography component="p" paragraph>
+            {t('booth.autoplayBlocked')}
+          </Typography>
+          <Button variant="contained" onClick={() => audioRef.current?.play()}>
+            {t('booth.play')}
+          </Button>
+        </Paper>
       </div>
     );
   }
+
+  if (error) {
+    return (
+      <div className={cx('src-soundcloud-Player', className)}>
+        <Paper className="src-soundcloud-Player-error">
+          <SvgIcon path={mdiAlertCircle} className="src-soundcloud-Player-errorIcon" />
+          <Typography component="p">
+            {t('soundcloud.error.template', {
+              error: t(getErrorMessage(error), { message: error.message }),
+            })}
+          </Typography>
+        </Paper>
+      </div>
+    );
+  }
+
+  return (
+    <div className={cx('src-soundcloud-Player', className)}>
+      <div className="src-soundcloud-Player-meta">
+        <div className="src-soundcloud-Player-info">
+          <img
+            className="src-soundcloud-Player-art"
+            src={media.thumbnail}
+            alt=""
+          />
+          <div className="src-soundcloud-Player-links">
+            <SongInfo
+              artist={sourceData.username}
+              title={sourceData.fullTitle}
+              artistUrl={sourceData.artistUrl}
+              trackUrl={sourceData.permalinkUrl}
+            />
+          </div>
+        </div>
+        <a
+          href={sourceData.permalinkUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="src-soundcloud-Player-permalink"
+        >
+          View on{' '}
+          <img src={soundcloudLogo.href} alt="SoundCloud" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default enhance(SoundCloudPlayer);
+export default SoundCloudPlayer;
