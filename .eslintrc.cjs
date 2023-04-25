def3bec35866d69844c25fe54606070bef701f8e
Renée Kooi <renee@kooi.me>
2023-04-25 17:08:14 +0200

    build: allow for-of

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,23 @@ module.exports = {
   rules: {
     'arrow-body-style': 'off',
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    // Modify eslint-config-airbnb's rule to allow ForOfStatement
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'import/extensions': ['error', 'ignorePackages', {
       js: 'never',
