3a6dd5ca42d7800f3d70da6bfbdeb852fc7500d3
Renée Kooi <renee@kooi.me>
2023-04-25 17:08:42 +0200

    build: lint passes

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,6 +1,7 @@
 import * as rtk from '@reduxjs/toolkit';
 import type { StoreState } from './configureStore';
 
+// eslint-disable-next-line import/prefer-default-export
 export const createAsyncThunk = rtk.createAsyncThunk.withTypes<{
   state: StoreState,
 }>();
