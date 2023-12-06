e513f368f6b2684d40f380b0ef8b39697335f35a
Renée Kooi <renee@kooi.me>
2023-12-06 13:07:51 +0100

    Fix SWR base URLs on demo site

--- src/utils/readApplicationConfig.js
+++ src/utils/readApplicationConfig.js
@@ -13,5 +13,11 @@ export default function readApplicationConfig(container = '#u-wave-config') {
     // ignore
   }
 
+  if (import.meta.env.VITE_DEMO) {
+    const url = new URL(window.location.href);
+    config.apiUrl = url.searchParams.get('apiUrl') ?? 'https://u-wave-demo.fly.dev/api';
+    config.socketUrl = url.searchParams.get('socketUrl') ?? 'wss://u-wave-demo.fly.dev';
+  }
+
   return config;
 }
