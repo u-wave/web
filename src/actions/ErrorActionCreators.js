ff5d0ea60649f42001402d7b4ac46354078400d9
Renée Kooi <renee@kooi.me>
2023-12-29 12:32:45 +0100

    Move error snackbar state into RTK slice

--- src/actions/ErrorActionCreators.js
+++
@@ -1,6 +0,0 @@
-import { ERROR_DISMISS } from '../constants/ActionTypes';
-
-// eslint-disable-next-line import/prefer-default-export
-export function dismiss() {
-  return { type: ERROR_DISMISS };
-}
--- src/reducers/errors.ts
+++ src/reducers/errors.ts
@@ -1,20 +1,32 @@
-import type { AnyAction } from 'redux';
-import type { StoreState } from '../redux/configureStore';
+import { type UnknownAction, createSlice } from '@reduxjs/toolkit';
 
-type State = string[];
+const slice = createSlice({
+  name: 'errors',
+  initialState: [] as string[],
+  reducers: {
+    dismissError: (state) => {
+      return state.slice(1);
+    },
+  },
+  selectors: {
+    firstError: (state) => state[0],
+  },
+  extraReducers(builder) {
+    builder.addMatcher(
+      (action: UnknownAction) => 'error' in action && Boolean(action.error),
+      (state, action: { type: string, payload?: Error, error?: Error }) => {
+        const { payload, error } = action;
+        const message = (typeof error === 'object' ? error?.message : null) ?? payload?.message ?? 'Unknown error';
+        // Avoid adding the same error over and over
+        if (state.length === 0 || state[state.length - 1] !== message) {
+          state.push(message);
+        }
+      },
+    );
+  },
+});
 
-export default function reduce(state: State = [], action: AnyAction): State {
-  const { type, payload, error } = action;
-  if (error) {
-    const message = (typeof error === 'object' && error?.message) ?? payload?.message ?? 'Unknown error';
-    // Avoid adding the same error over and over
-    if (state.length === 0 || state[state.length - 1] !== message) {
-      return [...state, message];
-    }
-  } else if (type === 'errors/DISMISS') {
-    return state.slice(1);
-  }
-  return state;
-}
+export default slice.reducer;
 
-export const firstErrorSelector = (state: StoreState) => state.errors[0];
+export const { dismissError } = slice.actions;
+export const { firstError: firstErrorSelector } = slice.selectors;
