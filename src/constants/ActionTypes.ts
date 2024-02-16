eee704b7f5e753cbe60465c792f7d739f1f50717
Renée Kooi <renee@kooi.me>
2024-02-16 15:49:32 +0100

    Remove old redux-based request implementation

--- src/constants/ActionTypes.ts
+++ src/constants/ActionTypes.ts
@@ -3,5 +3,4 @@ export * from './actionTypes/chat';
 export * from './actionTypes/errors';
 export * from './actionTypes/locales';
 export * from './actionTypes/moderation';
-export * from './actionTypes/request';
 export * from './actionTypes/waitlist';
